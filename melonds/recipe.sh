d5bfa13c32a75f95a9a8472d0eee4cfb3e06bfe8
icenine451 <benjamin.r.shelton@protonmail.com>
2025-05-13 16:20:58 -0400

    Initial recipe for MelonDS component

--- melonds/recipe.sh
+++ melonds/recipe.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-source "automation-tools/utils.sh"
+wget https://github.com/RetroDECK/net.kuribo64.melonDS/releases/latest/download/RetroDECK-melonds-Artifact.tar.gz
 
-grab flatpak_artifacts "https://github.com/RetroDECK/net.kuribo64.melonDS/releases/latest/download/RetroDECK-melonds-Artifact.tar.gz"
+mkdir melonds-tmp
 
-# custom commnads goes here
+mkdir melonds
 
-finalize
+tar -xzf RetroDECK-melonds-Artifact.tar.gz -C melonds-tmp
+
+mv melonds-tmp/files/bin/ melonds/
+mv melonds-tmp/files/lib/ melonds/
+mv melonds-tmp/files/share/ melonds/
+
+cp component_launcher.sh manifest.json functions.sh prepare_component.sh melonds/
+chmod +x melonds/component_launcher.sh
+
+tar -czf "melonds-artifact.tar.gz" "melonds"
+
+rm -rf melonds-tmp
+rm -rf melonds
