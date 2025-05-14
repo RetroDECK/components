5abe15020d3584b7543035f88771b34a01315328
icenine451 <benjamin.r.shelton@protonmail.com>
2025-05-14 15:09:55 -0400

    Remove unneeded libraries from Cemu AppImage extracted contents

--- cemu/recipe.sh
+++ cemu/recipe.sh
@@ -12,6 +12,9 @@ mv squashfs-root/apprun-hooks cemu/
 mv squashfs-root/usr/* cemu/
 rm -rf squashfs-root
 
+# This component appears to run fine with only this library included, so removing all others to save on space
+find cemu/lib/ -not -name 'libGLU.so.1' -delete
+
 cp component_launcher.sh manifest.json functions.sh prepare_component.sh cemu/
 chmod +x cemu/component_launcher.sh
 
