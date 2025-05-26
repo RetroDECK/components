950cc785cb106b229765c300c47473e57bfb42b0
XargonWan <xargonwan@gmail.com>
2025-05-26 11:54:23 +0900

    feat(recipe): update PPSSPP cheat database retrieval to use raw URL and save directly to work directory

--- ppsspp/recipe.sh
+++ ppsspp/recipe.sh
@@ -5,7 +5,6 @@ source "automation-tools/utils.sh"
 grab flatpak_id "org.ppsspp.PPSSPP"
 
 log i "Injecting PPSSPP cheat database..." "$logfile"
-wget https://github.com/Saramagrean/CWCheat-Database-Plus-/blob/master/cheat.db
-mv cheat.db "$component/artifacts/.tmp/"
+wget -O "$WORK_DIR/cheat.db" https://github.com/Saramagrean/CWCheat-Database-Plus-/raw/master/cheat.db
 
 finalize
