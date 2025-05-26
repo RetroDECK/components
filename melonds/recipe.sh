a9dac086b882e944264fad35c69f430cf03adc99
XargonWan <xargonwan@gmail.com>
2025-05-26 10:38:01 +0900

    feat(recipe): simplify melonds artifact retrieval process

--- melonds/recipe.sh
+++ melonds/recipe.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-wget https://github.com/RetroDECK/net.kuribo64.melonDS/releases/latest/download/RetroDECK-melonds-Artifact.tar.gz
+source "automation-tools/utils.sh"
 
-mkdir melonds-tmp
+grab flatpak_artifacts "https://github.com/RetroDECK/net.kuribo64.melonDS/releases/latest/download/RetroDECK-melonds-Artifact.tar.gz"
 
-mkdir melonds
-
-tar -xzf RetroDECK-melonds-Artifact.tar.gz -C melonds-tmp
-
-mv melonds-tmp/files/bin/ melonds/
-mv melonds-tmp/files/lib/ melonds/
-mv melonds-tmp/files/share/ melonds/
-
-cp component_launcher.sh manifest.json functions.sh prepare_component.sh melonds/
-chmod +x melonds/component_launcher.sh
-
-tar -czf "melonds-artifact.tar.gz" "melonds"
-
-rm -rf melonds-tmp
-rm -rf melonds
+finalize
