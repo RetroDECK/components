f33cb59c44ec1f7b2b74e9901ff25eda2879c3c9
XargonWan <xargonwan@gmail.com>
2025-05-27 15:01:29 +0900

    feat(ppsspp): enhance recipe script to download PPSSPP BIOS and cheat database

--- ppsspp/recipe.sh
+++ ppsspp/recipe.sh
@@ -4,7 +4,17 @@ source "automation-tools/utils.sh"
 
 grab flatpak_id "org.ppsspp.PPSSPP"
 
-log i "Injecting PPSSPP cheat database..." "$logfile"
+# PPSSPP BIOS
+log i "Downloading PPSSPP BIOS..." "$logfile"
+wget "https://github.com/hrydgard/ppsspp/archive/refs/heads/master.zip" -O "$WORK_DIR/ppsspp-bios.zip"
+unzip -o "$WORK_DIR/ppsspp-bios.zip" -d "$WORK_DIR/ppsspp-bios"
+mkdir -p "$WORK_DIR/ppsspp-bios/assets/PPSSPP"
+cp -rf "$WORK_DIR/ppsspp-bios/ppsspp-master/assets/PPSSPP"/* "$WORK_DIR/ppsspp-bios/assets/PPSSPP/"
+rm -f "$WORK_DIR/ppsspp-bios.zip"
+rm -rf "$WORK_DIR/ppsspp-bios"
+
+# PPSSPP Cheats
+log i "Downloading PPSSPP cheat database..." "$logfile"
 wget -O "$WORK_DIR/cheat.db" https://github.com/Saramagrean/CWCheat-Database-Plus-/raw/master/cheat.db
 
 finalize
