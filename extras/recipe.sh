1b3119e926f64a1dda62bafe02be5db4e6f60c60
XargonWan <xargonwan@gmail.com>
2025-05-27 15:00:48 +0900

    cleaunp(recipe): remove deprecated recipe script for the exztras component

--- extras/recipe.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source "automation-tools/utils.sh"
-
-# Universal Dynamic Input for Dolphin and Primehack
-git clone --depth 1 "https://github.com/Venomalia/UniversalDynamicInput.git" "$WORK_DIR/DynamicInputTextures"
-
-finalize
