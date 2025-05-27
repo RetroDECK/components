30fa1aed5e310a060187848f43c6221d55aaba3e
XargonWan <xargonwan@gmail.com>
2025-05-27 15:00:56 +0900

    feat(dolphin): add logging and cloning for Universal Dynamic Input textures

--- dolphin/recipe.sh
+++ dolphin/recipe.sh
@@ -6,4 +6,8 @@ grab flatpak_id org.DolphinEmu.dolphin-emu
 
 # custom commands goes here
 
+# Universal Dynamic Input for Dolphin and Primehack
+log i "Downloading Universal Dynamic Input textures for Dolphin and PrimeHack..." "$logfile"
+git clone --depth 1 "https://github.com/Venomalia/UniversalDynamicInput.git" "$WORK_DIR/DynamicInputTextures"
+
 finalize
