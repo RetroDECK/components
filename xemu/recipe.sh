da061091c3857bfa73c412ce3c8e78abf96adfba
XargonWan <xargonwan@gmail.com>
2025-05-27 15:01:42 +0900

    feat(xemu): add download and extraction steps for XEMU dummy HDD

--- xemu/recipe.sh
+++ xemu/recipe.sh
@@ -4,8 +4,13 @@ source "automation-tools/utils.sh"
 
 grab appimage "https://github.com/xemu-project/xemu/releases/latest/download/xemu-*-x86_64.AppImage"
 
-# custom commnads goes here
+# custom commands goes here
 
-# TODO: We need XBOX HDD, check main manifest
+# xemu Dummy Hdd
+log i "Downloading XEMU dummy HDD..." "$logfile"
+wget "https://github.com/mborgerson/xemu-hdd-image/releases/latest/download/xbox_hdd.qcow2.zip" -O "$WORK_DIR/xbox_hdd.qcow2.zip"
+unzip -o "$WORK_DIR/xbox_hdd.qcow2.zip" -d "$WORK_DIR/"
+cp -f "$WORK_DIR/xbox_hdd.qcow2" "$WORK_DIR/extras/xbox_hdd.qcow2"
+rm -f "$WORK_DIR/xbox_hdd.qcow2.zip"
 
 finalize
