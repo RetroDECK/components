73d88e72fc9f8c2b90ef508025701852fd43f46a
XargonWan <xargonwan@gmail.com>
2025-05-27 15:01:09 +0900

    feat(recipe): add theme downloading functionality to ES-DE

--- es-de/recipe.sh
+++ es-de/recipe.sh
@@ -6,4 +6,9 @@ grab flatpak_artifacts "https://github.com/RetroDECK/ES-DE/releases/latest/downl
 
 # custom commnads goes here
 
+# TODO: adapt the paths to the new NEO pathing scheme
+# RetroDECK Theme
+log i "Downloading RetroDECK theme..." "$logfile"
+git clone --depth 1 "https://github.com/RetroDECK/RetroDECK-theme" "$WORK_DIR/themes"
+
 finalize
