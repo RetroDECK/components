d69810fe0c0d9fee282cb8fabbd0fc5341c2b023
XargonWan <xargonwan@gmail.com>
2025-05-29 11:19:26 +0900

    fix(recipe): correct path to YAML files in flatpak-builder commands

--- shared-libs/recipe.sh
+++ shared-libs/recipe.sh
@@ -9,13 +9,13 @@ component="shared-libs"
 version=""                  # Needed for finalize function
 
 mkdir -p $WORK_DIR/artifacts/qt-67
-flatpak-builder --user --force-clean --install-deps-from=flathub --install-deps-from=flathub-beta --repo=$component/shared-libs-67-repo "$component/shared-libs-67-build-dir" "retrodeck.shared-libs.67.yml"
+flatpak-builder --user --force-clean --install-deps-from=flathub --install-deps-from=flathub-beta --repo=$component/shared-libs-67-repo "$component/shared-libs-67-build-dir" "$component/retrodeck.shared-libs.67.yml"
 mv $component/shared-libs-67-build-dir/files/lib/* $WORK_DIR/artifacts/qt-67/
 rm -rf $component/shared-libs-67-build-dir $WORK_DIR/shared-libs-67-repo
 version="6.7"
 
 mkdir -p $WORK_DIR/artifacts/qt-68
-flatpak-builder --user --force-clean --install-deps-from=flathub --install-deps-from=flathub-beta --repo=$component/shared-libs-68-repo "$component/shared-libs-68-build-dir" "retrodeck.shared-libs.68.yml"
+flatpak-builder --user --force-clean --install-deps-from=flathub --install-deps-from=flathub-beta --repo=$component/shared-libs-68-repo "$component/shared-libs-68-build-dir" "$component/retrodeck.shared-libs.68.yml"
 mv $component/shared-libs-68-build-dir/files/lib/* $WORK_DIR/artifacts/qt-68/
 rm -rf $component/shared-libs-68-build-dir $WORK_DIR/shared-libs-68-repo
 version="$version, 6.8"
