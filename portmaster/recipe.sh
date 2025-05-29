811657c431101bc6f735e79dc39baa945d0deff1
XargonWan <xargonwan@gmail.com>
2025-05-29 12:00:55 +0900

    fix(recipe): update paths for PortMaster artifacts and launcher

--- portmaster/recipe.sh
+++ portmaster/recipe.sh
@@ -3,18 +3,18 @@
 # This recipe differs fomr the standard as PortMaster needs a special setup
 
 source "automation-tools/assembler.sh"
-WORK_DIR=$(mktemp -d)
+
 component="portmaster"
 version="stable"
 
-wget -qc "https://github.com/PortsMaster/PortMaster-GUI/releases/latest/download/retrodeck.portmaster.zip" -O "$WORK_DIR/PortMaster.zip"
+wget -qc "https://github.com/PortsMaster/PortMaster-GUI/releases/latest/download/retrodeck.portmaster.zip" -O "$component/artifacts/PortMaster.zip"
 
 # Adding an additional launcher for harbourmaster
-echo '#!/bin/bash' > "$WORK_DIR/harbourmaster"
-echo "\"/var/data/PortMaster/harbourmaster\" \"$@\"" >> "$WORK_DIR/harbourmaster"
-chmod +x "$WORK_DIR/harbourmaster"
+echo '#!/bin/bash' > "$component/artifacts/harbourmaster"
+echo "\"/var/data/PortMaster/harbourmaster\" \"$@\"" >> "$component/artifacts/harbourmaster"
+chmod +x "$component/artifacts/harbourmaster"
 
-log d "Listing WORK_DIR ($WORK_DIR)"
-ls -lah "$WORK_DIR"
+log d "Listing WORK_DIR ($component/artifacts)"
+ls -lah "$component/artifacts"
 
 finalize
