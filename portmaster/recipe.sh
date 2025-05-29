6cc95810125e3cfc350e8dc4009f1020f6325ae3
XargonWan <xargonwan@gmail.com>
2025-05-29 11:02:14 +0900

    fix(portmaster): ensure WORK_DIR is created before downloading files

--- portmaster/recipe.sh
+++ portmaster/recipe.sh
@@ -3,6 +3,7 @@
 # This recipe differs fomr the standard as PortMaster needs a special setup
 
 source "automation-tools/assembler.sh"
+WORK_DIR=$(mktemp -d)
 
 wget -qc "https://github.com/PortsMaster/PortMaster-GUI/releases/latest/download/retrodeck.portmaster.zip" -O "$WORK_DIR/$EXTRAS/PortMaster.zip"
 
