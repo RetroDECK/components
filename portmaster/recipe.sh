70acfb2eac6f20a9564d0fcbf40d461d82d083be
XargonWan <xargonwan@gmail.com>
2025-05-29 11:42:37 +0900

    fix(recipe): correct file paths for PortMaster launcher and zip download

--- portmaster/recipe.sh
+++ portmaster/recipe.sh
@@ -4,12 +4,16 @@
 
 source "automation-tools/assembler.sh"
 WORK_DIR=$(mktemp -d)
+component="portmaster"
 
-wget -qc "https://github.com/PortsMaster/PortMaster-GUI/releases/latest/download/retrodeck.portmaster.zip" -O "$WORK_DIR/$EXTRAS/PortMaster.zip"
+wget -qc "https://github.com/PortsMaster/PortMaster-GUI/releases/latest/download/retrodeck.portmaster.zip" -O "$WORK_DIR/PortMaster.zip"
 
 # Adding an additional launcher for harbourmaster
-echo '#!/bin/bash' > "$WORK_DIR/$EXTRAS/harbourmaster"
-echo "\"/var/data/PortMaster/harbourmaster\" \"$@\"" >> "$WORK_DIR/$EXTRAS/harbourmaster"
-chmod +x "$WORK_DIR/$EXTRAS/harbourmaster"
+echo '#!/bin/bash' > "$WORK_DIR/harbourmaster"
+echo "\"/var/data/PortMaster/harbourmaster\" \"$@\"" >> "$WORK_DIR/harbourmaster"
+chmod +x "$WORK_DIR/harbourmaster"
+
+log d "Listing WORK_DIR ($WORK_DIR)"
+ls -lah "$WORK_DIR"
 
 finalize
