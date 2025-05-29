046d480b5f9e58f6ed794399a12bb0e97ec4ad3e
XargonWan <xargonwan@gmail.com>
2025-05-29 15:48:31 +0900

    feat(components): add component_launcher script for solarus integration

---
+++ solarus/component_launcher.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+COMPONENT_NAME="solarus"
+RD_MODULES="/app/retrodeck/components"
+
+LD_LIBRARY_PATH="$RD_MODULES/$COMPONENT_NAME/lib:${LD_LIBRARY_PATH}"
+
+exec "$RD_MODULES/$COMPONENT_NAME/bin/solarus-launcher" "$@"
