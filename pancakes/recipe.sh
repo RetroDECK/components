925ffae23657bcf13d42203b753142ff2cd3b6ba
XargonWan <xargonwan@gmail.com>
2025-05-29 11:12:41 +0900

    fix(recipe): replace move with copy for files from publish directory

--- pancakes/recipe.sh
+++ pancakes/recipe.sh
@@ -4,9 +4,12 @@ source "automation-tools/assembler.sh"
 
 grab generic "https://github.com/RetroDECK/Pancakes-bin/releases/latest/download/pancakes-Release-linux_x64.tar.gz"
 rm -rf pancakes.tar.gz
-shopt -s dotglob
-mv "$WORK_DIR/publish/"* "$WORK_DIR/"
-shopt -u dotglob
-rm -rf publish
+
+# Move the files from publish folder to work directory
+cp -rL "$WORK_DIR/publish/"* "$WORK_DIR/" || {
+        echo "ERROR: Failed to copy files from publish/"
+        exit 1
+    }
+rm -rf "$WORK_DIR/publish"
 
 finalize
