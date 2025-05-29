85f2b8fae98a1a7f70c6b60fac8647223f96aa1c
XargonWan <xargonwan@gmail.com>
2025-05-29 09:29:15 +0900

    fix(xemu): correct variable usage for extras directory in recipe script

--- xemu/recipe.sh
+++ xemu/recipe.sh
@@ -10,7 +10,7 @@ grab appimage "https://github.com/xemu-project/xemu/releases/latest/download/xem
 log i "Downloading XEMU dummy HDD..." "$logfile"
 wget "https://github.com/mborgerson/xemu-hdd-image/releases/latest/download/xbox_hdd.qcow2.zip" -O "$WORK_DIR/xbox_hdd.qcow2.zip"
 unzip -o "$WORK_DIR/xbox_hdd.qcow2.zip" -d "$WORK_DIR/"
-cp -f "$WORK_DIR/xbox_hdd.qcow2" "$WORK_DIR/extras/xbox_hdd.qcow2"
+cp -f "$WORK_DIR/xbox_hdd.qcow2" "$WORK_DIR/$$EXTRAS/xbox_hdd.qcow2"
 rm -f "$WORK_DIR/xbox_hdd.qcow2.zip"
 
 finalize
