6bc32bc2833c4fecce7bc74c112ed2daf9b68acc
XargonWan <xargonwan@gmail.com>
2025-05-30 14:41:18 +0900

    fix(components): correct library and plugin paths in component_launcher script

--- mame/component_launcher.sh
+++ mame/component_launcher.sh
@@ -2,7 +2,7 @@
 
 RD_MODULES="/app/retrodeck/components"
 
-LD_LIBRARY_PATH="$RD_MODULES/mame/lib:${LD_LIBRARY_PATH}"
-export QT_PLUGIN_PATH="/app/retrodeck/components/shared_libs/qt-6.8/plugins:${QT_PLUGIN_PATH}"
+LD_LIBRARY_PATH="$RD_MODULES/mame/lib:/app/retrodeck/components/shared-libs/qt-6.8/lib:${LD_LIBRARY_PATH}"
+export QT_PLUGIN_PATH="/app/retrodeck/components/shared-libs/qt-6.8/lib/plugins:${QT_PLUGIN_PATH}"
 
 exec "$RD_MODULES/mame/bin/mame" "$@"
