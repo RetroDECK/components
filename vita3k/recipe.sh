8bc42e7cb98c6293647a1566d1d38e6f73a74bce
Lazorne <thelazorne@protonmail.com>
2025-05-30 07:50:10 +0000

    Update recipe.sh
    x

--- vita3k/recipe.sh
+++ vita3k/recipe.sh
@@ -4,6 +4,6 @@ source "automation-tools/assembler.sh"
 
 grab appimage "https://github.com/Vita3K/Vita3K/releases/download/continuous/Vita3K-x86_64.AppImage"
 
-# custom commnads goes here
+# Custom Commands
 
 finalize
