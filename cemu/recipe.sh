e29ef5012d07dd4aefa3f0a56fdf67425ca07d1c
Lazorne <thelazorne@protonmail.com>
2025-05-30 07:54:17 +0000

    Update recipe.sh

--- cemu/recipe.sh
+++ cemu/recipe.sh
@@ -4,7 +4,7 @@ source "automation-tools/assembler.sh"
 
 grab appimage "https://github.com/cemu-project/Cemu/releases/latest/download/Cemu-*-x86_64.AppImage"
 
-# custom commands go here
+# Custom Commands
 
 # This component appears to run fine with only this library included, so removing all others to save on space
 find cemu/lib/ -not -name 'libGLU.so.1' -delete
