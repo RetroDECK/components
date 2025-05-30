89b3608c2b0eaa6e2d3fd82a9309a8b44963ac14
Lazorne <thelazorne@protonmail.com>
2025-05-30 07:51:47 +0000

    Update recipe.sh

--- primehack/recipe.sh
+++ primehack/recipe.sh
@@ -4,6 +4,6 @@ source "automation-tools/assembler.sh"
 
 grab flatpak_artifacts "https://github.com/RetroDECK/io.github.shiiion.primehack/releases/latest/download/RetroDECK-primehack-Artifact.tar.gz"
 
-# custom commnads goes here
+# Custom Commands
 
 finalize
