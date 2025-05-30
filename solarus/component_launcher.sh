fd948f1914d99d7a5d1c2875e325d254aff1516a
XargonWan <xargonwan@gmail.com>
2025-05-30 14:41:28 +0900

    fix(components): update library and plugin paths in component_launcher script

--- solarus/component_launcher.sh
+++ solarus/component_launcher.sh
@@ -3,7 +3,7 @@
 COMPONENT_NAME="solarus"
 RD_MODULES="/app/retrodeck/components"
 
-LD_LIBRARY_PATH="$RD_MODULES/$COMPONENT_NAME/lib:${LD_LIBRARY_PATH}"
-export QT_PLUGIN_PATH="/app/retrodeck/components/shared_libs/qt-6.8/plugins:${QT_PLUGIN_PATH}"
+LD_LIBRARY_PATH="$RD_MODULES/$COMPONENT_NAME/lib:/app/retrodeck/components/shared-libs/qt-5.15/lib:${LD_LIBRARY_PATH}"
+export QT_PLUGIN_PATH="/app/retrodeck/components/shared-libs/qt-5.15/lib/plugins:${QT_PLUGIN_PATH}"
 
 exec "$RD_MODULES/$COMPONENT_NAME/bin/solarus-launcher" "$@"
