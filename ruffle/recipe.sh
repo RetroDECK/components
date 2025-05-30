956373c52854963f8e5ac6ed973d9444460422ac
Lazorne <thelazorne@protonmail.com>
2025-05-30 07:50:51 +0000

    Update recipe.sh

--- ruffle/recipe.sh
+++ ruffle/recipe.sh
@@ -4,6 +4,6 @@ source "automation-tools/assembler.sh"
 
 grab generic "https://github.com/ruffle-rs/ruffle/releases/latest/download/ruffle-nightly-*-linux-x86_64.tar.gz"
 
-# custom commnads goes here
+# Custom Commands
 
 finalize
