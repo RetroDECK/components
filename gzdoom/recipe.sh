38f347e2fd0c12be19f3978dde082c618a54ac4b
Lazorne <thelazorne@protonmail.com>
2025-05-30 07:53:42 +0000

    Update recipe.sh

--- gzdoom/recipe.sh
+++ gzdoom/recipe.sh
@@ -4,6 +4,6 @@ source "automation-tools/assembler.sh"
 
 grab appimage "https://github.com/flegald/GZDeck/releases/latest/download/GZDeck*.AppImage"
 
-# custom commands go here
+# Custom Commands
 
 finalize
