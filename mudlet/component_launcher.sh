e221210bcfdf01ecc205751ab413d70887873bee
Lazorne <thelazorne@protonmail.com>
2025-06-02 13:33:37 +0200

    mudlet fix

--- mudlet/component_launcher.sh
+++ mudlet/component_launcher.sh
@@ -9,7 +9,8 @@ SHARED_LIBS="/app/retrodeck/components/shared-libs/qt-5.15/lib"
 # This ensures the application can find its resources
 export APPDIR="$RD_MODULES/$COMPONENT_NAME"
 
-LD_LIBRARY_PATH="$RD_MODULES/$COMPONENT_NAME/lib:${LD_LIBRARY_PATH}"
+LD_LIBRARY_PATH="$RD_MODULES/$COMPONENT_NAME/lib:$SHARED_LIBS:${LD_LIBRARY_PATH}"
+export QT_PLUGIN_PATH="$SHARED_LIBS/plugins:${QT_PLUGIN_PATH}"
 
 log i "RetroDECK is now launching $COMPONENT_NAME"
 log d "Library path is: $LD_LIBRARY_PATH"
