a37f161adfeb1ecedfe96ecbc4f84297d86e01a0
XargonWan <xargonwan@gmail.com>
2025-06-02 15:32:50 +0900

    feat(prepare_component): add RetroArch preparation script with enhanced backup and configuration handling + recipe refactor to include all the RetroArch components

---
+++ retroarch/prepare_component.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+if [[ "$component" =~ ^(retroarch|all)$ ]]; then
+    component_found="true"
+    extras="/app/retrodeck/retroarch/rd_extras"
+    log i "--------------------------------"
+    log i "Prepearing RetroArch"
+    log i "--------------------------------"
+    if [[ "$action" == "reset" ]]; then # Run reset-only commands
+    if [[ $multi_user_mode == "true" ]]; then # Multi-user actions
+        create_dir -d "$multi_user_data_folder/$SteamAppUser/config/retroarch"
+        cp -fv "$config/retroarch/retroarch.cfg" "$multi_user_data_folder/$SteamAppUser/config/retroarch/"
+        cp -fv "$config/retroarch/retroarch-core-options.cfg" "$multi_user_data_folder/$SteamAppUser/config/retroarch/"
+    else # Single-user actions
+        create_dir -d "$XDG_CONFIG_HOME/retroarch"
+        dir_prep "$bios_folder" "$XDG_CONFIG_HOME/retroarch/system"
+        dir_prep "$rdhome/logs/retroarch" "$XDG_CONFIG_HOME/retroarch/logs"
+        create_dir -d "$XDG_CONFIG_HOME/retroarch/shaders/"
+        if [[ -d "$cheats_folder/retroarch" && "$(ls -A "$cheats_folder/retroarch")" ]]; then
+        backup_file="$backups_folder/cheats/retroarch-$(date +%y%m%d).tar.gz"
+        create_dir "$(dirname "$backup_file")"
+        tar -czf "$backup_file" -C "$cheats_folder" retroarch
+        log i "RetroArch cheats backed up to $backup_file"
+        fi
+        create_dir -d "$cheats_folder/retroarch"
+        tar --strip-components=1 -xzf "/app/retrodeck/cheats/retroarch.tar.gz" -C "$cheats_folder/retroarch" --overwrite
+        cp -rf "/app/share/libretro/shaders" "$XDG_CONFIG_HOME/retroarch/"
+        dir_prep "$shaders_folder/retroarch" "$XDG_CONFIG_HOME/retroarch/shaders"
+        cp -fv "$config/retroarch/retroarch.cfg" "$XDG_CONFIG_HOME/retroarch/"
+        cp -fv "$config/retroarch/retroarch-core-options.cfg" "$XDG_CONFIG_HOME/retroarch/"
+        rsync -rlD --mkpath "$config/retroarch/core-overrides/" "$XDG_CONFIG_HOME/retroarch/config/"
+        rsync -rlD --mkpath "$config/retrodeck/presets/remaps/" "$XDG_CONFIG_HOME/retroarch/config/remaps/"
+        dir_prep "$borders_folder" "$XDG_CONFIG_HOME/retroarch/overlays/borders"
+        set_setting_value "$raconf" "savefile_directory" "$saves_folder" "retroarch"
+        set_setting_value "$raconf" "savestate_directory" "$states_folder" "retroarch"
+        set_setting_value "$raconf" "screenshot_directory" "$screenshots_folder" "retroarch"
+        set_setting_value "$raconf" "log_dir" "$logs_folder" "retroarch"
+        set_setting_value "$raconf" "rgui_browser_directory" "$roms_folder" "retroarch"
+        set_setting_value "$raconf" "cheat_database_path" "$cheats_folder/retroarch" "retroarch"
+    fi
+    # Shared actions
+
+    create_dir "$bios_folder/np2kai"
+    create_dir "$bios_folder/dc"
+    create_dir "$bios_folder/Mupen64plus"
+    create_dir "$bios_folder/quasi88"
+
+    retroarch_updater
+
+    # FBNEO
+    log i "--------------------------------"
+    log i "Prepearing FBNEO_LIBRETRO"
+    log i "--------------------------------"
+    create_dir "$bios_folder/fbneo/samples"
+    # TODO: cheats support
+    create_dir "$bios_folder/fbneo/cheats"
+    create_dir "$bios_folder/fbneo/blend"
+    dir_prep "$mods_folder/FBNeo" "$bios_folder/fbneo/patched"
+
+    # PPSSPP
+    log i "--------------------------------"
+    log i "Prepearing PPSSPP_LIBRETRO"
+    log i "--------------------------------"
+    if [ -d "$bios_folder/PPSSPP/flash0/font" ]
+    then
+        mv -fv "$bios_folder/PPSSPP/flash0/font" "$bios_folder/PPSSPP/flash0/font.bak"
+    fi
+        cp -rf "$extras/PPSSPP" "$bios_folder/PPSSPP"
+    if [ -d "$bios_folder/PPSSPP/flash0/font.bak" ]
+    then
+        mv -f "$bios_folder/PPSSPP/flash0/font.bak" "$bios_folder/PPSSPP/flash0/font"
+    fi
+
+    # MSX / SVI / ColecoVision / SG-1000
+    log i "-----------------------------------------------------------"
+    log i "Prepearing MSX / SVI / ColecoVision / SG-1000 LIBRETRO"
+    log i "-----------------------------------------------------------"
+    log i "Copying \"$extras/MSX/Databases\" in \"$bios_folder/Databases\""
+    cp -rf "$extras/MSX/Databases" "$bios_folder/Databases"
+    log i "Copying \"$extras/MSX/Machines\" in \"$bios_folder/Machines\""
+    cp -rf "$extras/MSX/Machines" "$bios_folder/Machines"
+
+    # AMIGA
+    log i "-----------------------------------------------------------"
+    log i "Prepearing AMIGA LIBRETRO"
+    log i "-----------------------------------------------------------"
+    log i "Copying \"$extras/Amiga/capsimg.so\" in \"$bios_folder/capsimg.so\""
+    cp -f "$extras/Amiga/capsimg.so" "$bios_folder/capsimg.so"
+
+    # ScummVM
+    log i "-----------------------------------------------------------"
+    log i "Prepearing ScummVM LIBRETRO"
+    log i "-----------------------------------------------------------"
+    cp -fv "$config/retroarch/scummvm.ini" "$ra_scummvm_conf"
+    create_dir "$mods_folder/RetroArch/ScummVM/icons"
+    log i "Installing ScummVM assets"
+    unzip -o "$config/retroarch/ScummVM.zip" 'scummvm/extra/*' -d /tmp
+    unzip -o "$config/retroarch/ScummVM.zip" 'scummvm/theme/*' -d /tmp
+    mv -f /tmp/scummvm/extra "$mods_folder/RetroArch/ScummVM"
+    mv -f /tmp/scummvm/theme "$mods_folder/RetroArch/ScummVM"
+    rm -rf /tmp/extra /tmp/theme
+    set_setting_value "$ra_scummvm_conf" "iconspath" "$mods_folder/RetroArch/ScummVM/icons" "libretro_scummvm" "scummvm"
+    set_setting_value "$ra_scummvm_conf" "extrapath" "$mods_folder/RetroArch/ScummVM/extra" "libretro_scummvm" "scummvm"
+    set_setting_value "$ra_scummvm_conf" "themepath" "$mods_folder/RetroArch/ScummVM/theme" "libretro_scummvm" "scummvm"
+    set_setting_value "$ra_scummvm_conf" "savepath" "$saves_folder/scummvm" "libretro_scummvm" "scummvm"
+    set_setting_value "$ra_scummvm_conf" "browser_lastpath" "$roms_folder/scummvm" "libretro_scummvm" "scummvm"
+
+    dir_prep "$texture_packs_folder/RetroArch-Mesen" "$XDG_CONFIG_HOME/retroarch/system/HdPacks"
+    dir_prep "$texture_packs_folder/RetroArch-Mupen64Plus/cache" "$XDG_CONFIG_HOME/retroarch/system/Mupen64plus/cache"
+    dir_prep "$texture_packs_folder/RetroArch-Mupen64Plus/hires_texture" "$XDG_CONFIG_HOME/retroarch/system/Mupen64plus/hires_texture"
+
+    # Reset default preset settings
+    set_setting_value "$rd_conf" "retroarch" "$(get_setting_value "$rd_defaults" "retroarch" "retrodeck" "cheevos")" "retrodeck" "cheevos"
+    set_setting_value "$rd_conf" "retroarch" "$(get_setting_value "$rd_defaults" "retroarch" "retrodeck" "cheevos_hardcore")" "retrodeck" "cheevos_hardcore"
+    set_setting_value "$rd_conf" "gb" "$(get_setting_value "$rd_defaults" "gb" "retrodeck" "borders")" "retrodeck" "borders"
+    set_setting_value "$rd_conf" "gba" "$(get_setting_value "$rd_defaults" "gba" "retrodeck" "borders")" "retrodeck" "borders"
+    set_setting_value "$rd_conf" "gbc" "$(get_setting_value "$rd_defaults" "gbc" "retrodeck" "borders")" "retrodeck" "borders"
+    set_setting_value "$rd_conf" "genesis" "$(get_setting_value "$rd_defaults" "genesis" "retrodeck" "borders")" "retrodeck" "borders"
+    set_setting_value "$rd_conf" "gg" "$(get_setting_value "$rd_defaults" "gg" "retrodeck" "borders")" "retrodeck" "borders"
+    set_setting_value "$rd_conf" "n64" "$(get_setting_value "$rd_defaults" "n64" "retrodeck" "borders")" "retrodeck" "borders"
+    set_setting_value "$rd_conf" "psx_ra" "$(get_setting_value "$rd_defaults" "psx_ra" "retrodeck" "borders")" "retrodeck" "borders"
+    set_setting_value "$rd_conf" "snes" "$(get_setting_value "$rd_defaults" "snes" "retrodeck" "borders")" "retrodeck" "borders"
+    set_setting_value "$rd_conf" "genesis" "$(get_setting_value "$rd_defaults" "genesis" "retrodeck" "widescreen")" "retrodeck" "widescreen"
+    set_setting_value "$rd_conf" "n64" "$(get_setting_value "$rd_defaults" "n64" "retrodeck" "widescreen")" "retrodeck" "widescreen"
+    set_setting_value "$rd_conf" "psx_ra" "$(get_setting_value "$rd_defaults" "psx_ra" "retrodeck" "widescreen")" "retrodeck" "widescreen"
+    set_setting_value "$rd_conf" "snes" "$(get_setting_value "$rd_defaults" "snes" "retrodeck" "widescreen")" "retrodeck" "widescreen"
+    set_setting_value "$rd_conf" "gb" "$(get_setting_value "$rd_defaults" "gb" "retrodeck" "abxy_button_swap")" "retrodeck" "abxy_button_swap"
+    set_setting_value "$rd_conf" "gba" "$(get_setting_value "$rd_defaults" "gba" "retrodeck" "abxy_button_swap")" "retrodeck" "abxy_button_swap"
+    set_setting_value "$rd_conf" "gbc" "$(get_setting_value "$rd_defaults" "gbc" "retrodeck" "abxy_button_swap")" "retrodeck" "abxy_button_swap"
+    set_setting_value "$rd_conf" "n64" "$(get_setting_value "$rd_defaults" "gb" "retrodeck" "abxy_button_swap")" "retrodeck" "abxy_button_swap"
+    set_setting_value "$rd_conf" "snes" "$(get_setting_value "$rd_defaults" "gba" "retrodeck" "abxy_button_swap")" "retrodeck" "abxy_button_swap"
+    set_setting_value "$rd_conf" "retroarch" "$(get_setting_value "$rd_defaults" "retroarch" "retrodeck" "savestate_auto_load")" "retrodeck" "savestate_auto_load"
+    set_setting_value "$rd_conf" "retroarch" "$(get_setting_value "$rd_defaults" "retroarch" "retrodeck" "savestate_auto_save")" "retrodeck" "savestate_auto_save"
+    fi
+    if [[ "$action" == "postmove" ]]; then # Run only post-move commands
+    dir_prep "$bios_folder" "$XDG_CONFIG_HOME/retroarch/system"
+    dir_prep "$logs_folder/retroarch" "$XDG_CONFIG_HOME/retroarch/logs"
+    dir_prep "$shaders_folder/retroarch" "$XDG_CONFIG_HOME/retroarch/shaders"
+    dir_prep "$texture_packs_folder/RetroArch-Mesen" "$XDG_CONFIG_HOME/retroarch/system/HdPacks"
+    dir_prep "$texture_packs_folder/RetroArch-Mupen64Plus/cache" "$XDG_CONFIG_HOME/retroarch/system/Mupen64plus/cache"
+    dir_prep "$texture_packs_folder/RetroArch-Mupen64Plus/hires_texture" "$XDG_CONFIG_HOME/retroarch/system/Mupen64plus/hires_texture"
+    set_setting_value "$raconf" "savefile_directory" "$saves_folder" "retroarch"
+    set_setting_value "$raconf" "savestate_directory" "$states_folder" "retroarch"
+    set_setting_value "$raconf" "screenshot_directory" "$screenshots_folder" "retroarch"
+    set_setting_value "$raconf" "log_dir" "$logs_folder" "retroarch"
+    fi
+fi
--- retroarch/recipe.sh
+++ retroarch/recipe.sh
@@ -2,9 +2,29 @@
 
 source "automation-tools/assembler.sh"
 
-grab appimage "https://buildbot.libretro.com/stable/*/linux/x86_64/RetroArch.7z"
+assemble appimage "https://buildbot.libretro.com/stable/*/linux/x86_64/RetroArch.7z"
 
 # Custom Commands
+
+artifacts_dir="$component/artifacts"
+
+# Unfortunately this is a one off as we need to extract even the assets from the same appimage
+wget "$url" -O "$WORK_DIR/RetroArch.7z"
+7z x "$WORK_DIR/RetroArch.7z" -o"$WORK_DIR"
+"$WORK_DIR/RetroArch-Linux-x86_64.AppImage" --appimage-extract
+mv "$WORK_DIR/RetroArch-Linux-x86_64/RetroArch-Linux-x86_64.AppImage.home/.config/retroarch/"* "$artifacts_dir/"
+
+# Citra Libretro Core
+# Citra is removed from the LibretroCores but is still available in Libretro 
+log i "Downloading Citra Libretro core..." "$logfile"
+wget "https://buildbot.libretro.com/nightly/linux/x86_64/latest/citra_libretro.so.zip" -O "$WORK_DIR/citra_libretro.so.zip"
+unzip -o "$WORK_DIR/citra_libretro.so.zip" -d "$artifacts_dir/cores/"
+
+# Sameduck Libretro Core
+# Sameduck is removed from the LibretroCores but is still available in Libretro
+log i "Downloading Sameduck Libretro core..." "$logfile"
+wget "https://buildbot.libretro.com/nightly/linux/x86_64/latest/sameduck_libretro.so.zip" -O "$WORK_DIR/sameduck_libretro.so.zip"
+unzip -o "$WORK_DIR/sameduck_libretro.so.zip" -d "$artifacts_dir/cores/"
 
 # Nightly Cores
 # As some comres are not available in the stable version, we need to download the nightly cores
@@ -12,54 +32,28 @@ grab appimage "https://buildbot.libretro.com/stable/*/linux/x86_64/RetroArch.7z"
 log i "Downloading RetroArch nightly cores..." "$logfile"
 wget "https://buildbot.libretro.com/nightly/linux/x86_64/RetroArch_cores.7z" -O "$WORK_DIR/RetroArch_cores_nightly.7z"
 7z x "$WORK_DIR/RetroArch_cores_nightly.7z" -o"$WORK_DIR/cores_nightly"
-cp -rf "$WORK_DIR/cores_nightly/RetroArch-Linux-x86_64.AppImage.home/.config/retroarch/cores/"* "$WORK_DIR/cores/"
-rm -f "$WORK_DIR/RetroArch_cores_nightly.7z"
-rm -rf "$WORK_DIR/cores_nightly"
-
-# Citra Libretro Core
-# Citra is removed from the LibretroCores but is still available in Libretro 
-log i "Downloading Citra Libretro core..." "$logfile"
-wget "https://buildbot.libretro.com/nightly/linux/x86_64/latest/citra_libretro.so.zip" -O "$WORK_DIR/citra_libretro.so.zip"
-unzip -o "$WORK_DIR/citra_libretro.so.zip" -d "$WORK_DIR/cores/"
-rm -f "$WORK_DIR/citra_libretro.so.zip"
-
-# Sameduck Libretro Core
-# Sameduck is removed from the LibretroCores but is still available in Libretro
-log i "Downloading Sameduck Libretro core..." "$logfile"
-wget "https://buildbot.libretro.com/nightly/linux/x86_64/latest/sameduck_libretro.so.zip" -O "$WORK_DIR/sameduck_libretro.so.zip"
-unzip -o "$WORK_DIR/sameduck_libretro.so.zip" -d "$WORK_DIR/cores/"
-rm -f "$WORK_DIR/sameduck_libretro.so.zip"
-
-# Retroarch Cores
-log i "Downloading RetroArch cores..." "$logfile"
-wget "https://buildbot.libretro.com/stable/1.20.0/linux/x86_64/RetroArch_cores.7z" -O $WORK_DIR/RetroArch_cores.7z
-7z x $WORK_DIR/RetroArch_cores.7z -o"$WORK_DIR/cores/"
-rm -f $WORK_DIR/RetroArch_cores.7z
+# Without overwriting the existent cores
+cp -rn "$WORK_DIR/cores_nightly/RetroArch-Linux-x86_64/RetroArch-Linux-x86_64.AppImage.home/.config/retroarch/cores/"* "$artifacts_dir/cores/"
 
 # RetroArch Cheats
 log i "Downloading RetroArch cheats..." "$logfile"
 git clone --depth 1 "https://github.com/libretro/libretro-database.git" "$WORK_DIR/libretro-database"
-mkdir -p "$WORK_DIR/cht"
-cp -rf "$WORK_DIR/libretro-database/cht"/* "$WORK_DIR/cht/"
-rm -rf "$WORK_DIR/libretro-database"
+mkdir -p "$artifacts_dir/cht"
+cp -rf "$WORK_DIR/libretro-database/cht"/* "$artifacts_dir/cht/"
 
 # MSX BIOS
 log i "Downloading MSX BIOS..." "$logfile"
 wget "http://bluemsx.msxblue.com/rel_download/blueMSXv282full.zip" -O "$WORK_DIR/bluemsx.zip"
 unzip -o "$WORK_DIR/bluemsx.zip" -d "$WORK_DIR/bluemsx"
-mkdir -p "$WORK_DIR/$EXTRAS/MSX"
-cp -rf "$WORK_DIR/bluemsx/blueMSXv282full/Machines" "$WORK_DIR/$EXTRAS/MSX/"
-cp -rf "$WORK_DIR/bluemsx/blueMSXv282full/Databases" "$WORK_DIR/$EXTRAS/MSX/"
-rm -rf "$WORK_DIR/bluemsx/blueMSXv282full"
-rm -f "$WORK_DIR/bluemsx.zip"
+mkdir -p "$artifacts_dir/$EXTRAS/MSX"
+cp -rf "$WORK_DIR/bluemsx/Machines" "$artifacts_dir/$EXTRAS/MSX/"
+cp -rf "$WORK_DIR/bluemsx/Databases" "$artifacts_dir/$EXTRAS/MSX/"
 
 # Amiga BIOS
 log i "Downloading Amiga BIOS..." "$logfile"
 wget "https://github.com/rsn8887/capsimg/releases/download/1.1/Capsimg_for_Retroarch.zip" -O "$WORK_DIR/capsimg.zip"
 unzip -o "$WORK_DIR/capsimg.zip" -d "$WORK_DIR/capsimg"
-mkdir -p "$WORK_DIR/$EXTRAS/Amiga"
-cp -r "$WORK_DIR/capsimg/Linux/x86-64/capsimg.so" "$WORK_DIR/$EXTRAS/Amiga/capsimg.so"
-rm -rf "$WORK_DIR/capsimg"
-rm -f "$WORK_DIR/capsimg.zip"
+mkdir -p "$artifacts_dir/$EXTRAS/Amiga"
+cp -r "$WORK_DIR/capsimg/Linux/x86-64/capsimg.so" "$artifacts_dir/$EXTRAS/Amiga/capsimg.so"
 
 finalize
