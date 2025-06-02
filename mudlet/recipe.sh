3e408fe787b4c180e7322b9cd214481f610b5109
Lazorne <thelazorne@protonmail.com>
2025-06-02 15:21:15 +0200

    mudlet trying nightly tar

--- mudlet/recipe.sh
+++ mudlet/recipe.sh
@@ -2,7 +2,7 @@
 
 source "automation-tools/assembler.sh"
 
-assemble appimage "https://www.mudlet.org/download/42/"
+assemble appimage "https://make.mudlet.org/snapshots/ac8fcb/Mudlet-4.19.1-ptb-2025-06-02-52436899-linux-x64.AppImage.tar"
 
 # Custom Commands
 
