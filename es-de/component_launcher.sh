73f481fc042ea1b204842477fc5605a67951b29a
XargonWan <xargonwan@gmail.com>
2025-06-02 11:43:59 +0900

    fix(component_launcher): refactor to use COMPONENT_NAME variable for paths

--- es-de/component_launcher.sh
+++ es-de/component_launcher.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+COMPONENT_NAME="es-de"
 RD_MODULES="/app/retrodeck/components"
+SHARED_LIBS="/app/retrodeck/components/shared-libs/qt-6.7/lib"
 
-LD_LIBRARY_PATH="$RD_MODULES/es-de/lib:${LD_LIBRARY_PATH}"
+LD_LIBRARY_PATH="$RD_MODULES/$COMPONENT_NAME/lib:$SHARED_LIBS:${LD_LIBRARY_PATH}"
+export QT_PLUGIN_PATH="$SHARED_LIBS/plugins:${QT_PLUGIN_PATH}"
 
-exec "$RD_MODULES/es-de/bin/es-de" "$@"
+exec "$RD_MODULES/$COMPONENT_NAME/bin/es-de" "$@"
