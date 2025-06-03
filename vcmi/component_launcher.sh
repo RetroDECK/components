4d5add4455710ba6aa0046e05f81cade9978441c
Lazorne <thelazorne@protonmail.com>
2025-06-03 16:00:58 +0200

    vcmi component launcher, added missing qtnetwork to 6.8 txt

--- vcmi/component_launcher.sh
+++ vcmi/component_launcher.sh
@@ -4,10 +4,13 @@ source /app/libexec/logger.sh
 
 COMPONENT_NAME="vcmi"
 RD_MODULES="/app/retrodeck/components"
+SHARED_LIBS="/app/retrodeck/components/shared-libs/qt-6.8/lib"
 
-LD_LIBRARY_PATH="$RD_MODULES/$COMPONENT_NAME/lib:${LD_LIBRARY_PATH}"
+LD_LIBRARY_PATH="$RD_MODULES/$COMPONENT_NAME/lib:$SHARED_LIBS:${LD_LIBRARY_PATH}"
+export QT_PLUGIN_PATH="$SHARED_LIBS/plugins:${QT_PLUGIN_PATH}"
 
 log i "RetroDECK is now launching $COMPONENT_NAME"
 log d "Library path is: $LD_LIBRARY_PATH"
+log d "AppDir is: $APPDIR"
 
 exec "$RD_MODULES/$COMPONENT_NAME/bin/vcmilauncher" "$@"
