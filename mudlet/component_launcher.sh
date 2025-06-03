e1c9e47d1cab83984ac93098af77f427c0f1f75f
Lazorne <thelazorne@protonmail.com>
2025-06-03 15:28:53 +0200

    vcmi experiment

--- mudlet/component_launcher.sh
+++ mudlet/component_launcher.sh
@@ -17,4 +17,5 @@ log d "Library path is: $LD_LIBRARY_PATH"
 log d "AppDir is: $APPDIR"
 
 # NOTE: AppRun is not working for Mudlet
-exec "$RD_MODULES/$COMPONENT_NAME/mudlet" "$@"
+exec "$RD_MODULES/$COMPONENT_NAME/mudlet --steammode" "$@"
+
---
+++ vcmi/recipe.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source "automation-tools/assembler.sh"
+
+assemble flatpak_id "eu.vcmi.VCMI"
+
+# Custom Commands
+
+finalize
