5b5a1c6ee8d9c88015fe662f11eb8ca7714b9482
Lazorne <thelazorne@protonmail.com>
2025-06-04 16:24:34 +0200

    Mudlet / vpinballx: untar and unzip

--- mudlet/recipe.sh
+++ mudlet/recipe.sh
@@ -7,5 +7,6 @@ assemble appimage "https://make.mudlet.org/snapshots/ac8fcb/Mudlet-4.19.1-ptb-20
 # Custom Commands
 
 tar -xvf Mudlet-*-linux-x64.AppImage.tar
+rm -f Mudlet-*-linux-x64.AppImage.tar
 
 finalize
