ed61c0329b34627a4fc257f94ee367b8361d8394
etjanster <thelazorne@protonmail.com>
2025-06-04 12:17:08 +0200

    Add: vpinballx-bgfx experiment

---
+++ vpinballx-bgfx/component_launcher.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source /app/libexec/logger.sh
+
+COMPONENT_NAME="vpinball"
+RD_MODULES="/app/retrodeck/components"
+
+# This ensures the application can find its resources
+export APPDIR="$RD_MODULES/$COMPONENT_NAME"
+
+LD_LIBRARY_PATH="$RD_MODULES/$COMPONENT_NAME/"
+
+log i "RetroDECK is now launching $COMPONENT_NAME"
+log d "Library path is: $LD_LIBRARY_PATH"
+log d "AppDir is: $APPDIR"
+
+# NOTE: AppRun is not working for Mudlet
+exec "$RD_MODULES/$COMPONENT_NAME/VPinballX_BGFX" "$@"
+
---
+++ vpinballx-bgfx/recipe.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source "automation-tools/assembler.sh"
+
+assemble generic "https://github.com/vpinball/vpinball/releases/download/v10.8.1-3155-8054cc2/VPinballX_BGFX-10.8.1-3155-8054cc2-linux-x64-Release.zip"
+
+# "https://github.com//vpinball/vpinball/releases/latest/download/VPinballX_BGFX-*-linux-x64-Release.zip"
+
+## Right now they are only on pre-release for BGFX we update to above later pathing later.
+
+# Custom Commands
+
+finalize
