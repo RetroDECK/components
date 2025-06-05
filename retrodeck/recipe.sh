4e1090fc5029b17091c4f4e301191d4675d565f5
XargonWan <XargonWan@gmail.com>
2025-06-05 23:18:21 +0900

    fix(framework): added missing source and finalize

--- retrodeck/recipe.sh
+++ retrodeck/recipe.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+source "automation-tools/assembler.sh"
+
 # This is a dummy recipe just to make this component available to being installed
 
 echo "Installing RetroDECK Framework"
+
+finalize
