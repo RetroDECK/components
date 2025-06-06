819d5cdedf45f29abd0fd4a2b617c2df2d28cb60
Lazorne <thelazorne@protonmail.com>
2025-06-06 19:33:41 +0200

    azahar wip

---
+++ azahar/component_launcher.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source /app/libexec/logger.sh
+
+COMPONENT_NAME="azahar"
+RD_MODULES="/app/retrodeck/components"
+SHARED_LIBS="/app/retrodeck/components/shared-libs/qt-6.8/lib"
+
+# This ensures the application can find its resources
+export APPDIR="$RD_MODULES/$COMPONENT_NAME"
+
+LD_LIBRARY_PATH="$RD_MODULES/$COMPONENT_NAME/lib:$SHARED_LIBS:${LD_LIBRARY_PATH}"
+export QT_PLUGIN_PATH="$SHARED_LIBS/plugins:${QT_PLUGIN_PATH}"
+
+log i "RetroDECK is now launching $COMPONENT_NAME"
+log d "Library path is: $LD_LIBRARY_PATH"
+log d "AppDir is: $APPDIR"
+
+exec "$RD_MODULES/$COMPONENT_NAME/usr/bin/azahar" "$@"
+
