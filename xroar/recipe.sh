56f3906de4e1c71c5ed906274400af2aa9303121
XargonWan <xargonwan@gmail.com>
2025-06-06 15:12:03 +0900

    fix(recipe): move and clean up xroar artifacts in recipe.sh [skip ci]

--- xroar/recipe.sh
+++ xroar/recipe.sh
@@ -6,6 +6,8 @@ assemble gh_latest_release "RetroDECK/XRoar"
 
 # Custom Commands
 
+mv "$component/artifacts/bin/xroar" "$component/artifacts/xroar"
+rm -rf "$component/artifacts/share" "$component/artifacts/bin"
 chmod +x "$component/artifacts/xroar"
 
 finalize
