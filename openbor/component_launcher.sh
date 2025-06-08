dcd4a26aab2e90c15245740d429d1bbde590656c
Lazorne <thelazorne@protonmail.com>
2025-06-08 14:07:24 +0200

    OpenBOR: error in component_launcher.sh

--- openbor/component_launcher.sh
+++ openbor/component_launcher.sh
@@ -7,7 +7,7 @@ COMPONENT_NAME="openbor"
 # This ensures the application can find its resources
 export APPDIR="$rd_components/$COMPONENT_NAME"
 
-LD_LIBRARY_PATH="$rd_components/$COMPONENT_NAME/lib
+LD_LIBRARY_PATH="$rd_components/$COMPONENT_NAME/lib"
 
 log i "RetroDECK is now launching $COMPONENT_NAME"
 log d "Library path is: $LD_LIBRARY_PATH"
