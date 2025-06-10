a48161e8fb6bedc3ea35bd40b406cf75d0b3ed1f
Lazorne <thelazorne@protonmail.com>
2025-06-10 07:18:29 +0200

    Vpinball: PinMAME to recipe

--- vpinballx-bgfx/recipe.sh
+++ vpinballx-bgfx/recipe.sh
@@ -9,8 +9,15 @@ assemble gh_latest_release "vpinball/vpinball/VPinballX_BGFX-*-linux-x64-Release
 
 # Custom Commands
 
+artifacts_dir="$component/artifacts"
+
 tar -xvf $component/artifacts/VPinballX_BGFX-*.tar.gz -C $component/artifacts/
 rm -f $component/artifacts/VPinballX_BGFX-*.tar.gz
 mkdir $component/artifacts/Plugins
+
+# PinMAME
+log i "Downloading PinMAME: libpinmame..." "$logfile"
+wget "https://github.com/vpinball/pinmame/releases/latest/download/libpinmame-3.6.0-1227-ecd032e-linux-x64.zip" -O "$WORK_DIR/libpinmame-3.6.0-1227-ecd032e-linux-x64.zip"
+unzip -o "$WORK_DIR/libpinmame-*-linux-x64.zip" -d "$artifacts_dir/pinmame/"
 
 finalize
