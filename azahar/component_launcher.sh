64cb067e7229c26189893e72f4575f2c3580ce6b
Lazorne <thelazorne@protonmail.com>
2025-06-12 17:00:28 +0200

    fixed

--- azahar/component_launcher.sh
+++ azahar/component_launcher.sh
@@ -3,13 +3,13 @@
 source /app/libexec/logger.sh
 
 component_name="azahar"
-component_folder_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
+component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-LD_LIBRARY_PATH="$component_folder_path/lib:/app/retrodeck/components/shared-libs/qt-6.8/lib:${LD_LIBRARY_PATH}"
+LD_LIBRARY_PATH="$component_path/lib:/app/retrodeck/components/shared-libs/qt-6.8/lib:${LD_LIBRARY_PATH}"
 export QT_PLUGIN_PATH="$rd_rd_shared_libs/qt-6.8/lib/plugins:${QT_PLUGIN_PATH}"
 
 log i "RetroDECK is now launching $component_name"
 log d "Library path is: $LD_LIBRARY_PATH"
-log d "AppDir is: $component_folder_path"
+log d "AppDir is: $component_path"
 
-exec "$component_folder_path/usr/bin/azahar" "$@"
+exec "$component_path/usr/bin/azahar" "$@"
--- scummvm/component_launcher.sh
+++ scummvm/component_launcher.sh
@@ -17,4 +17,4 @@ log d "Library Path: $LD_LIBRARY_PATH"
 log d "QT Plugin Path: $QT_PLUGIN_PATH"
 log d "QT QPA Platform Plugin Path is: $QT_QPA_PLATFORM_PLUGIN_PATH"
 
-exec "$components_path/$component_name/bin/scummvm_wrapper" "$@"
+exec "$component_path/$component_name/bin/scummvm_wrapper" "$@"
