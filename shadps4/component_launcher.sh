85e8b68584266c17da44cac701c5a2b1f556fe4d
Lazorne <thelazorne@protonmail.com>
2025-06-12 10:47:31 +0200

    shadps4 launcher fixes

--- shadps4/component_launcher.sh
+++ shadps4/component_launcher.sh
@@ -3,15 +3,18 @@
 source /app/libexec/logger.sh
 
 component_name="shadps4"
+component_folder="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-# This ensures the application can find its resources
-export APPDIR="$rd_components/$component_name"
+# set ld_library_path
+export ld_library_path="$component_folder/lib:/app/retrodeck/components/shared-libs/qt-6.8/lib:${ld_library_path}"
 
-LD_LIBRARY_PATH="$rd_components/$component_name/lib:$rd_shared_libs/qt-6.8/lib:${LD_LIBRARY_PATH}"
-export QT_PLUGIN_PATH="$rd_shared_libs/qt-6.8/lib/plugins:${QT_PLUGIN_PATH}"
+# set plugin paths
+export qt_plugin_path="/app/retrodeck/components/shared-libs/plugins:${qt_plugin_path}"
+export qt_qpa_platform_plugin_path="/app/retrodeck/components/shared-libs/qt-6.8/lib/plugins/platforms"
 
-log i "RetroDECK is now launching $component_name"
-log d "Library path is: $LD_LIBRARY_PATH"
-log d "AppDir is: $APPDIR"
+log i "Retrodeck is now launching $component_name"
+log d "Library Path: $ld_library_path"
+log d "QT Plugin Path: $qt_plugin_path"
+log d "QT QPA Platform Plugin Path is: $qt_qpa_platform_plugin_path"
 
 exec "$components_path/$component_name/bin/shadps4" "$@"
