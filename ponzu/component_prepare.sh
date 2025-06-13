a4fce55400463fce1d4ab6c515a57747a3c153f7
XargonWan <xargonwan@gmail.com>
2025-06-13 14:29:00 +0900

    fix(component_prepare): update log directory paths to use internal logs path

--- ponzu/component_prepare.sh
+++ ponzu/component_prepare.sh
@@ -32,7 +32,7 @@ if [[ $(get_setting_value "$rd_conf" "akai_ponzu" "retrodeck" "options") == "tru
         create_dir "$saves_folder/n3ds/citra/nand/"
         create_dir "$saves_folder/n3ds/citra/sdmc/"
         dir_prep "$bios_folder/citra/sysdata" "$XDG_DATA_HOME/citra-emu/sysdata"
-        dir_prep "$logs_folder/citra" "$XDG_DATA_HOME/citra-emu/log"
+        dir_prep "$rd_internal_logs_path/citra" "$XDG_DATA_HOME/citra-emu/log"
         dir_prep "$mods_folder/Citra" "$XDG_DATA_HOME/citra-emu/load/mods"
         dir_prep "$texture_packs_folder/Citra" "$XDG_DATA_HOME/citra-emu/load/textures"
 
@@ -81,7 +81,7 @@ if [[ $(get_setting_value "$rd_conf" "kiroi_ponzu" "retrodeck" "options") == "tr
         dir_prep "$saves_folder/switch/yuzu/sdmc" "$XDG_DATA_HOME/yuzu/sdmc"
         dir_prep "$bios_folder/switch/keys" "$XDG_DATA_HOME/yuzu/keys"
         dir_prep "$bios_folder/switch/firmware" "$XDG_DATA_HOME/yuzu/nand/system/Contents/registered"
-        dir_prep "$logs_folder/yuzu" "$XDG_DATA_HOME/yuzu/log"
+        dir_prep "$rd_internal_logs_path/yuzu" "$XDG_DATA_HOME/yuzu/log"
         dir_prep "$screenshots_folder" "$XDG_DATA_HOME/yuzu/screenshots"
         dir_prep "$mods_folder/Yuzu" "$XDG_DATA_HOME/yuzu/load"
         # removing dead symlinks as they were present in a past version
@@ -98,7 +98,7 @@ if [[ $(get_setting_value "$rd_conf" "kiroi_ponzu" "retrodeck" "options") == "tr
         dir_prep "$bios_folder/switch/firmware" "$XDG_DATA_HOME/yuzu/nand/system/Contents/registered"
         dir_prep "$saves_folder/switch/yuzu/nand" "$XDG_DATA_HOME/yuzu/nand"
         dir_prep "$saves_folder/switch/yuzu/sdmc" "$XDG_DATA_HOME/yuzu/sdmc"
-        dir_prep "$logs_folder/yuzu" "$XDG_DATA_HOME/yuzu/log"
+        dir_prep "$rd_internal_logs_path/yuzu" "$XDG_DATA_HOME/yuzu/log"
         dir_prep "$screenshots_folder" "$XDG_DATA_HOME/yuzu/screenshots"
         dir_prep "$mods_folder/Yuzu" "$XDG_DATA_HOME/yuzu/load"
         set_setting_value "$yuzuconf" "nand_directory" "$saves_folder/switch/yuzu/nand" "yuzu" "Data%20Storage"
