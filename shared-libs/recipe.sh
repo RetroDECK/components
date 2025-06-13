08aa59d2e667104b900159e3b1bee0389543e1a5
XargonWan <xargonwan@gmail.com>
2025-06-13 13:18:17 +0900

    fix(recipe): add missing plugin files to qt artifact directories

--- shared-libs/recipe.sh
+++ shared-libs/recipe.sh
@@ -18,18 +18,21 @@ mkdir -p "$component/artifacts/qt-5.15/lib"
 flatpak-builder --user --force-clean --install-deps-from=flathub --install-deps-from=flathub-beta --repo=$WORK_DIR/shared-libs-5.15-repo --state-dir="$WORK_DIR/.flatpak-builder" "$WORK_DIR/shared-libs-5.15-build-dir" "$component/shared-libs.5.15.yml"
 ls -lah $WORK_DIR/shared-libs-5.15-build-dir/files/lib # DEBUG
 cp -rL $WORK_DIR/shared-libs-5.15-build-dir/files/lib/* $component/artifacts/qt-5.15/lib
+cp -rL $WORK_DIR/shared-libs-5.15-build-dir/files/usr/lib/plugins $component/artifacts/qt-5.15/lib/
 version="5.15"
 
 mkdir -p "$component/artifacts/qt-6.7/lib"
 flatpak-builder --user --force-clean --install-deps-from=flathub --install-deps-from=flathub-beta --repo=$WORK_DIR/shared-libs-6.7-repo --state-dir="$WORK_DIR/.flatpak-builder" "$WORK_DIR/shared-libs-6.7-build-dir" "$component/shared-libs.6.7.yml"
 ls -lah $WORK_DIR/shared-libs-6.7-build-dir/files/lib # DEBUG
 cp -rL $WORK_DIR/shared-libs-6.7-build-dir/files/lib/* $component/artifacts/qt-6.7/lib
+cp -rL $WORK_DIR/shared-libs-6.7-build-dir/files/usr/lib/plugins $component/artifacts/qt-6.7/lib/
 version="$version, 6.7"
 
 mkdir -p "$component/artifacts/qt-6.8/lib"
 flatpak-builder --user --force-clean --install-deps-from=flathub --install-deps-from=flathub-beta --repo=$WORK_DIR/shared-libs-6.8-repo --state-dir="$WORK_DIR/.flatpak-builder" "$WORK_DIR/shared-libs-6.8-build-dir" "$component/shared-libs.6.8.yml"
 ls -lah $WORK_DIR/shared-libs-6.7-build-dir/files/lib # DEBUG
 cp -rL $WORK_DIR/shared-libs-6.8-build-dir/files/lib/* $component/artifacts/qt-6.8/lib
+cp -rL $WORK_DIR/shared-libs-6.8-build-dir/files/usr/lib/plugins $component/artifacts/qt-6.8/lib/
 version="$version, 6.8"
 
 finalize
