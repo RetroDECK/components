d68563d980413e4996e766d622e5d903cf29c672
etjanster <thelazorne@protonmail.com>
2025-06-16 09:12:16 +0200

    flycast fixes

---
+++ flycast/component_prepare.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Setting component name and path based on the directory name
+component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
+config="/app/retrodeck/components/$component_name/rd_config"
+
+if [[ "$action" == "reset" ]]; then # Run reset-only commands
+  log i "----------------------"
+  log i "Preparing $component_name "
+  log i "----------------------"
+
+  # Add Multiuser things and reset things
+
+  # Shared actions
+    set_setting_value "$flycast_config" "Dreamcast.ContentPath" "$rd_home_roms_path/dreamcast" "flycast"
+
+fi
+
+# Dreamcast.AutoLoadState = no, Dreamcast.AutoSaveState = no | add to autoresume
+# rend.WideScreen = no, rend.WidescreenGameHacks = no | add to widescreen
