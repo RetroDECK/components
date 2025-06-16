29af31267df880dcffcf0ee7c0af63777c97cb34
etjanster <thelazorne@protonmail.com>
2025-06-16 08:11:38 +0200

    shadps4 prep

--- shadps4/component_prepare.sh
+++ shadps4/component_prepare.sh
@@ -12,5 +12,10 @@ if [[ "$action" == "reset" ]]; then # Run reset-only commands
   # Add Multiuser things and reset things
 
   # Shared actions
+    create_dir "$rd_home_storage_path/ps4/shadps4/installed"
+    create_dir "$rd_home_storage_path/ps4/shadps4/addcont"
+    set_setting_value "$config/config.toml" "installDirs" "$rd_home_storage_path/ps4/installed"
+    set_setting_value "$config/config.toml" "addonInstallDir" "$rd_home_storage_path/ps4/addcont"
+    set_setting_value "$config/config.toml" "saveDataPath" "$rd_home_saves_path/ps4/shadps4"
 
 fi
