a7284c63367adac9525c9e0c056abb2f25cae50a
icenine451 <benjamin.r.shelton@protonmail.com>
2025-06-17 15:27:09 -0400

    Azahar component: Update config file variable name Remove redundant code Update rd_shared_libs path Update component_prepare and remoave multi-user segments

--- azahar/component_launcher.sh
+++ azahar/component_launcher.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-source /app/libexec/logger.sh
-
 # Setting component name and path based on the directory name
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-LD_LIBRARY_PATH="$component_path/lib:/app/retrodeck/components/shared-libs/qt-6.8/lib:${LD_LIBRARY_PATH}"
-export QT_PLUGIN_PATH="$rd_rd_shared_libs/qt-6.8/lib/plugins:${QT_PLUGIN_PATH}"
+LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs/qt-6.8/lib:${LD_LIBRARY_PATH}"
+export QT_PLUGIN_PATH="$rd_shared_libs/qt-6.8/lib/plugins:${QT_PLUGIN_PATH}"
 
 log i "RetroDECK is now launching $component_name"
 log d "Library path is: $LD_LIBRARY_PATH"
