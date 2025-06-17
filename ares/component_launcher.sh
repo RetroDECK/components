674b7701ba5d0fd2241aceb6c56536bc41868baa
icenine451 <benjamin.r.shelton@protonmail.com>
2025-06-17 15:05:13 -0400

    Remove redundant commands, multi-user commands and update variable use for Ares component

--- ares/component_launcher.sh
+++ ares/component_launcher.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-source /app/libexec/logger.sh
-
 # Setting component name and path based on the directory name
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 # Set LD_LIBRARY_PATH
-export LD_LIBRARY_PATH="$component_path/lib:/app/retrodeck/components/shared-libs/qt-6.8/lib:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs/qt-6.8/lib:${LD_LIBRARY_PATH}"
 
 # Set plugin paths
-export QT_PLUGIN_PATH="/app/retrodeck/components/shared-libs/plugins:${QT_PLUGIN_PATH}"
-export QT_QPA_PLATFORM_PLUGIN_PATH="/app/retrodeck/components/shared-libs/qt-6.8/lib/plugins/platforms"
+export QT_PLUGIN_PATH="$rd_shared_libs/plugins:${QT_PLUGIN_PATH}"
+export QT_QPA_PLATFORM_PLUGIN_PATH="$rd_shared_libs/qt-6.8/lib/plugins/platforms"
 
 log i "RetroDECK is now launching $component_name"
 log d "Library path is: $LD_LIBRARY_PATH"
