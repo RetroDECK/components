c46cea0fe903752b45a2fd50cda447bb666dadc2
icenine451 <benjamin.r.shelton@protonmail.com>
2025-06-17 16:06:28 -0400

    Flycast component: Updated config file variable names in component_functions.sh Updated component_config variable use in component_prepare.sh Removed redundant code, updated $rd_shared_libs use in component_launcher.sh

--- flycast/component_functions.sh
+++ flycast/component_functions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-flycast_config_emu="$XDG_CONFIG_HOME/flycast/emu.cfg"
-flycast_controller_keyboard="$XDG_CONFIG_HOME/flycast/mappings/flycast_controller_keyboard.cfg"
-flycast_controller_neptune="$XDG_CONFIG_HOME/flycast/mappings/flycast_controller_neptune.cfg"
-flycast_controller_xbox360="$XDG_CONFIG_HOME/flycast/mappings/flycast_controller_xbox360.cfg"
+flycast_config="$XDG_CONFIG_HOME/flycast/emu.cfg"
+flycast_config_controller_keyboard="$XDG_CONFIG_HOME/flycast/mappings/flycast_controller_keyboard.cfg"
+flycast_config_controller_neptune="$XDG_CONFIG_HOME/flycast/mappings/flycast_controller_neptune.cfg"
+flycast_config_controller_xbox360="$XDG_CONFIG_HOME/flycast/mappings/flycast_controller_xbox360.cfg"
--- flycast/component_prepare.sh
+++ flycast/component_prepare.sh
@@ -2,18 +2,14 @@
 
 # Setting component name and path based on the directory name
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
-config="/app/retrodeck/components/$component_name/rd_config"
+component_config="/app/retrodeck/components/$component_name/rd_config"
 
 if [[ "$action" == "reset" ]]; then # Run reset-only commands
   log i "----------------------"
   log i "Preparing $component_name "
   log i "----------------------"
 
-  # Add Multiuser things and reset things
-
-  # Shared actions
-    set_setting_value "$flycast_config" "Dreamcast.ContentPath" "$rd_home_roms_path/dreamcast" "flycast"
-
+  set_setting_value "$flycast_config" "Dreamcast.ContentPath" "$roms_path/dreamcast" "flycast"
 fi
 
 # Dreamcast.AutoLoadState = no, Dreamcast.AutoSaveState = no | add to autoresume
