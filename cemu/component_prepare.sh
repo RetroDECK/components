8e7d5f80634c6eec94f7e24e144d0c69ae32391b
XargonWan <xargonwan@gmail.com>
2025-06-17 15:48:26 +0900

    fix(component_prepare): correct variable name for cemu configuration

--- cemu/component_prepare.sh
+++ cemu/component_prepare.sh
@@ -4,6 +4,9 @@
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 config="/app/retrodeck/components/$component_name/rd_config"
 
+cemu_conf="$XDG_CONFIG_HOME/Cemu/settings.xml"
+cemu_controller_conf="$XDG_CONFIG_HOME/Cemu/controllerProfiles/controller0.xml"
+
 if [[ "$action" == "reset" ]]; then # Run reset-only commands
   log i "----------------------"
   log i "Preparing $component_name"
@@ -17,8 +20,8 @@ if [[ "$action" == "reset" ]]; then # Run reset-only commands
   else
     create_dir -d "$XDG_CONFIG_HOME/Cemu/"
     cp -fr "$config/"* "$XDG_CONFIG_HOME/Cemu/"
-    set_setting_value "$cemuconf" "mlc_path" "$rd_home_bios_path/cemu" "cemu"
-    set_setting_value "$cemuconf" "Entry" "$rd_home_roms_path/wiiu" "cemu" "GamePaths"
+    set_setting_value "$cemu_conf" "mlc_path" "$rd_home_bios_path/cemu" "cemu"
+    set_setting_value "$cemu_conf" "Entry" "$rd_home_roms_path/wiiu" "cemu" "GamePaths"
     if [[ -e "$rd_home_bios_path/cemu/keys.txt" ]]; then
       rm -rf "$XDG_DATA_HOME/Cemu/keys.txt" && ln -s "$rd_home_bios_path/cemu/keys.txt" "$XDG_DATA_HOME/Cemu/keys.txt" && log d "Linked $rd_home_bios_path/cemu/keys.txt to $XDG_DATA_HOME/Cemu/keys.txt"
     fi
@@ -27,8 +30,8 @@ if [[ "$action" == "reset" ]]; then # Run reset-only commands
   dir_prep "$rd_home_saves_path/wiiu/cemu" "$rd_home_bios_path/cemu/usr/save"
 fi
 if [[ "$action" == "postmove" ]]; then # Run commands that apply to both resets and moves
-  set_setting_value "$cemuconf" "mlc_path" "$rd_home_bios_path/cemu" "cemu"
-  set_setting_value "$cemuconf" "Entry" "$rd_home_roms_path/wiiu" "cemu" "GamePaths"
+  set_setting_value "$cemu_conf" "mlc_path" "$rd_home_bios_path/cemu" "cemu"
+  set_setting_value "$cemu_conf" "Entry" "$rd_home_roms_path/wiiu" "cemu" "GamePaths"
   dir_prep "$rd_home_saves_path/wiiu/cemu" "$rd_home_bios_path/cemu/usr/save"
 fi
 
