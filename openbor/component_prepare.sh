7819cab5f505d3a79e6b3613a9b34e93188ff6ae
icenine451 <benjamin.r.shelton@protonmail.com>
2025-06-17 16:32:13 -0400

    OpenBOR component: Updated $component_config variable usage, removed multi-user segments and empty postmove section in component_prepare.sh Removed redundant code and fixed LD_LIBRARY_PATH in component_launcher.sh

--- openbor/component_prepare.sh
+++ openbor/component_prepare.sh
@@ -2,33 +2,14 @@
 
 # Setting component name and path based on the directory name
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
-config="/app/retrodeck/components/$component_name/rd_config"
+component_config="/app/retrodeck/components/$component_name/rd_config"
 
 if [[ "$action" == "reset" ]]; then # Run reset-only commands
-
   log i "------------------------"
   log i "Preparing $component_name"
   log i "------------------------"
-  
-    if [[ $multi_user_mode == "true" ]]; then # Multi-user actions
 
-      # if this section is empty the if will be invalid so we put a log message here
-      log d "TODO: Implement multi-user actions for $component_name"
-
-    else # Single-user actions
-
-      # if this section is empty the if will be invalid so we put a log message here
-      log d "TODO: Implement single-user actions for $component_name"
-
-    fi
-    # Shared actions
-
-    # Reset default preset settings
-
-fi
-
-if [[ "$action" == "postmove" ]]; then # Run only post-move commands
-
+  # Put reset actions here
 fi
 
 # Each game has it's own config and folder structure. All data is contained within each games folder under roms/openbor/<gamedir>. IDK if there is a standard path to saves and if each game looks the same.
