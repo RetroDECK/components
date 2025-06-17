d2c024dffbdb9e6b4e4c1dfd8eb407bc9c342390
icenine451 <benjamin.r.shelton@protonmail.com>
2025-06-17 15:30:17 -0400

    Update internal rd path variable names to new spec for Azahar component

--- azahar/component_prepare.sh
+++ azahar/component_prepare.sh
@@ -12,11 +12,11 @@ if [[ "$action" == "reset" ]]; then # Run reset-only commands
 
   create_dir -d "$XDG_CONFIG_HOME/azahar/"
   cp -fT "$config/azahar/qt-config.ini" "$azahar_config"
-  set_setting_value "$azahar_config" "nand_directory" "$rd_home_saves_path/n3ds/azahar/nand/" "azahar"
-  set_setting_value "$azahar_config" "nand_directory" "$rd_home_saves_path/n3ds/azahar/sdmc/" "azahar"
-  set_setting_value "$azahar_config" "Paths\gamedirs\3\path" "$rd_home_roms_path/n3ds" "azahar"
-  set_setting_value "$azahar_config" "Paths\screenshotPath" "$rd_home_screenshots_path/n3ds/azahar" "azahar"
-  create_dir "$rd_home_screenshots_path/n3ds/azahar"
-  create_dir "$rd_home_saves_path/n3ds/azahar/nand/"
-  create_dir "$rd_home_saves_path/n3ds/azahar/sdmc/"
+  set_setting_value "$azahar_config" "nand_directory" "$saves_path/n3ds/azahar/nand/" "azahar"
+  set_setting_value "$azahar_config" "nand_directory" "$saves_path/n3ds/azahar/sdmc/" "azahar"
+  set_setting_value "$azahar_config" "Paths\gamedirs\3\path" "$roms_path/n3ds" "azahar"
+  set_setting_value "$azahar_config" "Paths\screenshotPath" "$screenshots_path/n3ds/azahar" "azahar"
+  create_dir "$screenshots_path/n3ds/azahar"
+  create_dir "$saves_path/n3ds/azahar/nand/"
+  create_dir "$saves_path/n3ds/azahar/sdmc/"
 fi
