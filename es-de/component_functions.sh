781fbde3eb33159b0f93fbf46f7248168f7fdfc1
icenine451 <benjamin.r.shelton@protonmail.com>
2025-06-17 16:02:52 -0400

    ES-DE component: Update config file variable name in component_functions.sh Update $component_config varaible use, removed multi-user segments and updated rd path variables to latest spec in component_prepare.sh Removed redundant code and updated $rd_shared_libs variable use in component_launcher.sh

--- es-de/component_functions.sh
+++ es-de/component_functions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 es_de_appdata_path="$XDG_CONFIG_HOME/ES-DE"
-es_de_config_es_settings="$XDG_CONFIG_HOME/ES-DE/settings/es_settings.xml"
+es_de_config="$XDG_CONFIG_HOME/ES-DE/settings/es_settings.xml"
 es_de_logs_path="$XDG_CONFIG_HOME/ES-DE/logs"
--- es-de/component_prepare.sh
+++ es-de/component_prepare.sh
@@ -2,31 +2,31 @@
 
 # Setting component name and path based on the directory name
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
-config="/app/retrodeck/components/$component_name/rd_config"
+component_config="/app/retrodeck/components/$component_name/rd_config"
 
 log i "--------------------------------"
 log i "Preparing $component_name"
 log i "--------------------------------"
 
 if [[ "$action" == "reset" ]]; then
-    rm -rf "$XDG_CONFIG_HOME/ES-DE"
-    create_dir "$XDG_CONFIG_HOME/ES-DE/settings"
-    log d "Prepearing es_de_config_es_settings.xml"
-    cp -f "$config/es_de_config_es_settings.xml" "$XDG_CONFIG_HOME/ES-DE/settings/es_de_config_es_settings.xml"
-    set_setting_value "$es_de_config_es_settings" "ROMDirectory" "$rd_home_roms_path" "es_de_config_es_settings"
-    set_setting_value "$es_de_config_es_settings" "MediaDirectory" "$rd_home_downloaded_media_path" "es_de_config_es_settings"
-    set_setting_value "$es_de_config_es_settings" "UserThemeDirectory" "$rd_home_themes_path" "es_de_config_es_settings"
-    dir_prep "$rd_home_path/ES-DE/gamelists" "$XDG_CONFIG_HOME/ES-DE/gamelists"
-    dir_prep "$rd_home_path/ES-DE/collections" "$XDG_CONFIG_HOME/ES-DE/collections"
-    dir_prep "$rd_home_path/ES-DE/custom_systems" "$XDG_CONFIG_HOME/ES-DE/custom_systems"
-    log d "Generating roms system folders"
-    start_esde --create-system-dirs
-    update_splashscreens
+  rm -rf "$XDG_CONFIG_HOME/ES-DE"
+  create_dir "$XDG_CONFIG_HOME/ES-DE/settings"
+  log d "Prepearing es_de_config_es_settings.xml"
+  cp -f "$component_config/es_de_config_es_settings.xml" "$es_de_config"
+  set_setting_value "$es_de_config" "ROMDirectory" "$roms_path" "es_de_config_es_settings"
+  set_setting_value "$es_de_config" "MediaDirectory" "$downloaded_media_path" "es_de_config_es_settings"
+  set_setting_value "$es_de_config" "UserThemeDirectory" "$themes_path" "es_de_config_es_settings"
+  dir_prep "$path/ES-DE/gamelists" "$XDG_CONFIG_HOME/ES-DE/gamelists"
+  dir_prep "$path/ES-DE/collections" "$XDG_CONFIG_HOME/ES-DE/collections"
+  dir_prep "$path/ES-DE/custom_systems" "$XDG_CONFIG_HOME/ES-DE/custom_systems"
+  log d "Generating roms system folders"
+  start_esde --create-system-dirs
+  update_splashscreens
 fi
 
 if [[ "$action" == "postmove" ]]; then
-    set_setting_value "$es_de_config_es_settings" "ROMDirectory" "$rd_home_roms_path" "es_de_config_es_settings"
-    set_setting_value "$es_de_config_es_settings" "MediaDirectory" "$rd_home_downloaded_media_path" "es_de_config_es_settings"
-    set_setting_value "$es_de_config_es_settings" "UserThemeDirectory" "$rd_home_themes_path" "es_de_config_es_settings"
-    dir_prep "$rd_home_path/ES-DE/gamelists" "$XDG_CONFIG_HOME/ES-DE/gamelists"
+  set_setting_value "$es_de_config" "ROMDirectory" "$roms_path" "es_de_config_es_settings"
+  set_setting_value "$es_de_config" "MediaDirectory" "$downloaded_media_path" "es_de_config_es_settings"
+  set_setting_value "$es_de_config" "UserThemeDirectory" "$themes_path" "es_de_config_es_settings"
+  dir_prep "$path/ES-DE/gamelists" "$XDG_CONFIG_HOME/ES-DE/gamelists"
 fi
