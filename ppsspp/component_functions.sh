59516dd57a0f55d5138aed90f97582ca76e0ca48
icenine451 <benjamin.r.shelton@protonmail.com>
2025-06-17 16:48:34 -0400

    PPSSPP component: Updated config file variable names in component_functions.sh Updated $component_config variable usage, removed multi-user segments, updated rd path variable names in component_prepare.sh Removed redundant code in component_launcher.sh

--- ppsspp/component_functions.sh
+++ ppsspp/component_functions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ppsspp_config_ppsspp="$XDG_CONFIG_HOME/ppsspp/ppsspp.ini"
+ppsspp_config="$XDG_CONFIG_HOME/ppsspp/ppsspp.ini"
 ppsspp_config_controls="$XDG_CONFIG_HOME/ppsspp/controls.ini"
 ppsspp_retroachievements_dat="$XDG_CONFIG_HOME/ppsspp/ppsspp_retroachievements.dat"
-ppsspp_cheats_db="$XDG_CONFIG_HOME/ppsspp/ppsspp_retroachievements.dat"
+ppsspp_cheats_db="$rd_components/ppsspp/cheats/cheat.db"
--- ppsspp/component_prepare.sh
+++ ppsspp/component_prepare.sh
@@ -2,50 +2,36 @@
 
 # Setting component name and path based on the directory name
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
-config="/app/retrodeck/components/$component_name/rd_config"
-
-ppsspp_cheats_db="$rd_components/ppsspp/cheats/cheat.db"
+component_config="/app/retrodeck/components/$component_name/rd_config"
 
 if [[ "$action" == "reset" ]]; then # Run reset-only commands
-
   log i "------------------------"
   log i "Preparing $component_name"
   log i "------------------------"
 
-  if [[ $multi_user_mode == "true" ]]; then # Multi-user actions
-    create_dir -d "$multi_user_data_folder/$SteamAppUser/config/ppsspp/PSP/SYSTEM/"
-    cp -fv "$config/"* "$multi_user_data_folder/$SteamAppUser/config/ppsspp/PSP/SYSTEM/"
-    set_setting_value "$multi_user_data_folder/$SteamAppUser/config/ppsspp/PSP/SYSTEM/ppsspp.ini" "CurrentDirectory" "$rd_home_roms_path/psp" "ppsspp" "General"
-    dir_prep "$multi_user_data_folder/$SteamAppUser/config/ppsspp" "$XDG_CONFIG_HOME/ppsspp"
-
-  else # Single-user actions
-
-    create_dir -d "$XDG_CONFIG_HOME/ppsspp/PSP/SYSTEM/"
-    cp -fv "$config/"* "$XDG_CONFIG_HOME/ppsspp/PSP/SYSTEM/"
-    set_setting_value "$ppsspp_conf" "CurrentDirectory" "$rd_home_roms_path/psp" "ppsspp" "General"
-  fi
-
-  # Shared actions
-  dir_prep "$rd_home_saves_path/PSP/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/PSP/SAVEDATA"
-  dir_prep "$rd_home_states_path/PSP/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/PSP/PPSSPP_STATE"
-  dir_prep "$rd_home_texture_packs_path/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/PSP/TEXTURES"
-  dir_prep "$rd_home_shaders_path/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/assets/shaders"
-  create_dir -d "$rd_home_cheats_path/PPSSPP-SA"
-  dir_prep "$rd_home_cheats_path/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/PSP/Cheats"
-  if [[ -d "$rd_home_cheats_path/PPSSPP-SA" && "$(ls -A "$rd_home_cheats_path"/PPSSPP)" ]]; then
-    backup_file="$rd_home_backups_path/cheats/PPSSPP-SA-$(date +%y%m%d).tar.gz"
+  create_dir -d "$XDG_CONFIG_HOME/ppsspp/PSP/SYSTEM/"
+  cp -fv "$component_config/"* "$XDG_CONFIG_HOME/ppsspp/PSP/SYSTEM/"
+  set_setting_value "$ppsspp_config" "CurrentDirectory" "$roms_path/psp" "ppsspp" "General"
+  dir_prep "$saves_path/PSP/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/PSP/SAVEDATA"
+  dir_prep "$states_path/PSP/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/PSP/PPSSPP_STATE"
+  dir_prep "$texture_packs_path/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/PSP/TEXTURES"
+  dir_prep "$shaders_path/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/assets/shaders"
+  create_dir -d "$cheats_path/PPSSPP-SA"
+  dir_prep "$cheats_path/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/PSP/Cheats"
+  if [[ -d "$cheats_path/PPSSPP-SA" && "$(ls -A "$cheats_path"/PPSSPP)" ]]; then
+    backup_file="$backups_path/cheats/PPSSPP-SA-$(date +%y%m%d).tar.gz"
     create_dir "$(dirname "$backup_file")"
-    tar -czf "$backup_file" -C "$rd_home_cheats_path" PPSSPP
+    tar -czf "$backup_file" -C "$cheats_path" PPSSPP
     log i "PPSSPP cheats backed up to $backup_file"
   fi
-  rsync "$ppsspp_cheats_db" "$rd_home_cheats_path/PPSSPP/"
+  rsync "$ppsspp_cheats_db" "$cheats_path/PPSSPP/"
 fi
 
 if [[ "$action" == "postmove" ]]; then # Run only post-move commands
-  set_setting_value "$ppsspp_conf" "CurrentDirectory" "$rd_home_roms_path/psp" "ppsspp" "General"
-  dir_prep "$rd_home_saves_path/PSP/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/PSP/SAVEDATA"
-  dir_prep "$rd_home_states_path/PSP/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/PSP/PPSSPP_STATE"
-  dir_prep "$rd_home_texture_packs_path/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/PSP/TEXTURES"
-  dir_prep "$rd_home_shaders_path/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/assets/shaders"
-  dir_prep "$rd_home_cheats_path/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/PSP/Cheats"
+  set_setting_value "$ppsspp_config" "CurrentDirectory" "$roms_path/psp" "ppsspp" "General"
+  dir_prep "$saves_path/PSP/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/PSP/SAVEDATA"
+  dir_prep "$states_path/PSP/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/PSP/PPSSPP_STATE"
+  dir_prep "$texture_packs_path/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/PSP/TEXTURES"
+  dir_prep "$shaders_path/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/assets/shaders"
+  dir_prep "$cheats_path/PPSSPP-SA" "$XDG_CONFIG_HOME/ppsspp/PSP/Cheats"
 fi
