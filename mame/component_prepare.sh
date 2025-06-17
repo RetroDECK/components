efa071b7bdfd89489a8d68141d36290668409156
XargonWan <xargonwan@gmail.com>
2025-06-17 12:00:29 +0900

    fix(mame): update variable names for MAME configuration files

--- mame/component_prepare.sh
+++ mame/component_prepare.sh
@@ -4,6 +4,10 @@
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 config="/app/retrodeck/components/$component_name/rd_config"
 
+mame_conf="$XDG_CONFIG_HOME/mame/ini/mame.ini"
+mame_ui_conf="$XDG_CONFIG_HOME/mame/ini/ui.ini"
+mame_default_conf="$XDG_CONFIG_HOME/mame/cfg/default.cfg"
+
 # TODO: do a proper script
 # This is just a placeholder script to test the emulator's flow
 log i "----------------------"
@@ -56,19 +60,19 @@ create_dir "$XDG_DATA_HOME/mame/assets/ui"
 create_dir "$rd_home_shaders_path/mame/bgfx/"
 
 dir_prep "$rd_home_saves_path/mame-sa/hiscore" "$XDG_CONFIG_HOME/mame/hiscore"
-cp -fvr "$config/mame.ini" "$mameconf"
-cp -fvr "$config/ui.ini" "$mameuiconf"
-cp -fvr "$config/default.cfg" "$mamedefconf"
+cp -fvr "$config/mame.ini" "$mame_conf"
+cp -fvr "$config/ui.ini" "$mame_ui_conf"
+cp -fvr "$config/default.cfg" "$mame_default_conf"
 cp -fvr "/app/retrodeck/components/mame/share/mame/bgfx/"* "$rd_home_shaders_path/mame/bgfx"
 
-sed -i 's#RETRODECKROMSDIR#'"$rd_home_roms_path"'#g' "$mameconf" # one-off as roms folders are a lot
-set_setting_value "$mameconf" "nvram_directory" "$rd_home_saves_path/mame-sa/nvram" "mame"
-set_setting_value "$mameconf" "state_directory" "$rd_home_states_path/mame-sa" "mame"
-set_setting_value "$mameconf" "snapshot_directory" "$rd_home_screenshots_path/mame-sa" "mame"
-set_setting_value "$mameconf" "diff_directory" "$rd_home_saves_path/mame-sa/diff" "mame"
-set_setting_value "$mameconf" "samplepath" "$rd_home_bios_path/mame-sa/samples" "mame"
-set_setting_value "$mameconf" "cheatpath" "$rd_home_cheats_path/mame" "mame"
-set_setting_value "$mameconf" "bgfx_path" "$rd_home_shaders_path/mame/bgfx/" "mame"
+sed -i 's#RETRODECKROMSDIR#'"$rd_home_roms_path"'#g' "$mame_conf" # one-off as roms folders are a lot
+set_setting_value "$mame_conf" "nvram_directory" "$rd_home_saves_path/mame-sa/nvram" "mame"
+set_setting_value "$mame_conf" "state_directory" "$rd_home_states_path/mame-sa" "mame"
+set_setting_value "$mame_conf" "snapshot_directory" "$rd_home_screenshots_path/mame-sa" "mame"
+set_setting_value "$mame_conf" "diff_directory" "$rd_home_saves_path/mame-sa/diff" "mame"
+set_setting_value "$mame_conf" "samplepath" "$rd_home_bios_path/mame-sa/samples" "mame"
+set_setting_value "$mame_conf" "cheatpath" "$rd_home_cheats_path/mame" "mame"
+set_setting_value "$mame_conf" "bgfx_path" "$rd_home_shaders_path/mame/bgfx/" "mame"
 
 log i "Placing cheats in \"$rd_home_cheats_path/mame\""
 unzip -j -o "$config/cheat0264.zip" 'cheat.7z' -d "$rd_home_cheats_path/mame"
