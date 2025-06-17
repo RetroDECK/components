852ca7ba277f3ac213f89218a72c77cf20b9cc7c
icenine451 <benjamin.r.shelton@protonmail.com>
2025-06-17 15:43:38 -0400

    Dolphin component: Updated variable name and fixed incorrect pathing in component_functions.sh Fixed $component_config variable use, removed multi-user segments and updated rd paths variable names to latest spec in component_prepare.sh Removed redundant code and updated $rd_shared_libs use in component_launcher.sh

--- dolphin/component_prepare.sh
+++ dolphin/component_prepare.sh
@@ -2,59 +2,42 @@
 
 # Setting component name and path based on the directory name
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
-config="/app/retrodeck/components/$component_name/rd_config"
+component_config="/app/retrodeck/components/$component_name/rd_config"
 
 if [[ "$action" == "reset" ]]; then # Run reset-only commands
+  log i "----------------------"
+  log i "Preparing $component_name"
+  log i "----------------------"
 
-    log i "----------------------"
-    log i "Preparing $component_name"
-    log i "----------------------"
-
-    if [[ $multi_user_mode == "true" ]]; then # Multi-user actions
-        create_dir -d "$multi_user_data_folder/$SteamAppUser/config/dolphin-emu"
-        cp -fvr "$config/"* "$multi_user_data_folder/$SteamAppUser/config/dolphin-emu/"
-        set_setting_value "$multi_user_data_folder/$SteamAppUser/config/dolphin-emu/Dolphin.ini" "BIOS" "$rd_home_bios_path" "dolphin" "GBA"
-        set_setting_value "$multi_user_data_folder/$SteamAppUser/config/dolphin-emu/Dolphin.ini" "SavesPath" "$rd_home_saves_path/gba" "dolphin" "GBA"
-        set_setting_value "$multi_user_data_folder/$SteamAppUser/config/dolphin-emu/Dolphin.ini" "ISOPath0" "$rd_home_roms_path/wii" "dolphin" "General"
-        set_setting_value "$multi_user_data_folder/$SteamAppUser/config/dolphin-emu/Dolphin.ini" "ISOPath1" "$rd_home_roms_path/gc" "dolphin" "General"
-        set_setting_value "$multi_user_data_folder/$SteamAppUser/config/dolphin-emu/Dolphin.ini" "WiiSDCardPath" "$rd_home_saves_path/wii/dolphin/sd.raw" "dolphin" "General"
-        dir_prep "$multi_user_data_folder/$SteamAppUser/config/dolphin-emu" "$XDG_CONFIG_HOME/dolphin-emu"
-    else # Single-user actions
-        create_dir -d "$XDG_CONFIG_HOME/dolphin-emu/"
-        cp -fvr "$config/"* "$XDG_CONFIG_HOME/dolphin-emu/"
-        set_setting_value "$dolphin_config_Dolphin" "BIOS" "$rd_home_bios_path" "dolphin" "GBA"
-        set_setting_value "$dolphin_config_Dolphin" "SavesPath" "$rd_home_saves_path/gba" "dolphin" "GBA"
-        set_setting_value "$dolphin_config_Dolphin" "ISOPath0" "$rd_home_roms_path/wii" "dolphin" "General"
-        set_setting_value "$dolphin_config_Dolphin" "ISOPath1" "$rd_home_roms_path/gc" "dolphin" "General"
-        set_setting_value "$dolphin_config_Dolphin" "WiiSDCardPath" "$rd_home_saves_path/wii/dolphin/sd.raw" "dolphin" "General"
-    fi
-
-    # Shared actions
-    dir_prep "$rd_home_saves_path/gc/dolphin/EU" "$XDG_DATA_HOME/dolphin-emu/GC/EUR" # TODO: Multi-user one-off
-    dir_prep "$rd_home_saves_path/gc/dolphin/US" "$XDG_DATA_HOME/dolphin-emu/GC/USA" # TODO: Multi-user one-off
-    dir_prep "$rd_home_saves_path/gc/dolphin/JP" "$XDG_DATA_HOME/dolphin-emu/GC/JAP" # TODO: Multi-user one-off
-    dir_prep "$rd_home_screenshots_path" "$XDG_DATA_HOME/dolphin-emu/ScreenShots"
-    dir_prep "$rd_home_states_path/dolphin" "$XDG_DATA_HOME/dolphin-emu/StateSaves"
-    dir_prep "$rd_home_saves_path/wii/dolphin" "$XDG_DATA_HOME/dolphin-emu/Wii"
-    dir_prep "$rd_home_mods_path/Dolphin" "$XDG_DATA_HOME/dolphin-emu/Load/GraphicMods"
-    dir_prep "$rd_home_texture_packs_path/Dolphin" "$XDG_DATA_HOME/dolphin-emu/Load/Textures"
-
-    # Reset default preset settings
-    set_setting_value "$rd_conf" "dolphin" "$(get_setting_value "$rd_defaults" "dolphin" "retrodeck" "ask_to_exit")" "retrodeck" "ask_to_exit"
+  create_dir -d "$XDG_CONFIG_HOME/dolphin-emu/"
+  cp -fvr "$component_config/"* "$XDG_CONFIG_HOME/dolphin-emu/"
+  set_setting_value "$dolphin_config" "BIOS" "$bios_path" "dolphin" "GBA"
+  set_setting_value "$dolphin_config" "SavesPath" "$saves_path/gba" "dolphin" "GBA"
+  set_setting_value "$dolphin_config" "ISOPath0" "$roms_path/wii" "dolphin" "General"
+  set_setting_value "$dolphin_config" "ISOPath1" "$roms_path/gc" "dolphin" "General"
+  set_setting_value "$dolphin_config" "WiiSDCardPath" "$saves_path/wii/dolphin/sd.raw" "dolphin" "General"
+  dir_prep "$saves_path/gc/dolphin/EU" "$XDG_DATA_HOME/dolphin-emu/GC/EUR" # TODO: Multi-user one-off
+  dir_prep "$saves_path/gc/dolphin/US" "$XDG_DATA_HOME/dolphin-emu/GC/USA" # TODO: Multi-user one-off
+  dir_prep "$saves_path/gc/dolphin/JP" "$XDG_DATA_HOME/dolphin-emu/GC/JAP" # TODO: Multi-user one-off
+  dir_prep "$screenshots_path" "$XDG_DATA_HOME/dolphin-emu/ScreenShots"
+  dir_prep "$states_path/dolphin" "$XDG_DATA_HOME/dolphin-emu/StateSaves"
+  dir_prep "$saves_path/wii/dolphin" "$XDG_DATA_HOME/dolphin-emu/Wii"
+  dir_prep "$mods_path/Dolphin" "$XDG_DATA_HOME/dolphin-emu/Load/GraphicMods"
+  dir_prep "$texture_packs_path/Dolphin" "$XDG_DATA_HOME/dolphin-emu/Load/Textures"
 fi
 
 if [[ "$action" == "postmove" ]]; then # Run only post-move commands
-    dir_prep "$rd_home_saves_path/gc/dolphin/EU" "$XDG_DATA_HOME/dolphin-emu/GC/EUR"
-    dir_prep "$rd_home_saves_path/gc/dolphin/US" "$XDG_DATA_HOME/dolphin-emu/GC/USA"
-    dir_prep "$rd_home_saves_path/gc/dolphin/JP" "$XDG_DATA_HOME/dolphin-emu/GC/JAP"
-    dir_prep "$rd_home_screenshots_path" "$XDG_DATA_HOME/dolphin-emu/ScreenShots"
-    dir_prep "$rd_home_states_path/dolphin" "$XDG_DATA_HOME/dolphin-emu/StateSaves"
-    dir_prep "$rd_home_saves_path/wii/dolphin" "$XDG_DATA_HOME/dolphin-emu/Wii"
-    dir_prep "$rd_home_mods_path/Dolphin" "$XDG_DATA_HOME/dolphin-emu/Load/GraphicMods"
-    dir_prep "$rd_home_texture_packs_path/Dolphin" "$XDG_DATA_HOME/dolphin-emu/Load/Textures"
-    set_setting_value "$dolphin_config_Dolphin" "BIOS" "$rd_home_bios_path" "dolphin" "GBA"
-    set_setting_value "$dolphin_config_Dolphin" "SavesPath" "$rd_home_saves_path/gba" "dolphin" "GBA"
-    set_setting_value "$dolphin_config_Dolphin" "ISOPath0" "$rd_home_roms_path/wii" "dolphin" "General"
-    set_setting_value "$dolphin_config_Dolphin" "ISOPath1" "$rd_home_roms_path/gc" "dolphin" "General"
-    set_setting_value "$dolphin_config_Dolphin" "WiiSDCardPath" "$rd_home_saves_path/wii/dolphin/sd.raw" "dolphin" "General"
+    dir_prep "$saves_path/gc/dolphin/EU" "$XDG_DATA_HOME/dolphin-emu/GC/EUR"
+    dir_prep "$saves_path/gc/dolphin/US" "$XDG_DATA_HOME/dolphin-emu/GC/USA"
+    dir_prep "$saves_path/gc/dolphin/JP" "$XDG_DATA_HOME/dolphin-emu/GC/JAP"
+    dir_prep "$screenshots_path" "$XDG_DATA_HOME/dolphin-emu/ScreenShots"
+    dir_prep "$states_path/dolphin" "$XDG_DATA_HOME/dolphin-emu/StateSaves"
+    dir_prep "$saves_path/wii/dolphin" "$XDG_DATA_HOME/dolphin-emu/Wii"
+    dir_prep "$mods_path/Dolphin" "$XDG_DATA_HOME/dolphin-emu/Load/GraphicMods"
+    dir_prep "$texture_packs_path/Dolphin" "$XDG_DATA_HOME/dolphin-emu/Load/Textures"
+    set_setting_value "$dolphin_config" "BIOS" "$bios_path" "dolphin" "GBA"
+    set_setting_value "$dolphin_config" "SavesPath" "$saves_path/gba" "dolphin" "GBA"
+    set_setting_value "$dolphin_config" "ISOPath0" "$roms_path/wii" "dolphin" "General"
+    set_setting_value "$dolphin_config" "ISOPath1" "$roms_path/gc" "dolphin" "General"
+    set_setting_value "$dolphin_config" "WiiSDCardPath" "$saves_path/wii/dolphin/sd.raw" "dolphin" "General"
 fi
