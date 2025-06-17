ab49312fafd6b0edfc7c10d13bf5078a063cefb2
icenine451 <benjamin.r.shelton@protonmail.com>
2025-06-17 16:12:17 -0400

    Gargoyle component: Updated config file variable names in component_functions.sh Updated component_config variable use, removed multi-user segments in component_prepare.sh Removed redundant code and updated $rd_shared_libs use in component_launcher.sh

--- gargoyle/component_functions.sh
+++ gargoyle/component_functions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 gargoyle_config="$XDG_CONFIG_HOME/gargoyle/QtProject.conf"
-gargoyle_Gargoylerc="$XDG_CONFIG_HOME/gargoyle/Gargoylerc"
-gargoyle_user_dirs="$XDG_CONFIG_HOME/gargoyle/user-dirs.dirs"
+gargoyle_config_Gargoylerc="$XDG_CONFIG_HOME/gargoyle/Gargoylerc"
+gargoyle_config_user_dirs="$XDG_CONFIG_HOME/gargoyle/user-dirs.dirs"
