f8c6a00d0c2ddc7c50b8f06495efea0821cd4901
icenine451 <benjamin.r.shelton@protonmail.com>
2025-06-18 09:22:48 -0400

    Vita3K component: Update component config file variable name in component_functions.sh Update component_config variable usage, remove multi-user segments, update rd path variable names in component_prepare.sh Remove redundant code in component_launcher.sh

--- vita3k/component_functions.sh
+++ vita3k/component_functions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-vita3k_config_config="$XDG_CONFIG_HOME/vita3k/config.yml"
+vita3k_config="$XDG_CONFIG_HOME/vita3k/config.yml"
 vita3k_config_user="$XDG_CONFIG_HOME/vita3k/ux0/user/00/user.xml"
--- vita3k/component_prepare.sh
+++ vita3k/component_prepare.sh
@@ -2,33 +2,24 @@
 
 # Setting component name and path based on the directory name
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
-config="/app/retrodeck/components/$component_name/rd_config"
+component_config="/app/retrodeck/components/$component_name/rd_config"
 
 if [[ "$action" == "reset" ]]; then # Run reset-only commands
 
-    log i "----------------------"
-    log i "Preparing $component_name"
-    log i "----------------------"
+  log i "----------------------"
+  log i "Preparing $component_name"
+  log i "----------------------"
 
-    if [[ $multi_user_mode == "true" ]]; then # Multi-user actions
-      log d "Figure out what Vita3k needs for multi-user"
-
-    else # Single-user actions
-
-      # NOTE: the component is writing in "." so it must be placed in the rw filesystem. A symlink of the binary is already placed in /app/bin/Vita3K
-      rm -rf "$XDG_CONFIG_HOME/Vita3K"
-      create_dir "$XDG_CONFIG_HOME/Vita3K"
-      cp -fvr "$config/config.yml" "$vita3k_config_config" # component config
-      cp -fvr "$config/ux0" "$rd_home_storage_path/psvita/Vita3K/" # User config
-      set_setting_value "$vita3k_config_config" "pref-path" "$rd_home_storage_path/psvita/Vita3K/" "vita3k"
-    fi
-
-    # Shared actions
-    dir_prep "$rd_home_saves_path/psvita/vita3k" "$rd_home_storage_path/psvita/Vita3K/ux0/user/00/savedata" # Multi-user safe?
+  # NOTE: the component is writing in "." so it must be placed in the rw filesystem. A symlink of the binary is already placed in /app/bin/Vita3K
+  rm -rf "$XDG_CONFIG_HOME/Vita3K"
+  create_dir "$XDG_CONFIG_HOME/Vita3K"
+  cp -fvr "$component_config/config.yml" "$vita3k_config" # component config
+  cp -fvr "$component_config/ux0" "$storage_path/psvita/Vita3K/" # User config
+  set_setting_value "$vita3k_config" "pref-path" "$storage_path/psvita/Vita3K/" "vita3k"
+  dir_prep "$saves_path/psvita/vita3k" "$storage_path/psvita/Vita3K/ux0/user/00/savedata" # Multi-user safe?
 fi
 
 if [[ "$action" == "postmove" ]]; then # Run only post-move commands
-  dir_prep "$rd_home_saves_path/psvita/vita3k" "$rd_home_storage_path/psvita/Vita3K/ux0/user/00/savedata" # Multi-user safe?
-  set_setting_value "$vita3k_config_config" "pref-path" "$rd_home_storage_path/psvita/Vita3K/" "vita3k"
+  dir_prep "$saves_path/psvita/vita3k" "$storage_path/psvita/Vita3K/ux0/user/00/savedata" # Multi-user safe?
+  set_setting_value "$vita3k_config" "pref-path" "$storage_path/psvita/Vita3K/" "vita3k"
 fi
-
