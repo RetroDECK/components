cbb902807c7ffc5df2d4fcfbc51ea9dfa2df3c32
icenine451 <benjamin.r.shelton@protonmail.com>
2025-06-18 08:44:24 -0400

    RPCS3 component: Update component config file variable name in component_functions.sh Update component_config variable name usage, remove multi-user segments and fix rd path variable names in component_prepare.sh Remove redundant code in component_launcher.sh

--- rpcs3/component_functions.sh
+++ rpcs3/component_functions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rpcs3_config_config="$XDG_CONFIG_HOME/rpcs3/config.yml"
+rpcs3_config="$XDG_CONFIG_HOME/rpcs3/config.yml"
 rpcs3_config_evdev_positive_axis="$XDG_CONFIG_HOME/rpcs3/evdev_positive_axis.yml"
 rpcs3_config_vfs="$XDG_CONFIG_HOME/rpcs3/vfs.yml"
 rpcs3_gui_current_settings="$XDG_CONFIG_HOME/rpcs3/GuiConfigs/CurrentSettings.ini"
--- rpcs3/component_prepare.sh
+++ rpcs3/component_prepare.sh
@@ -2,45 +2,31 @@
 
 # Setting component name and path based on the directory name
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
-config="/app/retrodeck/components/$component_name/rd_config"
+component_config="/app/retrodeck/components/$component_name/rd_config"
 
 if [[ "$action" == "reset" ]]; then # Run reset-only commands
+  log i "------------------------"
+  log i "Preparing $component_name"
+  log i "------------------------"
 
-    log i "------------------------"
-    log i "Preparing $component_name"
-    log i "------------------------"
-
-    if [[ $multi_user_mode == "true" ]]; then # Multi-user actions
-        create_dir -d "$multi_user_data_folder/$SteamAppUser/config/rpcs3/"
-        cp -fr "$config/"* "$multi_user_data_folder/$SteamAppUser/config/rpcs3/"
-        # This is an unfortunate one-off because set_setting_value does not currently support settings with $ in the name.
-        sed -i 's^\^$(EmulatorDir): .*^$(EmulatorDir): '"$rd_home_storage_path/ps3/rpcs3/"'^' "$multi_user_data_folder/$SteamAppUser/config/rpcs3/vfs.yml"
-        set_setting_value "$multi_user_data_folder/$SteamAppUser/config/rpcs3/vfs.yml" "/games/" "$rd_home_roms_path/ps3/" "rpcs3"
-        dir_prep "$multi_user_data_folder/$SteamAppUser/config/rpcs3" "$XDG_CONFIG_HOME/rpcs3"
-
-    else # Single-user actions
-
-        create_dir -d "$XDG_CONFIG_HOME/rpcs3/"
-        cp -fr "$config/"* "$XDG_CONFIG_HOME/rpcs3/"
-        # This is an unfortunate one-off because set_setting_value does not currently support settings with $ in the name.
-        sed -i 's^\^$(EmulatorDir): .*^$(EmulatorDir): '"$rd_home_storage_path/ps3/rpcs3/"'^' "$rpcs3_config_vfs"
-        set_setting_value "$rpcs3_config_vfs" "/games/" "$rd_home_roms_path/ps3/" "rpcs3"
-        dir_prep "$rd_home_saves_path/ps3/rpcs3" "$rd_home_storage_path/ps3/rpcs3/dev_hdd0/home/00000001/savedata"
-        dir_prep "$rd_home_states_path/ps3/rpcs3" "$XDG_CONFIG_HOME/rpcs3/savestates"
-    fi
-
-    # Shared actions
-    create_dir "$rd_home_storage_path/ps3/rpcs3/dev_hdd0"
-    create_dir "$rd_home_storage_path/ps3/rpcs3/dev_hdd1"
-    create_dir "$rd_home_storage_path/ps3/rpcs3/dev_flash"
-    create_dir "$rd_home_storage_path/ps3/rpcs3/dev_flash2"
-    create_dir "$rd_home_storage_path/ps3/rpcs3/dev_flash3"
-    create_dir "$rd_home_storage_path/ps3/rpcs3/dev_bdvd"
-    create_dir "$rd_home_storage_path/ps3/rpcs3/dev_usb000"
+  create_dir -d "$XDG_CONFIG_HOME/rpcs3/"
+  cp -fr "$component_config/"* "$XDG_CONFIG_HOME/rpcs3/"
+  # This is an unfortunate one-off because set_setting_value does not currently support settings with $ in the name.
+  sed -i 's^\^$(EmulatorDir): .*^$(EmulatorDir): '"$storage_path/ps3/rpcs3/"'^' "$rpcs3_config_vfs"
+  set_setting_value "$rpcs3_config_vfs" "/games/" "$roms_path/ps3/" "rpcs3"
+  dir_prep "$saves_path/ps3/rpcs3" "$storage_path/ps3/rpcs3/dev_hdd0/home/00000001/savedata"
+  dir_prep "$states_path/ps3/rpcs3" "$XDG_CONFIG_HOME/rpcs3/savestates"
+  create_dir "$storage_path/ps3/rpcs3/dev_hdd0"
+  create_dir "$storage_path/ps3/rpcs3/dev_hdd1"
+  create_dir "$storage_path/ps3/rpcs3/dev_flash"
+  create_dir "$storage_path/ps3/rpcs3/dev_flash2"
+  create_dir "$storage_path/ps3/rpcs3/dev_flash3"
+  create_dir "$storage_path/ps3/rpcs3/dev_bdvd"
+  create_dir "$storage_path/ps3/rpcs3/dev_usb000"
 fi
 
 if [[ "$action" == "postmove" ]]; then # Run only post-move commands
-    # This is an unfortunate one-off because set_setting_value does not currently support settings with $ in the name.
-    sed -i 's^\^$(EmulatorDir): .*^$(EmulatorDir): '"$rd_home_storage_path/ps3/rpcs3/"'^' "$rpcs3_config_vfs"
-    set_setting_value "$rpcs3_config_vfs" "/games/" "$rd_home_roms_path/ps3" "rpcs3"
+  # This is an unfortunate one-off because set_setting_value does not currently support settings with $ in the name.
+  sed -i 's^\^$(EmulatorDir): .*^$(EmulatorDir): '"$storage_path/ps3/rpcs3/"'^' "$rpcs3_config_vfs"
+  set_setting_value "$rpcs3_config_vfs" "/games/" "$roms_path/ps3" "rpcs3"
 fi
