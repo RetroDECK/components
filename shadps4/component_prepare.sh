e22dc8d783832bb11e9e5d665f7d729d0620b09c
icenine451 <benjamin.r.shelton@protonmail.com>
2025-06-18 09:04:21 -0400

    ShadPS4 component: Fix component config file variable name in component_functions.sh Fix component_config variable usage, update component config file variable name usage, update rd path variable names and remove multi-user segments in component_prepare.sh Remove redundant code and update $rd_shared_libs variable usage in component_launcher.sh

--- shadps4/component_prepare.sh
+++ shadps4/component_prepare.sh
@@ -2,20 +2,16 @@
 
 # Setting component name and path based on the directory name
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
-config="/app/retrodeck/components/$component_name/rd_config"
+component_config="/app/retrodeck/components/$component_name/rd_config"
 
 if [[ "$action" == "reset" ]]; then # Run reset-only commands
   log i "----------------------"
   log i "Preparing $component_name"
   log i "----------------------"
 
-  # Add Multiuser things and reset things
-
-  # Shared actions
-    create_dir "$rd_home_storage_path/ps4/shadps4/installed"
-    create_dir "$rd_home_storage_path/ps4/shadps4/addcont"
-    set_setting_value "$shadps4_config_config" "installDirs" "$rd_home_storage_path/ps4/installed" "shadps4"
-    set_setting_value "$shadps4_config_config" "addonInstallDir" "$rd_home_storage_path/ps4/addcont" "shadps4"
-    set_setting_value "$shadps4_config_config" "saveDataPath" "$rd_home_saves_path/ps4/shadps4" "shadps4"
-
+  create_dir "$storage_path/ps4/shadps4/installed"
+  create_dir "$storage_path/ps4/shadps4/addcont"
+  set_setting_value "$shadps4_config" "installDirs" "$storage_path/ps4/installed" "shadps4"
+  set_setting_value "$shadps4_config" "addonInstallDir" "$storage_path/ps4/addcont" "shadps4"
+  set_setting_value "$shadps4_config" "saveDataPath" "$saves_path/ps4/shadps4" "shadps4"
 fi
