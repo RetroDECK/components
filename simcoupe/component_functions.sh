a6672776bfb12100bdcf587b2b4f6667402d713e
icenine451 <benjamin.r.shelton@protonmail.com>
2025-06-18 09:07:21 -0400

    SimCoupe component: Update component config file variable name in component_functions.sh Update component_config variable usage and remove multi-user segments in component_prepare.sh Remove redundant code and fix LD_LIBRARY_PATH usage in component_launcher.sh

--- simcoupe/component_functions.sh
+++ simcoupe/component_functions.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-simcoupe_config_SimCoupe="$XDG_CONFIG_HOME/simcoupe/SimCoupe.cfg"
+simcoupe_config="$XDG_CONFIG_HOME/simcoupe/SimCoupe.cfg"
--- simcoupe/component_launcher.sh
+++ simcoupe/component_launcher.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-source /app/libexec/logger.sh
-
 # Setting component name and path based on the directory name
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-LD_LIBRARY_PATH="$component_path/"
+LD_LIBRARY_PATH="$component_path/:${LD_LIBRARY_PATH}"
 
 log i "RetroDECK is now launching $component_name"
 log d "Library path is: $LD_LIBRARY_PATH"
