71b5edf23f56a359a6f21ef840cbc0dd8210a12f
icenine451 <benjamin.r.shelton@protonmail.com>
2025-06-18 09:16:16 -0400

    ES-DE component: Fix rd path variable names in component_prepare.sh

--- es-de/component_prepare.sh
+++ es-de/component_prepare.sh
@@ -11,14 +11,14 @@ log i "--------------------------------"
 if [[ "$action" == "reset" ]]; then
   rm -rf "$XDG_CONFIG_HOME/ES-DE"
   create_dir "$XDG_CONFIG_HOME/ES-DE/settings"
-  log d "Prepearing es_de_config_es_settings.xml"
+  log d "Preparing es_de_config_es_settings.xml"
   cp -f "$component_config/es_de_config_es_settings.xml" "$es_de_config"
   set_setting_value "$es_de_config" "ROMDirectory" "$roms_path" "es_de_config_es_settings"
   set_setting_value "$es_de_config" "MediaDirectory" "$downloaded_media_path" "es_de_config_es_settings"
   set_setting_value "$es_de_config" "UserThemeDirectory" "$themes_path" "es_de_config_es_settings"
-  dir_prep "$path/ES-DE/gamelists" "$XDG_CONFIG_HOME/ES-DE/gamelists"
-  dir_prep "$path/ES-DE/collections" "$XDG_CONFIG_HOME/ES-DE/collections"
-  dir_prep "$path/ES-DE/custom_systems" "$XDG_CONFIG_HOME/ES-DE/custom_systems"
+  dir_prep "$rd_home_path/ES-DE/gamelists" "$XDG_CONFIG_HOME/ES-DE/gamelists"
+  dir_prep "$rd_home_path/ES-DE/collections" "$XDG_CONFIG_HOME/ES-DE/collections"
+  dir_prep "$rd_home_path/ES-DE/custom_systems" "$XDG_CONFIG_HOME/ES-DE/custom_systems"
   log d "Generating roms system folders"
   start_esde --create-system-dirs
   update_splashscreens
@@ -28,5 +28,5 @@ if [[ "$action" == "postmove" ]]; then
   set_setting_value "$es_de_config" "ROMDirectory" "$roms_path" "es_de_config_es_settings"
   set_setting_value "$es_de_config" "MediaDirectory" "$downloaded_media_path" "es_de_config_es_settings"
   set_setting_value "$es_de_config" "UserThemeDirectory" "$themes_path" "es_de_config_es_settings"
-  dir_prep "$path/ES-DE/gamelists" "$XDG_CONFIG_HOME/ES-DE/gamelists"
+  dir_prep "$rd_home_path/ES-DE/gamelists" "$XDG_CONFIG_HOME/ES-DE/gamelists"
 fi
