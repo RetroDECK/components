23c4b1d247807a535261d41923c7a6e9450f2259
icenine451 <benjamin.r.shelton@protonmail.com>
2025-06-18 09:08:48 -0400

    Solarus component: Fix component_config variable usage and remove multi-user segments in component_prepare.sh Remove redundant code and fix $rd_shared_libs variable usage in component_launcher.sh

--- solarus/component_prepare.sh
+++ solarus/component_prepare.sh
@@ -2,21 +2,12 @@
 
 # Setting component name and path based on the directory name
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
-config="/app/retrodeck/components/$component_name/rd_config"
+component_config="/app/retrodeck/components/$component_name/rd_config"
 
 if [[ "$action" == "reset" ]]; then # Run reset-only commands
   log i "----------------------"
   log i "Preparing $component_name"
   log i "----------------------"
 
-  if [[ $multi_user_mode == "true" ]]; then # Multi-user actions
-  # Add Multiuser things and reset things
-
-  else # Single-user actions
   create_dir "$XDG_CONFIG_HOME/solarus"
-
-  fi
-  # Shared actions
-
-
 fi
