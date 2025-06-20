3fd0b302e5ae00058cb0713a751b75ca1138b199
icenine451 <benjamin.r.shelton@protonmail.com>
2025-06-20 11:08:23 -0400

    Vita3k component: Fix config file paths in component_functions.sh Fix extraneous recursive copy for single file in component_prepare.sh

--- vita3k/component_prepare.sh
+++ vita3k/component_prepare.sh
@@ -13,8 +13,8 @@ if [[ "$action" == "reset" ]]; then # Run reset-only commands
   # NOTE: the component is writing in "." so it must be placed in the rw filesystem. A symlink of the binary is already placed in /app/bin/Vita3K
   rm -rf "$XDG_CONFIG_HOME/Vita3K"
   create_dir "$XDG_CONFIG_HOME/Vita3K"
-  cp -fvr "$component_config/config.yml" "$vita3k_config" # component config
-  cp -fvr "$component_config/ux0" "$storage_path/psvita/Vita3K/" # User config
+  cp -fv "$component_config/config.yml" "$vita3k_config" # component config
+  cp -frv "$component_config/ux0" "$storage_path/psvita/Vita3K/" # User config
   set_setting_value "$vita3k_config" "pref-path" "$storage_path/psvita/Vita3K/" "vita3k"
   dir_prep "$saves_path/psvita/vita3k" "$storage_path/psvita/Vita3K/ux0/user/00/savedata" # Multi-user safe?
 fi
