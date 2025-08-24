1c5aa073c93b3649e3d527a053fedb1672f92390
Lazorne <thelazorne@protonmail.com>
2025-08-24 14:45:29 +0200

    Ruffle: removed dual --fullscreen command causing crash, bin folder correct target

--- ruffle/component_launcher.sh
+++ ruffle/component_launcher.sh
@@ -9,15 +9,15 @@ component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 log i "RetroDECK is now launching $component_name"
 log i "Ruffle is running: $arg"
 
-static_invoke="--config /var/data/ruffle --save-directory $saves_path/ruffle --fullscreen"
+static_invoke="--config /var/data/ruffle --save-directory $saves_path/ruffle"
 
 #Check if Steam Deck in Desktop Mode
 if [[ $(check_desktop_mode) == "true" ]]; then
     log d "Running Ruffle in Desktop Mode"
     log d "ruffle --graphics vulkan $static_invoke $@"
-    exec "$component_path/ruffle" --graphics vulkan $static_invoke "$@"
+    exec "$component_path/bin/ruffle" --graphics vulkan $static_invoke "$@"
 else
     log d "Running Ruffle in Gaming Mode"
     log d "ruffle --graphics gl --no-gui $static_invoke $@"
-    exec "$component_path/ruffle" --graphics gl --no-gui $static_invoke "$@"
+    exec "$component_path/bin/ruffle" --graphics gl --no-gui $static_invoke "$@"
 fi
