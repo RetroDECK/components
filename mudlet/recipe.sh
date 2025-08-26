d24a948315f8259e1fb0b09f938da75794a9e283
etjanster <thelazorne@protonmail.com>
2025-08-26 07:26:40 +0200

    mudlet source archive

--- mudlet/recipe.sh
+++ mudlet/recipe.sh
@@ -5,7 +5,7 @@ source "automation-tools/assembler.sh"
 # URL is a redirect, so we need to resolve it
 resolved_url=$(curl -L -o /dev/null -w '%{url_effective}' https://www.mudlet.org/download/42)
 
-assemble appimage "https://www.mudlet.org/wp-content/files/Mudlet.AppImage"
+assemble appimage "https://github.com/RetroDECK/components/tree/cooker/archive_sources/Mudlet 4.19.1.AppImage"
 
 # Custom Commands
 
