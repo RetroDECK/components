3abec4e14172a837b67e00517e2dd23cf2796b14
XargonWan <xargonwan@gmail.com>
2025-08-29 13:30:07 +0900

    fix(xemu): is now using qt6.8

--- xemu/component_launcher.sh
+++ xemu/component_launcher.sh
@@ -6,7 +6,7 @@ component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 unset QEMU_AUDIO_DRV
 
-export LD_LIBRARY_PATH="$component_path/lib:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs/qt-6.8/lib:${LD_LIBRARY_PATH}"
 
 log i "RetroDECK is now launching $component_name"
 log d "Library path is: $LD_LIBRARY_PATH"
