687aead5242d20a8c14e00ccc45d53c731033b43
XargonWan <xargonwan@gmail.com>
2025-08-29 09:44:06 +0900

    fix(component_launcher): update LD_LIBRARY_PATH to include qt-6.8 library path

--- ppsspp/component_launcher.sh
+++ ppsspp/component_launcher.sh
@@ -4,7 +4,7 @@
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-export LD_LIBRARY_PATH="$component_path/lib:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs/qt-6.8/lib:${LD_LIBRARY_PATH}"
 
 # Workaround for vDSO issues in some environments
 export LD_PRELOAD=""
