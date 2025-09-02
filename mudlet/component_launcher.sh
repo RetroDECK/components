4ab14f9c753ae21ac74db62963d06052a4e83b05
laz <thelazorne@protonmail.com>
2025-09-02 07:02:22 +0200

    Fixed launcher missing lib path, added component source txt

--- mudlet/component_launcher.sh
+++ mudlet/component_launcher.sh
@@ -4,7 +4,7 @@
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-export LD_LIBRARY_PATH="$component_path:$rd_shared_libs/qt-5.15/lib:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs/qt-5.15/lib:${LD_LIBRARY_PATH}"
 export QT_PLUGIN_PATH="$rd_shared_libs/qt-5.15/lib/plugins:${QT_PLUGIN_PATH}"
 
 log i "RetroDECK is now launching $component_name"
