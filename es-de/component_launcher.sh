1e5f7acd8248f1d86da889e4c8b30e0b355abd84
Lazorne <thelazorne@protonmail.com>
2025-10-01 17:43:29 +0200

    es-de component-lib

--- es-de/component_launcher.sh
+++ es-de/component_launcher.sh
@@ -4,8 +4,8 @@
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs/qt-6.7/lib:${LD_LIBRARY_PATH}"
-export QT_PLUGIN_PATH="$rd_shared_libs/qt-6.7/lib/plugins:${QT_PLUGIN_PATH}"
+export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs/qt-6.9:${LD_LIBRARY_PATH}"
+export QT_PLUGIN_PATH="$rd_shared_libs/qt-6.9/plugins:${QT_PLUGIN_PATH}"
 
 log i "RetroDECK is now launching $component_name"
 log d "Library path is: $LD_LIBRARY_PATH"
