063708b5b055f2f7da098c858013654392b1a455
etjanster <thelazorne@protonmail.com>
2025-10-02 07:49:26 +0200

    rpcs3, ra, primehack, ppsspp - libs

--- lindbergh/recipe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source "automation-tools/assembler.sh"
-
-assemble appimage "https://github.com/lindbergh-loader/lindbergh-loader/releases/latest/download/lindbergh-loader.AppImage"
-
-# Custom Commands
-
-finalize
