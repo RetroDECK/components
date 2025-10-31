3c736865c953fa8a96c7804ed2f96d0b78d003b2
Lazorne <thelazorne@protonmail.com>
2025-10-31 15:44:47 +0100

    dolphin cemu recipes

--- azahar/recipe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source "automation-tools/assembler.sh"
-
-assemble appimage "https://github.com/azahar-emu/azahar/releases/latest/download/azahar.AppImage"
-
-# Custom Commands
-
-finalize
