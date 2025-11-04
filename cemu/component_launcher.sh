e9f0b5a278b59a3208ee82f79610664f65bfdc7c
icenine451 <benjamin.r.shelton@protonmail.com>
2025-11-04 14:16:45 -0500

    Update Cemu launcher and recipe to reflect new Flatpak source.

--- cemu/component_launcher.sh
+++ cemu/component_launcher.sh
@@ -4,15 +4,10 @@
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-source "$component_path/apprun-hooks/linuxdeploy-plugin-checkrt.sh"
-log d "Loaded app run hooks in apprun-hooks/linuxdeploy-plugin-checkrt.sh"
-source "$component_path/apprun-hooks/linuxdeploy-plugin-gtk.sh"
-log d "Loaded app run hooks in apprun-hooks/linuxdeploy-plugin-gtk.sh"
-
 log i "RetroDECK is now launching $component_name"
 log d "Library path is: $LD_LIBRARY_PATH"
 log d "AppDir is: $component_path"
 
-export LD_LIBRARY_PATH="$component_path/lib:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs:${LD_LIBRARY_PATH}"
 
-exec "$component_path/usr/bin/Cemu" "$@"
+exec "$component_path/bin/Cemu_relwithdebinfo" "$@"
