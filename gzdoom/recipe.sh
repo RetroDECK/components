d948e2ae9321fcb5a1c538013d8e2473451f75cb
etjanster <thelazorne@protonmail.com>
2025-11-04 08:14:49 +0100

    more recipe fixes, broken []

--- gzdoom/recipe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source "automation-tools/assembler.sh"
-
-assemble appimage "https://github.com/flegald/GZDeck/releases/latest/download/GZDeck*.AppImage"
-
-# Custom Commands
-
-finalize
