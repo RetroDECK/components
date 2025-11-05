ce5cddd842731e2ff5c77712cdebdba6558594a5
Lazorne <thelazorne@protonmail.com>
2025-11-05 19:42:09 +0100

    lindbergh notes

--- lindbergh/component_launcher.sh
+++ lindbergh/component_launcher.sh
@@ -16,4 +16,4 @@ log d "Library path is: $LD_LIBRARY_PATH"
 log d "QT plugin path is: $QT_PLUGIN_PATH"
 log d "QT QPA PLATFORM plugin path is: $QT_QPA_PLATFORM_PLUGIN_PATH"
 
-exec "$component_path/usr/bin/lindbergh" "$@"
+exec "$component_path/bin/lindbergh" "$@"
