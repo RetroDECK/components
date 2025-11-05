31aecf4feeb98ab0868a66db85b81df6c5d448a8
Lazorne <thelazorne@protonmail.com>
2025-11-05 07:56:40 +0100

    azahar poc

--- azahar/component_launcher.sh
+++ azahar/component_launcher.sh
@@ -4,8 +4,9 @@
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs:${LD_LIBRARY_PATH}"
-export QT_PLUGIN_PATH="$rd_shared_libs:${QT_PLUGIN_PATH}"
+export LD_LIBRARY_PATH=export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs/org.kde.Platform/6.9/:${LD_LIBRARY_PATH}"
+export QT_PLUGIN_PATH="$rd_shared_libs/org.kde.Platform/6.9/lib/plugins/:${QT_PLUGIN_PATH}"
+export QT_QPA_PLATFORM_PLUGIN_PATH="$rd_shared_libs/org.kde.Platform/6.9/plugins/platforms/:${QT_QPA_PLATFORM_PLUGIN_PATH}"
 
 log i "RetroDECK is now launching $component_name"
 log d "Library path is: $LD_LIBRARY_PATH"
