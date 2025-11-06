a7dcf6334cdacd1ec509253fa9c3f187c9cd25c3
XargonWan <xargonwan@gmail.com>
2025-11-06 16:15:27 +0900

    feat(pcsx2): half migrated to alchemist build system, missing some assets and deep testing still

--- pcsx2/component_launcher.sh
+++ pcsx2/component_launcher.sh
@@ -5,12 +5,10 @@ component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 source "$component_path/apprun-hooks/linuxdeploy-plugin-qt-hook.sh"
-source "$component_path/apprun-hooks/default-to-x11.sh"
 
 export LD_LIBRARY_PATH="$component_path/lib:${LD_LIBRARY_PATH}"
 
 log i "RetroDECK is now launching $component_name"
 log d "Loaded app run hooks in apprun-hooks/linuxdeploy-plugin-qt-hook.sh"
-log d "Loaded app run hooks in apprun-hooks/default-to-x11.sh"
 
-exec "$component_path/usr/bin/pcsx2-qt" "$@"
+exec "$component_path/bin/pcsx2-qt" "$@"
