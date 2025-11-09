94d9d3c88efbd0aa23362e49498cfbc78ed59216
Lazorne <thelazorne@protonmail.com>
2025-11-09 18:07:53 +0100

    pico-8

--- pico-8/recipe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Just a dummy recipe to make the component assemblation work.
-
-source "automation-tools/assembler.sh"
-
-assemble local "$component/component_launcher.sh"
-
-finalize
