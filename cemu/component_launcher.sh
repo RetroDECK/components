0cd99d4bd6aa2178ab29b1cf5818f1348be0e92e
laz <thelazorne@protonmail.com>
2025-11-09 11:32:09 +0100

    launchers

--- cemu/component_launcher.sh
+++ cemu/component_launcher.sh
@@ -8,6 +8,8 @@ log i "RetroDECK is now launching $component_name"
 log d "Library path is: $LD_LIBRARY_PATH"
 log d "AppDir is: $component_path"
 
-export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs/org.kde.Platform/6.10/:${LD_LIBRARY_PATH}"
+export QT_PLUGIN_PATH="$rd_shared_libs/org.kde.Platform/6.10/plugins/:${QT_PLUGIN_PATH}"
+export QT_QPA_PLATFORM_PLUGIN_PATH="$rd_shared_libs/shared-libs/org.kde.Platform/6.10/plugins/platforms/:${QT_QPA_PLATFORM_PLUGIN_PATH}"
 
 exec "$component_path/bin/Cemu_relwithdebinfo" "$@"
--- rpcs3/component_launcher.sh
+++ rpcs3/component_launcher.sh
@@ -4,8 +4,9 @@
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs/qt-5.15-24.08:${LD_LIBRARY_PATH}"
-export QT_PLUGIN_PATH="$rd_shared_libs/qt-5.15-24.08/plugins:${QT_PLUGIN_PATH}"
+export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs/org.kde.Platform/6.10/:${LD_LIBRARY_PATH}"
+export QT_PLUGIN_PATH="$rd_shared_libs/org.kde.Platform/6.10/plugins/:${QT_PLUGIN_PATH}"
+export QT_QPA_PLATFORM_PLUGIN_PATH="$rd_shared_libs/shared-libs/org.kde.Platform/6.10/plugins/platforms/:${QT_QPA_PLATFORM_PLUGIN_PATH}"
 
 log i "RetroDECK is now launching $component_name"
 log d "Library path is: $LD_LIBRARY_PATH"
