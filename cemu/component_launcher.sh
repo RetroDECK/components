28780c12a288adc33da7b516ce68f5ab6e61d297
IceNine451 <benjamin.r.shelton@protonmail.com>
2025-11-11 21:35:47 -0500

    Update cemu recipe and launcher

--- cemu/component_launcher.sh
+++ cemu/component_launcher.sh
@@ -8,6 +8,6 @@ log i "RetroDECK is now launching $component_name"
 log d "Library path is: $LD_LIBRARY_PATH"
 log d "AppDir is: $component_path"
 
-export LD_LIBRARY_PATH="$component_path/lib:$component_path/shared-libs:$component_path/shared-libs/org.kde.Platform/6.10/:$component_path/shared-libs/org.gnome.Platform/49/:$component_path/shared-libs/org.freedesktop.Platform/25.08/:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="$component_path/lib:$component_path/shared-libs:$component_path/shared-libs/org.kde.Platform/6.10/:$component_path/shared-libs/org.gnome.Platform/49/:$component_path/shared-libs/org.freedesktop.Platform/24.08/:${LD_LIBRARY_PATH}"
 
 exec "$component_path/bin/Cemu_relwithdebinfo" "$@"
