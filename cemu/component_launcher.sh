fcb47b9b580aef2108050f2e5315e3091866c177
XargonWan <xargonwan@gmail.com>
2025-11-13 21:29:40 +0900

    fix(cemu): brought into playable status

--- cemu/component_launcher.sh
+++ cemu/component_launcher.sh
@@ -4,10 +4,9 @@
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
+export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs:$component_path/shared-libs:$component_path/shared-libs/org.kde.Platform/6.10/:$component_path/shared-libs/org.gnome.Platform/49/:$component_path/shared-libs/org.freedesktop.Platform/24.08/:${LD_LIBRARY_PATH}"
+
 log i "RetroDECK is now launching $component_name"
 log d "Library path is: $LD_LIBRARY_PATH"
-log d "AppDir is: $component_path"
-
-export LD_LIBRARY_PATH="$component_path/lib:$component_path/shared-libs:$component_path/shared-libs/org.kde.Platform/6.10/:$component_path/shared-libs/org.gnome.Platform/49/:$component_path/shared-libs/org.freedesktop.Platform/24.08/:${LD_LIBRARY_PATH}"
 
 exec "$component_path/bin/Cemu_relwithdebinfo" "$@"
