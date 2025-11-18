af0040c2cab8bca0949a8dbe32c078889d929a0b
icenine451 <benjamin.r.shelton@protonmail.com>
2025-11-18 09:53:57 -0500

    Internalize ES-DE launcher function for use in other scripts. [skip ci]

--- es-de/component_functions.sh
+++ es-de/component_functions.sh
@@ -3,3 +3,8 @@
 es_de_appdata_path="$XDG_CONFIG_HOME/ES-DE"
 es_de_config="$XDG_CONFIG_HOME/ES-DE/settings/es_settings.xml"
 es_de_logs_path="$XDG_CONFIG_HOME/ES-DE/logs"
+
+start_esde(){
+  log d "Starting ES-DE"
+  /bin/bash /app/retrodeck/components/es-de/component_launcher.sh "$@"
+}
