828b2a3bfb2d809c2fb19dd24cfcfc00536af652
XargonWan <xargonwan@gmail.com>
2025-11-25 16:30:41 +0900

    fix(azahar): update qt-config path and settings in component_prepare.sh [skip ci]

--- azahar/component_prepare.sh
+++ azahar/component_prepare.sh
@@ -4,18 +4,24 @@
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 component_config="/app/retrodeck/components/$component_name/rd_config"
 
+azahar_config_path="$XDG_CONFIG_HOME/azahar-emu"
+azahar_qtconfig="$azahar_config_path/qtconfig.ini"
+
 if [[ "$action" == "reset" ]]; then # Run reset-only commands
 
   log i "----------------------"
   log i "Preparing $component_name"
   log i "----------------------"
 
-  create_dir -d "$XDG_CONFIG_HOME/azahar/"
+  create_dir -d "$azahar_config_path"
+
   cp -fT "$component_config/qt-config.ini" "$azahar_config"
-  set_setting_value "$azahar_config" "nand_directory" "$saves_path/n3ds/azahar/nand/" "azahar"
-  set_setting_value "$azahar_config" "nand_directory" "$saves_path/n3ds/azahar/sdmc/" "azahar"
-  set_setting_value "$azahar_config" "Paths\gamedirs\3\path" "$roms_path/n3ds" "azahar"
-  set_setting_value "$azahar_config" "Paths\screenshotPath" "$screenshots_path/n3ds/azahar" "azahar"
+
+  set_setting_value "$azahar_qtconfig" "nand_directory" "$saves_path/n3ds/azahar/nand/" "azahar"
+  set_setting_value "$azahar_qtconfig" "nand_directory" "$saves_path/n3ds/azahar/sdmc/" "azahar"
+  set_setting_value "$azahar_qtconfig" "Paths\gamedirs\3\path" "$roms_path/n3ds" "azahar"
+  set_setting_value "$azahar_qtconfig" "Paths\screenshotPath" "$screenshots_path/n3ds/azahar" "azahar"
+
   create_dir "$screenshots_path/n3ds/azahar"
   create_dir "$saves_path/n3ds/azahar/nand/"
   create_dir "$saves_path/n3ds/azahar/sdmc/"
