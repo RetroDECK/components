bc0d8b59f956fa8d50b65a08b51b6331abe72339
XargonWan <xargonwan@gmail.com>
2025-11-26 11:23:27 +0900

    fix(azahar): correct path for qt-config.ini copy [skip ci]

--- azahar/component_prepare.sh
+++ azahar/component_prepare.sh
@@ -12,7 +12,7 @@ if [[ "$action" == "reset" ]]; then # Run reset-only commands
 
   create_dir -d "$azahar_config_path"
 
-  cp -fT "$component_config/qt-config.ini" "$azahar_config"
+  cp -fT "$component_config/qt-config.ini" "$azahar_config_path/qt-config.ini"
 
   set_setting_value "$azahar_qtconfig" "nand_directory" "$saves_path/n3ds/azahar/nand/" "azahar"
   set_setting_value "$azahar_qtconfig" "nand_directory" "$saves_path/n3ds/azahar/sdmc/" "azahar"
