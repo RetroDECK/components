5d6e10a485d303e1a50f658725e7b5a288ae0554
XargonWan <xargonwan@gmail.com>
2025-11-27 12:01:07 +0900

    fix(melonds): update setting values for BIOS and file paths to correct parameters [skip ci]

--- melonds/component_prepare.sh
+++ melonds/component_prepare.sh
@@ -11,11 +11,11 @@ if [[ "$action" == "reset" ]]; then # Run reset-only commands
 
   create_dir -d "$XDG_CONFIG_HOME/melonDS/"
   cp -fv "$component_config/melonDS.toml" "$melonds_config"
-  set_setting_value "$melonds_config" "BIOS9Path" "$bios_path/bios9.bin" "melonds"
-  set_setting_value "$melonds_config" "BIOS7Path" "$bios_path/bios7.bin" "melonds"
-  set_setting_value "$melonds_config" "FirmwarePath" "$bios_path/firmware.bin" "melonds"
-  set_setting_value "$melonds_config" "SaveFilePath" "$saves_path/nds/melonds" "melonds"
-  set_setting_value "$melonds_config" "SavestatePath" "$states_path/nds/melonds" "melonds"
+  set_setting_value "$melonds_config" "BIOS9Path" "$bios_path/bios9.bin" "DS" "melonds"
+  set_setting_value "$melonds_config" "BIOS7Path" "$bios_path/bios7.bin" "DS" "melonds"
+  set_setting_value "$melonds_config" "FirmwarePath" "$bios_path/firmware.bin" "DS" "melonds"
+  set_setting_value "$melonds_config" "SaveFilePath" "$saves_path/nds/melonds" "Instance0" "melonds"
+  set_setting_value "$melonds_config" "SavestatePath" "$states_path/nds/melonds" "Instance0" "melonds"
   create_dir "$saves_path/nds/melonds"
   create_dir "$states_path/nds/melonds"
   dir_prep "$bios_path" "$XDG_CONFIG_HOME/melonDS/bios"
@@ -23,9 +23,9 @@ fi
 
 if [[ "$action" == "postmove" ]]; then # Run only post-move commands
     dir_prep "$bios_path" "$XDG_CONFIG_HOME/melonDS/bios"
-    set_setting_value "$melonds_config" "BIOS9Path" "$bios_path/bios9.bin" "melonds"
-    set_setting_value "$melonds_config" "BIOS7Path" "$bios_path/bios7.bin" "melonds"
-    set_setting_value "$melonds_config" "FirmwarePath" "$bios_path/firmware.bin" "melonds"
-    set_setting_value "$melonds_config" "SaveFilePath" "$saves_path/nds/melonds" "melonds"
-    set_setting_value "$melonds_config" "SavestatePath" "$states_path/nds/melonds" "melonds"
+    set_setting_value "$melonds_config" "BIOS9Path" "$bios_path/bios9.bin" "DS" "melonds"
+    set_setting_value "$melonds_config" "BIOS7Path" "$bios_path/bios7.bin" "DS" "melonds"
+    set_setting_value "$melonds_config" "FirmwarePath" "$bios_path/firmware.bin" "DS" "melonds"
+    set_setting_value "$melonds_config" "SaveFilePath" "$saves_path/nds/melonds" "Instance0" "melonds"
+    set_setting_value "$melonds_config" "SavestatePath" "$states_path/nds/melonds" "Instance0" "melonds"
 fi
