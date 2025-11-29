7672d8718830d94a0771dc450611511b16e95ec5
XargonWan <xargonwan@gmail.com>
2025-11-29 23:24:46 +0900

    feat(portmaster): initialize environment variables and logging for component launcher

--- portmaster/component_launcher.sh
+++ portmaster/component_launcher.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+export LD_LIBRARY_PATH="$component_path/lib:$ffmpeg_path/25.08:$rd_shared_libs:$rd_shared_libs/org.kde.Platform/6.10/:$rd_shared_libs/org.gnome.Platform/49/:$rd_shared_libs/org.freedesktop.Platform/25.08/:${DEFAULT_LD_LIBRARY_PATH}"
+export QT_PLUGIN_PATH="$rd_shared_libs/org.kde.Platform/6.10/plugins/:${QT_PLUGIN_PATH}"
+export QT_QPA_PLATFORM_PLUGIN_PATH="$rd_shared_libs/org.kde.Platform/6.10/plugins/platforms/:${QT_QPA_PLATFORM_PLUGIN_PATH}"
+
+log i "RetroDECK is now launching $component_name"
+log d "Library path is: $LD_LIBRARY_PATH"
+log d "QT plugin path is: $QT_PLUGIN_PATH"
+
 exec "/var/data/PortMaster/PortMaster.sh" "$@"
