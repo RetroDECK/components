abb4f99f4743f86282ea34ba8de53d653238155c
XargonWan <xargonwan@gmail.com>
2025-12-02 11:55:31 +0900

    feat(framework): add removal of ponzu engine, akai_ponzu and kiroi_ponzu settings in component update

--- framework/component_update.sh
+++ framework/component_update.sh
@@ -496,6 +496,13 @@ if [[ $(check_version_is_older_than "$version_being_updated" "0.9.2b") == "true"
   fi
 fi
 
+if [[ $(check_version_is_older_than "$version_being_updated" "0.10.0b") == "true" ]]; then
+  ponzu_remove
+  # TODO: strip away "akai_ponzu": false, and "kiroi_ponzu": false, from retrodeck.json
+  delete_setting "$rd_conf" "akai_ponzu" "retrodeck" "options"
+  delete_setting "$rd_conf" "kiroi_ponzu" "retrodeck" "options"
+fi
+
 #######################################
 # These actions happen at every update
 #######################################
