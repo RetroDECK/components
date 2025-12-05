91d9ddf487740ecd527a67bded8bd84a81186e58
XargonWan <xargonwan@gmail.com>
2025-12-05 11:09:07 +0900

    fix(xroar): remove unnecessary config flag from xroar execution

--- xroar/component_launcher.sh
+++ xroar/component_launcher.sh
@@ -5,6 +5,7 @@ component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 export LD_LIBRARY_PATH="$component_path/lib:$ffmpeg_path/25.08:$rd_shared_libs:$rd_shared_libs/org.kde.Platform/6.10/:$rd_shared_libs/org.gnome.Platform/49/:$rd_shared_libs/org.freedesktop.Platform/25.08/:${DEFAULT_LD_LIBRARY_PATH}"
+export XROAR_CONF_PATH="$XDG_CONFIG_HOME/xroar"
 
 log i "RetroDECK is now launching $component_name"
 log d "Library path is: $LD_LIBRARY_PATH"
@@ -12,4 +13,4 @@ log d "AppDir is: $component_path"
 
 log d "Executing: $component_path/bin/xroar -c $xroar_config $*"
 
-exec "$component_path/bin/xroar" -c "$xroar_config" -joy-db-file "gamecontrollerdb.txt" "$@"
+exec "$component_path/bin/xroar" -joy-db-file "gamecontrollerdb.txt" "$@"
