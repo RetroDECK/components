4d2c610406056f191a33910bbd7f969d0d573f1f
Lazorne <thelazorne@protonmail.com>
2025-12-11 18:50:41 +0100

    wrong cheat path ppsspp update [skip ci]

--- duckstation/component_functions.sh
+++ duckstation/component_functions.sh
@@ -2,3 +2,4 @@
 
 duckstation_config="$XDG_CONFIG_HOME/duckstation/settings.ini"
 duckstation_rd_config_dir="$rd_components/duckstation/rd_config"
+duckstation_textures_path="$XDG_CONFIG_HOME/duckstation/texturesg"
