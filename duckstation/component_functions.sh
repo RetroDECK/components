6273eb520d6cdd9c31bef8a7869a36609ee247ee
etjanster <thelazorne@protonmail.com>
2025-12-12 10:31:26 +0100

    duck functions [skip ci]

--- duckstation/component_functions.sh
+++ duckstation/component_functions.sh
@@ -2,4 +2,11 @@
 
 duckstation_config="$XDG_CONFIG_HOME/duckstation/settings.ini"
 duckstation_rd_config_dir="$rd_components/duckstation/rd_config"
-duckstation_textures_path="$XDG_CONFIG_HOME/duckstation/texturesg"
+duckstation_textures_path="$XDG_CONFIG_HOME/duckstation/textures"
+duckstation_screenshots_path="$XDG_CONFIG_HOME/duckstation/screenshots"
+duckstation_videos_path="$XDG_CONFIG_HOME/duckstation/videos"
+duckstation_gamesettings_path="$XDG_CONFIG_HOME/duckstation/gamesettings"
+duckstation_shaders_path="$XDG_CONFIG_HOME/duckstation/shaders"
+duckstation_dump_textures_path="$XDG_CONFIG_HOME/duckstation/dump/textures"
+duckstation_dump_audio_path="$XDG_CONFIG_HOME/duckstation/dump/audio"
+duckstation_covers_path="$XDG_CONFIG_HOME/duckstation/covers"
