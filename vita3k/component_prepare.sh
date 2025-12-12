6a30a89e632a2118b2f20cd8ee9f3673fc51ce24
Lazorne <thelazorne@protonmail.com>
2025-12-12 06:14:56 +0100

    fixed wrong dirs vita3k [skip ci]

--- vita3k/component_prepare.sh
+++ vita3k/component_prepare.sh
@@ -14,13 +14,19 @@ if [[ "$action" == "reset" ]]; then # Run reset-only commands
   rm -rf "$XDG_CONFIG_HOME/Vita3K"
   create_dir "$XDG_CONFIG_HOME/Vita3K"
   cp -fv "$component_config/config.yml" "$vita3k_config" # component config
-  create_dir "$storage_path/psvita/Vita3K/"
-  cp -frv "$component_config/ux0" "$storage_path/psvita/Vita3K/" # User config
-  set_setting_value "$vita3k_config" "pref-path" "$storage_path/psvita/Vita3K/" "vita3k"
-  dir_prep "$saves_path/psvita/vita3k" "$storage_path/psvita/Vita3K/ux0/user/00/savedata" # Multi-user safe?
+  create_dir "$storage_path/Vita3K/"
+  cp -frv "$component_config/ux0" "$storage_path/Vita3K/" # User config
+  set_setting_value "$vita3k_config" "pref-path" "$storage_path/Vita3K/" "vita3k"
+  dir_prep "$saves_path/psvita/vita3k" "$storage_path/Vita3K/ux0/user/00/savedata" # Multi-user safe?
+  dir_prep "$texture_packs_path/Vita3K/import" "$storage_path/Vita3K/ux0/textures/import" # Textures 
+  dir_prep "$storage_path/Vita3K/lang"  "$XDG_DATA_HOME/Vita3K/lang"
+  dir_prep "$storage_path/Vita3K/patch"  "$XDG_DATA_HOME/Vita3K/patch"
 fi
 
 if [[ "$action" == "postmove" ]]; then # Run only post-move commands
-  dir_prep "$saves_path/psvita/vita3k" "$storage_path/psvita/Vita3K/ux0/user/00/savedata" # Multi-user safe?
+  dir_prep "$saves_path/psvita/vita3k" "$storage_path/Vita3K/ux0/user/00/savedata" # Multi-user safe?
+  dir_prep "$texture_packs_path/Vita3K/import" "$storage_path/Vita3K/ux0/textures/import" # Textures 
+  dir_prep "$storage_path/Vita3K/lang"  "$XDG_DATA_HOME/Vita3K/lang"
+  dir_prep "$storage_path/Vita3K/patch"  "$XDG_DATA_HOME/Vita3K/patch"
   set_setting_value "$vita3k_config" "pref-path" "$storage_path/psvita/Vita3K/" "vita3k"
 fi
