4c2b1b43044d68b884def1bc35056ab649e588d5
lazzite <thelazorne@protonmail.com>
2025-12-21 07:26:06 +0100

    moon [skip ci]

---
+++ archive_later/moonlight/component_launcher.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Setting component name and path based on the directory name
+component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
+component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+log i "RetroDECK is now launching $component_name"
+log d "Library path is: $LD_LIBRARY_PATH"
+log d "QT plugin path is: $QT_PLUGIN_PATH"
+log d "QT QPA PLATFORM plugin path is: $QT_QPA_PLATFORM_PLUGIN_PATH"
+
+# Launch
+exec "$component_path/bin/moonlight "$@"
