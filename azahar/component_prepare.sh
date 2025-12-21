57c72301634bb315d0ec0904c3c872e8e062636f
laz <thelazorne@protonmail.com>
2025-12-21 18:10:19 +0100

    azhar folder naming errors [skip ci]

--- azahar/component_prepare.sh
+++ azahar/component_prepare.sh
@@ -20,16 +20,16 @@ if [[ "$action" == "reset" ]]; then # Run reset-only commands
   set_setting_value "$azahar_qtconfig" "nand_directory" "$saves_path/n3ds/azahar/nand/" "azahar"
   set_setting_value "$azahar_qtconfig" "sdmc_directory" "$saves_path/n3ds/azahar/sdmc/" "azahar"
   set_setting_value "$azahar_qtconfig" "Paths\gamedirs\3\path" "$roms_path/n3ds" "azahar"
-  set_setting_value "$azahar_qtconfig" "Paths\screenshotPath" "$screenshots_path/n3ds/azahar" "azahar"
+  set_setting_value "$azahar_qtconfig" "Paths\screenshotPath" "$screenshots_path/Azahar" "azahar"
 
-  create_dir "$screenshots_path/n3ds/azahar"
+  create_dir "$screenshots_path/Azahar"
   create_dir "$saves_path/n3ds/azahar/nand/"
   create_dir "$saves_path/n3ds/azahar/sdmc/"
 
-  dir_prep "$mods_path/azahar/mods" "$azahar_mods_path"
-  dir_prep "$texture_packs_path/azahar/textures" "$azahar_textures_path"
-  dir_prep "$shaders_path/azahar/" "$azahar_shaders_path"
-  dir_prep "$logs_path/azahar/" "$azahar_logs_path"
-  dir_prep "$cheats/azahar/" "$azahar_cheats_path"
+  dir_prep "$mods_path/Azahar/mods" "$azahar_mods_path"
+  dir_prep "$texture_packs_path/Azahar/textures" "$azahar_textures_path"
+  dir_prep "$shaders_path/Azahar/" "$azahar_shaders_path"
+  dir_prep "$logs_path/Azahar/" "$azahar_logs_path"
+  dir_prep "$cheats/Azahar/" "$azahar_cheats_path"
 
 fi
