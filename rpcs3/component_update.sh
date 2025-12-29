7262e42f0d98a472b860384bdf63cea38f50b0b7
XargonWan <xargonwan@gmail.com>
2025-12-29 21:18:59 +0900

    fix(rpcs3): migrate RPCS3 storage folders and update vfs configuration for version 0.10.0b

--- rpcs3/component_update.sh
+++ rpcs3/component_update.sh
@@ -41,3 +41,21 @@ if [[ $(check_version_is_older_than "$version_being_updated" "0.8.0b") == "true"
   dir_prep "$saves_path/ps3/rpcs3" "$bios_path/rpcs3/dev_hdd0/home/00000001/savedata"
   log i "RPCS3 saves migration completed, a backup was made here: \"$backups_folder/$(date +"%0m%0d")_rpcs3_save_data.zip\"."
 fi
+
+if [[ $(check_version_is_older_than "$version_being_updated" "0.10.0b") == "true" ]]; then
+
+  # Since in 0.10.0b we added the storage folder we need to migrate the folders
+  sed -i 's^\^$(EmulatorDir): .*^$(EmulatorDir): '"$storage_path/rpcs3/"'^' "$rpcs3_config_vfs"
+  dir_prep "$saves_path/ps3/rpcs3" "$storage_path/rpcs3/dev_hdd0/home/00000001/savedata"
+  dir_prep "$states_path/ps3/rpcs3" "$XDG_CONFIG_HOME/rpcs3/savestates"
+  move "$bios_folder/rpcs3/dev_hdd0" "$storage_path/rpcs3/dev_hdd0"
+  move "$bios_folder/rpcs3/dev_hdd1" "$storage_path/rpcs3/dev_hdd1"
+  move "$bios_folder/rpcs3/dev_flash" "$storage_path/rpcs3/dev_flash"
+  move "$bios_folder/rpcs3/dev_flash2" "$storage_path/rpcs3/dev_flash2"
+  move "$bios_folder/rpcs3/dev_flash3" "$storage_path/rpcs3/dev_flash3"
+  move "$bios_folder/rpcs3/dev_bdvd" "$storage_path/rpcs3/dev_bdvd"
+  move "$bios_folder/rpcs3/dev_usb000" "$storage_path/rpcs3/dev_usb000"
+  dir_prep "$storage_path/rpcs3/captures" "$XDG_CONFIG_HOME/rpcs3/captures"
+  dir_prep "$storage_path/rpcs3/patches" "$XDG_CONFIG_HOME/rpcs3/patches"
+
+fi
