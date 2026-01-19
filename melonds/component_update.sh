f3a96d10bd51c1a5dcf1bef538a92837fad237d0
IceNine451 <benjamin.r.shelton@protonmail.com>
2026-01-19 09:57:29 -0500

    Temporary workarounds for MelonDS config setup until the new format can be handled by the Framework [skip ci]

--- melonds/component_update.sh
+++ melonds/component_update.sh
@@ -8,8 +8,10 @@ if [[ $(check_version_is_older_than "$version_being_updated" "0.10.1b") == "true
 
   log i "0.10.1b Upgrade - Fix Bios Path: MelonDS"
 
-  set_setting_value "$melonds_config" "BIOS9Path" "$bios_path/bios9.bin" "DS" "melonds"
-  set_setting_value "$melonds_config" "BIOS7Path" "$bios_path/bios7.bin" "DS" "melonds"
-  set_setting_value "$melonds_config" "FirmwarePath" "$bios_path/firmware.bin" "DS" "melonds"
-
+#   set_setting_value "$melonds_config" "BIOS9Path" "$bios_path/bios9.bin" "DS" "melonds"
+#   set_setting_value "$melonds_config" "BIOS7Path" "$bios_path/bios7.bin" "DS" "melonds"
+#   set_setting_value "$melonds_config" "FirmwarePath" "$bios_path/firmware.bin" "DS" "melonds"
+  sed -i "s#RETRODECKSTATESDIR#${states_path}#g" "$melonds_config"
+  sed -i "s#RETRODECKSAVESDIR#${saves_path}#g" "$melonds_config"
+  sed -i "s#RETRODECKBIOSDIR#${bios_path}#g" "$melonds_config"
 fi
