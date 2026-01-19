316941a8b5dd730cf45822134d8dec3d4566d335
etjanster <thelazorne@protonmail.com>
2026-01-19 07:27:58 +0100

    melonds bios path [skip ci]

--- melonds/component_update.sh
+++ melonds/component_update.sh
@@ -4,3 +4,12 @@
 # These actions happen conditionally based on the version being upgraded
 #########################################################################
 
+if [[ $(check_version_is_older_than "$version_being_updated" "0.10.1b") == "true" ]]; then
+
+  log i "0.10.1b Upgrade - Fix Bios Path: MelonDS"
+
+  set_setting_value "$melonds_config" "BIOS9Path" "$bios_path/bios9.bin" "DS" "melonds"
+  set_setting_value "$melonds_config" "BIOS7Path" "$bios_path/bios7.bin" "DS" "melonds"
+  set_setting_value "$melonds_config" "FirmwarePath" "$bios_path/firmware.bin" "DS" "melonds"
+
+fi
