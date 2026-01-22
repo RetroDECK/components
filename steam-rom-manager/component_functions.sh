b31a3afec837e996fe55cfe6b09dc532902e0bc5
icenine451 <benjamin.r.shelton@protonmail.com>
2026-01-22 10:00:02 -0500

    Move SRM-specific functions to SRM component [skip ci]

--- steam-rom-manager/component_functions.sh
+++ steam-rom-manager/component_functions.sh
@@ -397,3 +397,37 @@ decode_filename() {
       -e 's/&quot;/"/g' \
       -e 's/&#39;/'"'"'/g'
 }
+
+install_retrodeck_controller_profile() {
+  # This function will install the needed files for the custom RetroDECK controller profile
+  # NOTE: These files need to be stored in shared locations for Steam, outside of the normal RetroDECK folders and should always be an optional user choice
+  # BIGGER NOTE: As part of this process, all emulators will need to have their configs hard-reset to match the controller mappings of the profile
+  # USAGE: install_retrodeck_controller_profile
+  if [[ -d "$HOME/.steam/steam/controller_base/templates/" ]]; then
+    if [[ -d "$HOME/.steam/steam/controller_base/templates/" ]]; then # If a normal binary Steam install exists
+      rsync -rlD --mkpath "/app/retrodeck/binding_icons/" "$HOME/.steam/steam/tenfoot/resource/images/library/controller/binding_icons/"
+      rsync -rlD --mkpath "$rd_core_files/controller_configs/" "$HOME/.steam/steam/controller_base/templates/"
+    fi
+    if [[ -d "$HOME/.var/app/com.valvesoftware.Steam/.steam/steam/controller_base/templates/" ]]; then # If a Flatpak Steam install exists
+      rsync -rlD --mkpath "/app/retrodeck/binding_icons/" "$HOME/.var/app/com.valvesoftware.Steam/.steam/steam/tenfoot/resource/images/library/controller/binding_icons/"
+      rsync -rlD --mkpath "$rd_core_files/controller_configs/" "$HOME/.var/app/com.valvesoftware.Steam/.steam/steam/controller_base/templates/"
+    fi
+  else
+    configurator_generic_dialog "RetroDECK - Install: Steam Controller Templates" "The target directories for the controller profile do not exist.\n\nThis may occur if <span foreground='$purple'><b>Steam is not installed</b></span> or if the location does not have <span foreground='$purple'><b>read permissions</b></span>."
+  fi
+}
+
+install_retrodeck_controller_profile_and_add_to_steam() {
+  install_retrodeck_controller_profile
+  add_retrodeck_to_steam
+  
+  rd_zenity --question --no-wrap --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" --title "RetroDECK Initial Install - Steam Synchronization" --cancel-label="No" --ok-label "Yes" \
+    --text="Enable Steam synchronization?\n\nThis will scan your games for any <span foreground='$purple'><b>Favorited</b></span> games in ES-DE and add them to your Steam library as individual entries.\n\nYou will need to restart Steam for the changes to take effect."
+
+  if [[ $? == 0 ]]; then
+    configurator_enable_steam_sync
+  fi
+  if [[ $(get_setting_value "$rd_conf" "steam_sync" retrodeck "options") =~ (flatpak) ]]; then # If Flatpak Steam, warn about permission
+    configurator_generic_dialog "RetroDeck Configurator - Steam Flatpak Warning" "You are using the <span foreground='purple'><b>Flatpak Version of Steam</b></span>.\n\nTo allow RetroDECK to launch, Steam must be granted the following permission:\n<span foreground='purple'><b>org.freedesktop.Flatpak</b></span>\n\nPlease read the RetroDECK wiki for instructions"
+  fi
+}
