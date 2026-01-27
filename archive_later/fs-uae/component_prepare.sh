34e701cb90172e52517fcbc8a06c9a231649d65a
etjanster <thelazorne@protonmail.com>
2026-01-27 07:12:09 +0100

    amiga [skip ci]

--- archive_later/fs-uae/component_prepare.sh
+++ archive_later/fs-uae/component_prepare.sh
@@ -9,6 +9,7 @@ if [[ "$action" == "reset" ]]; then # Run reset-only commands
   log i "Preparing $component_name"
   log i "----------------------"
 
+  create_dir -d "$XDG_CONFIG_HOME/FS-UAE/"
   create_dir "$storage_path/FS-UAE/AmigaVision/shared"
 
 fi
