2b6b0a13e97f556e0a8b2f6964ecefe7c6174ef5
icenine451 <benjamin.r.shelton@protonmail.com>
2026-01-30 11:34:21 -0500

    Rework retroarch_updater to include shipped filters and update files in userland locations rather than redundant symlinks [skip ci]

--- retroarch/component_functions.sh
+++ retroarch/component_functions.sh
@@ -2,7 +2,7 @@
 
 retroarch_config="$XDG_CONFIG_HOME/retroarch/retroarch.cfg"
 retroarch_config_core_options="$XDG_CONFIG_HOME/retroarch/retroarch-core-options.cfg"
-retroarch_config_scummvm="$XDG_CONFIG_HOME/retroarch/system/scummvm.ini"
+retroarch_config_scummvm="$bios_path/scummvm.ini"
 retroarch_cores_path="$XDG_CONFIG_HOME/retroarch/cores"
 retroarch_extras_path="$rd_components/retroarch/rd_extras"
 retroarch_rd_config_dir="$rd_components/retroarch/rd_config"
@@ -14,14 +14,18 @@ retroarch_updater() {
   log i "Running RetroArch updater"
 
   log i "Updating overlays/borders..."
-  create_dir "$XDG_CONFIG_HOME/retroarch/overlays"
-  tar -xzf "$retroarch_extras_path/overlays.tar.gz" -C "$XDG_CONFIG_HOME/retroarch/overlays" --overwrite && log d "RetroArch overlays and borders updated correctly"
+  create_dir "$borders_path/retroarch/overlays"
+  tar -h -xzf "$retroarch_extras_path/overlays.tar.gz" -C "$borders_path/retroarch/overlays" --overwrite && log d "RetroArch overlays and borders updated correctly"
 
   log i "Updating shaders..."
-  create_dir "$XDG_CONFIG_HOME/retroarch/shaders"
-  tar -xzf "$retroarch_extras_path/shaders.tar.gz" -C "$XDG_CONFIG_HOME/retroarch/shaders" --overwrite && log d "RetroArch shaders updated correctly"
+  create_dir "$shaders_path/retroarch/shaders"
+  tar -h -xzf "$retroarch_extras_path/shaders.tar.gz" -C "$shaders_path/retroarch/shaders" --overwrite && log d "RetroArch shaders updated correctly"
+
+  log i "Updating filters..."
+  create_dir "$shaders_path/retroarch/filters"
+  tar -h -xzf "$retroarch_extras_path/filters.tar.gz" -C "$shaders_path/retroarch/filters" --overwrite && log d "RetroArch shaders updated correctly"
 
   log i "Updating cheats..."
   create_dir "$cheats_path/retroarch"
-  tar -xzf "$retroarch_extras_path/cheats.tar.gz" -C "$cheats_path/retroarch" --overwrite && log d "RetroArch cheats updated correctly"
+  tar -h -xzf "$retroarch_extras_path/cheats.tar.gz" -C "$cheats_path/retroarch" --overwrite && log d "RetroArch cheats updated correctly"
 }
