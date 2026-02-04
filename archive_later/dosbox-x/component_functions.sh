d2a27e0ed3c9f8b3629694724ec126ad1a03de09
icenine451 <benjamin.r.shelton@protonmail.com>
2026-02-04 09:47:24 -0500

    Remove archive components to keep repo clean. These should be broken out into their own building branches and merged into the version branch when they are deemed ready for testing.

--- archive_later/dosbox-x/component_functions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dosbox_x_config="$XDG_CONFIG_HOME/dosbox-x/dosbox-x.conf"
-dosbox_x_mapper="$XDG_CONFIG_HOME/dosbox-x/mapper-dosbox-x.map"
-dosbox_x_os_configs_dir="$component_config/os_configs"
--- archive_later/fs-uae/component_functions.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-amigavision_config="$XDG_CONFIG_HOME/FS-UAE/AmigaVision.fs-uae"
-custom_config="$XDG_CONFIG_HOME/FS-UAE/custom.fs-uae"
