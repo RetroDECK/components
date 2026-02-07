5edd490ab8c96343177745771ac0e866b4f9da2c
lazzite <thelazorne@protonmail.com>
2026-02-07 12:16:21 +0100

    right quake [skip ci]

---
+++ archive_later/ironwail/component_launcher.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Setting component name and path based on the directory name
+component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
+component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+# Set LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="$component_path/lib:$ffmpeg_path/25.08:$rd_shared_libs:${DEFAULT_LD_LIBRARY_PATH}"
+export QT_PLUGIN_PATH="${QT_PLUGIN_PATH}"
+export QT_QPA_PLATFORM_PLUGIN_PATH="${QT_QPA_PLATFORM_PLUGIN_PATH}"
+
+log i "RetroDECK is now launching $component_name"
+log d "Library path is: $LD_LIBRARY_PATH"
+log d "QT plugin path is: $QT_PLUGIN_PATH"
+log d "QT QPA PLATFORM plugin path is: $QT_QPA_PLATFORM_PLUGIN_PATH"
+
+# Launch
+exec "$component_path/bin/ironwail" "$@"
