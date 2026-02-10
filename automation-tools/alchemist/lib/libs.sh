5fe9a98ae910dde093de784bc3c0979afa2bf2c5
icenine451 <benjamin.r.shelton@protonmail.com>
2026-02-10 14:55:57 -0500

    Add relative path handling to lib gathering and properly crash out when required args are missing. [skip ci]

--- automation-tools/alchemist/lib/libs.sh
+++ automation-tools/alchemist/lib/libs.sh
@@ -151,7 +151,9 @@ gather_lib() {
 }
 
 process_gather_lib() {
-  parse_gather_lib_args "$@"
+  if ! parse_gather_lib_args "$@"; then
+    return 1
+  fi
 
   if ! gather_lib "$name" "$dest" "$runtime_name" "$runtime_version" "$source"; then
     log error "Gathering component library $name failed"
