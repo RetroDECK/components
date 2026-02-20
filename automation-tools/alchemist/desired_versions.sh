433546e37dac5eb9f3b758a7ef70028e75579f88
icenine451 <benjamin.r.shelton@protonmail.com>
2026-02-20 14:40:34 -0500

    Update Vita3K desired version [skip ci]

--- automation-tools/alchemist/desired_versions.sh
+++ automation-tools/alchemist/desired_versions.sh
@@ -344,7 +344,7 @@ export DESIRED_QT5_RUNTIME_VERSION="5.15-25.08"
 # Link: https://github.com/RetroDECK/Vita3K-bin/releases
 # ------------------------------------------------------------------------------
 # MAIN (Stable)
-  export VITA3K_DESIRED_VERSION="3912"
+  export VITA3K_DESIRED_VERSION="3924"
 
 # COOKER (Override)
 # export VITA3K_DESIRED_VERSION="latest"
