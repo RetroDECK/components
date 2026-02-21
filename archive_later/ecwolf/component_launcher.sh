6964349a0a357309379f3d7a2e09de3a75d22178
laz <thelazorne@protonmail.com>
2026-02-21 10:48:42 +0100

    wiki

--- archive_later/ecwolf/component_launcher.sh
+++ archive_later/ecwolf/component_launcher.sh
@@ -12,4 +12,4 @@ log d "QT plugin path is: $QT_PLUGIN_PATH"
 log d "QT QPA PLATFORM plugin path is: $QT_QPA_PLATFORM_PLUGIN_PATH"
 
 # Launch
-exec "$component_path/bin/ecwolf" "$@"
+exec "$component_path/bin/ecwolf" --config /var/config/ecwolf/ecwolf.cfg" "$@"
