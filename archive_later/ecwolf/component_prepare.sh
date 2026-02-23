d464627d96a381d2f9c7f6e7a2e437791195d4a6
lazzite <thelazorne@protonmail.com>
2026-02-23 15:55:43 +0100

    ecwolf [skip ci]

--- archive_later/ecwolf/component_prepare.sh
+++ archive_later/ecwolf/component_prepare.sh
@@ -10,8 +10,8 @@ if [[ "$action" == "reset" ]]; then # Run reset-only commands
   log i "----------------------"
 
   cp -fT "$component_config/ecwolf.cfg" "$ecwolf_config_path/ecwolf.cfg"
-  create_dir "$roms_path/wolf/Wolfenstein 3D (Full).wolf"
-  create_dir "$roms_path/wolf/Wolfenstein 3D (Shareware).wolf"
+  create_dir "$roms_path/wolf/Wolfenstein 3D - Full.wolf"
+  create_dir "$roms_path/wolf/Wolfenstein 3D - Shareware.wolf"
   create_dir "$roms_path/wolf/Spear of Destiny.wolf"
   create_dir "$roms_path/wolf/Mission Pack 1.wolf"
   create_dir "$roms_path/wolf/Mission Pack 2.wolf"
@@ -24,8 +24,8 @@ if [[ "$action" == "postmove" ]]; then # Run commands that apply to both resets 
   log i "Post-moving $component_name"
   log i "----------------------"
 
-  create_dir "$roms_path/wolf/Wolfenstein 3D (Full).wolf"
-  create_dir "$roms_path/wolf/Wolfenstein 3D (Shareware).wolf"
+  create_dir "$roms_path/wolf/Wolfenstein 3D - Full.wolf"
+  create_dir "$roms_path/wolf/Wolfenstein 3D - Shareware.wolf"
   create_dir "$roms_path/wolf/Spear of Destiny.wolf"
   create_dir "$roms_path/wolf/Mission Pack 1.wolf"
   create_dir "$roms_path/wolf/Mission Pack 2.wolf"
