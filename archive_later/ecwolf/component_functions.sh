c227de5ad1ce74c44abd5086048f11b8d4324760
lazzite <thelazorne@protonmail.com>
2026-02-23 21:01:01 +0100

    ecwolf [skip ci]

--- archive_later/ecwolf/component_functions.sh
+++ archive_later/ecwolf/component_functions.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
+# Paths
+
 ecwolf_config_path="$XDG_CONFIG_HOME/ecwolf"
+ecwolf_saves_path="$XDG_DATA_HOME/ecwolf"
+ecwolf_gamefiles_symlinks_path="$XDG_DATA_HOME/ecwolf"
+
+# Wolfenstein 3D
+
+ecwolf_config_wolfenstein_shareware="$XDG_CONFIG_HOME/ecwolf/ecwolf_wolf_share_wl1.cfg"
+cwolf_config_wolfenstein_full="$XDG_CONFIG_HOME/ecwolf/ecwolf_wolf_full_wl6.cfg"
+
+# Spear of Destiny
+
+ecwolf_config_spear_full="$XDG_CONFIG_HOME/ecwolf/ecwolf_sod_full_sod.cfg"
+ecwolf_config_spear_demo="$XDG_CONFIG_HOME/ecwolf/ecwolf_sod_demo_sdm.cfg"
+ecwolf_config_spear_misson1="$XDG_CONFIG_HOME/ecwolf/ecwolf_sod_sd1.cfg"
+ecwolf_config_spear_misson3="$XDG_CONFIG_HOME/ecwolf/ecwolf_sod_sd2.cfg"
+ecwolf_config_spear_misson2="$XDG_CONFIG_HOME/ecwolf/ecwolf_sod_sd3.cfg"
+
+# Super 3-D Noah's Ark
+
+ecwolf_config_full_n3d="$XDG_CONFIG_HOME/ecwolf/ecwolf_noah_full_n3d.cfg"
+
