e7d93c6fa36ba3c53677cbea64b1bec6ad468d88
etjanster <thelazorne@protonmail.com>
2026-02-25 08:09:33 +0100

    ecwolf test [skip ci]

--- archive_later/ecwolf/component_functions.sh
+++ archive_later/ecwolf/component_functions.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
+# Config
+
+ecwolf_config="$XDG_CONFIG_HOME/ecwolf_rd.cfg"
+
 # Paths
 
 ecwolf_config_path="$XDG_CONFIG_HOME/ecwolf"
 ecwolf_saves_path="$XDG_DATA_HOME/ecwolf/saves"
-ecwolf_gamefiles_symlinks_path="$XDG_DATA_HOME/ecwolf"
+ecwolf_roms_path="$roms_path/wolf"
 
-# Wolfenstein 3D
 
-ecwolf_config_wolfenstein_shareware="$XDG_CONFIG_HOME/ecwolf/ecwolf_wolf_share_wl1.cfg"
-cwolf_config_wolfenstein_full="$XDG_CONFIG_HOME/ecwolf/ecwolf_wolf_full_wl6.cfg"
-
-# Spear of Destiny
-
-ecwolf_config_spear_full="$XDG_CONFIG_HOME/ecwolf/ecwolf_sod_full_sod.cfg"
-ecwolf_config_spear_demo="$XDG_CONFIG_HOME/ecwolf/ecwolf_sod_demo_sdm.cfg"
-ecwolf_config_spear_misson1="$XDG_CONFIG_HOME/ecwolf/ecwolf_sod_sd1.cfg"
-ecwolf_config_spear_misson3="$XDG_CONFIG_HOME/ecwolf/ecwolf_sod_sd2.cfg"
-ecwolf_config_spear_misson2="$XDG_CONFIG_HOME/ecwolf/ecwolf_sod_sd3.cfg"
-
-# Super 3-D Noah's Ark
-
-ecwolf_config_full_n3d="$XDG_CONFIG_HOME/ecwolf/ecwolf_noah_full_n3d.cfg"
 
--- archive_later/ecwolf/component_prepare.sh
+++ archive_later/ecwolf/component_prepare.sh
@@ -10,23 +10,6 @@ if [[ "$action" == "reset" ]]; then # Run reset-only commands
   log i "----------------------"
 
   dir_prep "$saves_path/ecwolf" "$ecwolf_saves_path"
-  create_dir "$ecwolf_saves_path/wl6"
-  create_dir "$ecwolf_saves_path/wl1"
-  create_dir "$ecwolf_saves_path/sod"
-  create_dir "$ecwolf_saves_path/sdm"
-  create_dir "$ecwolf_saves_path/sd1"
-  create_dir "$ecwolf_saves_path/sd2"
-  create_dir "$ecwolf_saves_path/sd3"
-  create_dir "$ecwolf_saves_path/n3d"
-
-  create_dir "$roms_path/wolf/Wolfenstein 3D - Full.wolf"
-  create_dir "$roms_path/wolf/Wolfenstein 3D - Shareware.wolf"
-  create_dir "$roms_path/wolf/Spear of Destiny - Full.wolf"
-  create_dir "$roms_path/wolf/Spear of Destiny - Demo.wolf"
-  create_dir "$roms_path/wolf/Spear of Destiny - Mission Pack 1.wolf"
-  create_dir "$roms_path/wolf/Spear of Destiny - Mission Pack 2.wolf"
-  create_dir "$roms_path/wolf/Spear of Destiny - Mission Pack 3.wolf"
-  create_dir "$roms_path/wolf/Super 3D Noahs Ark.wolf"
 
 fi
 
@@ -37,12 +20,4 @@ if [[ "$action" == "postmove" ]]; then # Run commands that apply to both resets 
 
   dir_prep "$saves_path/ecwolf" "$ecwolf_saves_path"
   
-  create_dir "$roms_path/wolf/Wolfenstein 3D - Full.wolf"
-  create_dir "$roms_path/wolf/Wolfenstein 3D - Shareware.wolf"
-  create_dir "$roms_path/wolf/Spear of Destiny - Full.wolf"
-  create_dir "$roms_path/wolf/Spear of Destiny - Demo.wolf"
-  create_dir "$roms_path/wolf/Spear of Destiny - Mission Pack 1.wolf"
-  create_dir "$roms_path/wolf/Spear of Destiny - Mission Pack 2.wolf"
-  create_dir "$roms_path/wolf/Spear of Destiny - Mission Pack 3.wolf"
-  create_dir "$roms_path/wolf/Super 3D Noahs Ark.wolf"
 fi
