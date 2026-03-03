88e7b6ba0031a73ccce89652a7884b5150a5363c
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-03 16:44:51 -0500

    Migrate more component-specific functions to the component related libraries

--- portmaster/component_functions.sh
+++ portmaster/component_functions.sh
@@ -14,6 +14,40 @@ portmaster_show(){
   fi
 }
 
+configurator_portmaster_toggle_dialog() {
+  if [[ $(get_setting_value "$rd_conf" "portmaster_show" "retrodeck" "options") == "true" ]]; then
+    rd_zenity --question \
+    --no-wrap --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" \
+    --title "RetroDECK Configurator - PortMaster Visibility" \
+    --text="PortMaster is currently <span foreground='$purple'><b>Visible</b></span> in ES-DE. Do you want to hide it?\n\n\<span foreground='$purple'><b>Note: The installed games will still be visible.</b></span>"
+
+    if [ $? == 0 ] # User clicked "Yes"
+    then
+      portmaster_show "false"
+      rd_zenity --info \
+      --no-wrap --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" \
+      --title "RetroDECK Configurator - PortMaster Visibility" \
+      --text="PortMaster is now <span foreground='$purple'><b>Hidden</b></span> in ES-DE.\n\Please refresh your game list in ES-DE or restart RetroDECK to see the changes.\n\n\To launch PortMaster, you can access it from:\n<span foreground='$purple'><b>Configurator -> Open Component -> PortMaster</b></span>."
+    fi
+  else
+    rd_zenity --question \
+    --no-wrap --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" \
+    --title "RetroDECK Configurator - PortMaster Visibility" \
+    --text="PortMaster is currently <span foreground='$purple'><b>Hidden</b></span> in ES-DE. Do you want to show it?"
+
+    if [ $? == 0 ] # User clicked "Yes"
+    then
+      portmaster_show "true"
+      rd_zenity --info \
+      --no-wrap --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" \
+      --title "RetroDECK Configurator - PortMaster Visibility" \
+      --text="PortMaster is now <span foreground='$purple'><b>Visible</b></span> in ES-DE.\nPlease refresh your game list in ES-DE or restart RetroDECK to see the changes."
+    fi
+  fi
+
+  configurator_global_presets_and_settings_dialog
+}
+
 _set_setting_value::portmaster() {
   local file="$1" name="$2" value="$3"
 
