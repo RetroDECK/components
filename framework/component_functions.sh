ce4bdf86be6c88cb1fbe8518e092d03599f01b71
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-03 16:41:19 -0500

    Initial version of reworked framework prepare_component routed function

--- framework/component_functions.sh
+++ framework/component_functions.sh
@@ -12,6 +12,65 @@ _compress_game::zip() {
   zip -jq9 "$dest_file.zip" "$source_file"
 }
 
+_prepare_component::framework() {
+  # Perform preparation actions for the RetroDECK Framework.
+  # USAGE: _prepare_component::framework "$action"
+
+  local action="$1"
+
+  if [[ "$action" == "reset" ]]; then
+    log i "--------------------------------"
+    log i "Resetting RetroDECK Framework"
+    log i "--------------------------------"
+
+    while IFS=$'\t' read -r setting_name setting_value; do
+      [[ -z "$setting_name" ]] && continue
+      [[ "$setting_name" =~ ^(rd_home_path|sdcard)$ ]] && continue
+
+      local relative_path="${setting_value#*retrodeck/}"
+      local new_value="$rd_home_path/$relative_path"
+
+      log d "Setting: $setting_name=$new_value"
+
+      set_setting_value "$rd_conf" "$setting_name" "$new_value" "retrodeck" "paths"
+
+      declare -g "$setting_name=$new_value"
+      export "$setting_name"
+
+      if [[ "$setting_name" == "logs_path" ]]; then
+        rm -rf "$new_value"
+        dir_prep "$rd_xdg_config_logs_path" "$new_value"
+        log d "Logs folder linked from $rd_xdg_config_logs_path to $new_value"
+      else
+        create_dir "$new_value"
+      fi
+    done < <(jq -r '.paths | to_entries[] | [.key, (.value | tostring)] | @tsv' "$rd_conf")
+
+    create_dir -d "$XDG_CONFIG_HOME/retrodeck/graphics"
+    rsync -rlD --delete --mkpath "/app/retrodeck/graphics/folder-iconsets/" "$XDG_CONFIG_HOME/retrodeck/graphics/folder-iconsets/"
+  fi
+
+  if [[ "$action" == "postmove" ]]; then
+    log i "--------------------------------"
+    log i "Post-moving RetroDECK Framework"
+    log i "--------------------------------"
+
+    while IFS=$'\t' read -r setting_name setting_value; do
+      [[ -z "$setting_name" ]] && continue
+      [[ "$setting_name" =~ ^(rd_home_path|sdcard)$ ]] && continue
+
+      local new_path="$rd_home_path/${setting_value#*retrodeck/}"
+      if [[ -d "$new_path" ]]; then
+        set_setting_value "$rd_conf" "$setting_name" "$new_path" "retrodeck" "paths"
+        declare -g "$setting_name=$new_path"
+        export "$setting_name"
+      fi
+    done < <(jq -r '.paths | to_entries[] | [.key, (.value | tostring)] | @tsv' "$rd_conf")
+
+    dir_prep "$rd_xdg_config_logs_path" "$logs_path"
+  fi
+}
+
 handle_folder_iconsets() {
   local iconset="$1"
 
