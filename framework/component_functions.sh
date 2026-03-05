b390ea1afa244ab24014ba08087a9f91e41e4cd4
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-05 17:23:21 -0500

    Migrate Framework component prepare actions into component_functions for new router. Move handle_folder_iconsets back into core code.

--- framework/component_functions.sh
+++ framework/component_functions.sh
@@ -89,110 +89,62 @@ _compress_game::zip() {
 }
 
 _prepare_component::framework() {
-  # Perform preparation actions for the RetroDECK Framework.
-  # USAGE: _prepare_component::framework "$action"
-
   local action="$1"
 
-  if [[ "$action" == "reset" ]]; then
-    log i "--------------------------------"
-    log i "Resetting RetroDECK Framework"
-    log i "--------------------------------"
+  local component_config="$(get_own_component_path)/rd_config"
 
-    while IFS=$'\t' read -r setting_name setting_value; do
-      [[ -z "$setting_name" ]] && continue
-      [[ "$setting_name" =~ ^(rd_home_path|sdcard)$ ]] && continue
+  case "$action" in
 
-      local relative_path="${setting_value#*retrodeck/}"
-      local new_value="$rd_home_path/$relative_path"
+    reset)
+      log i "--------------------------------"
+      log i "Resetting RetroDECK Framework"
+      log i "--------------------------------"
 
-      log d "Setting: $setting_name=$new_value"
+      while IFS=$'\t' read -r setting_name setting_value; do
+        [[ -z "$setting_name" ]] && continue
+        [[ "$setting_name" =~ ^(rd_home_path|sdcard)$ ]] && continue
 
-      set_setting_value "$rd_conf" "$setting_name" "$new_value" "retrodeck" "paths"
+        local relative_path="${setting_value#*retrodeck/}"
+        local new_value="$rd_home_path/$relative_path"
 
-      declare -g "$setting_name=$new_value"
-      export "$setting_name"
+        log d "Resetting retrodeck.json setting to: $setting_name=$new_value"
 
-      if [[ "$setting_name" == "logs_path" ]]; then
-        rm -rf "$new_value"
-        dir_prep "$rd_xdg_config_logs_path" "$new_value"
-        log d "Logs folder linked from $rd_xdg_config_logs_path to $new_value"
-      else
-        create_dir "$new_value"
-      fi
-    done < <(jq -r '.paths | to_entries[] | [.key, (.value | tostring)] | @tsv' "$rd_conf")
+        set_setting_value "$rd_conf" "$setting_name" "$new_value" "retrodeck" "paths"
 
-    create_dir -d "$XDG_CONFIG_HOME/retrodeck/graphics"
-    rsync -rlD --delete --mkpath "/app/retrodeck/graphics/folder-iconsets/" "$XDG_CONFIG_HOME/retrodeck/graphics/folder-iconsets/"
-  fi
-
-  if [[ "$action" == "postmove" ]]; then
-    log i "--------------------------------"
-    log i "Post-moving RetroDECK Framework"
-    log i "--------------------------------"
-
-    while IFS=$'\t' read -r setting_name setting_value; do
-      [[ -z "$setting_name" ]] && continue
-      [[ "$setting_name" =~ ^(rd_home_path|sdcard)$ ]] && continue
-
-      local new_path="$rd_home_path/${setting_value#*retrodeck/}"
-      if [[ -d "$new_path" ]]; then
-        set_setting_value "$rd_conf" "$setting_name" "$new_path" "retrodeck" "paths"
-        declare -g "$setting_name=$new_path"
-        export "$setting_name"
-      fi
-    done < <(jq -r '.paths | to_entries[] | [.key, (.value | tostring)] | @tsv' "$rd_conf")
-
-    dir_prep "$rd_xdg_config_logs_path" "$logs_path"
-  fi
-}
-
-handle_folder_iconsets() {
-  local iconset="$1"
-
-  if [[ ! "$iconset" == "false" ]]; then
-    if [[ -d "$folder_iconsets_dir/$iconset" ]]; then
-      while read -r icon; do
-        local icon_relative_path="${icon#$folder_iconsets_dir/$iconset/}"
-        local icon_relative_path="${icon_relative_path%.ico}"
-        local icon_relative_root="${icon_relative_path%%/*}"
-        local path_var_name="${icon_relative_root}_path"
-        local path_name=""
-
-        if [[ "$icon_relative_path" =~ (sync) ]]; then # If the icon is for a hidden folder, add the leading dot temporarily for searching
-          icon_relative_path=".${icon_relative_path}"
-        fi
-
-        if [[ -v "$path_var_name" ]]; then
-          path_name="${!path_var_name}"
-          if [[ ! "$icon_relative_path" == "$icon_relative_root" ]]; then
-            path_name="$path_name/${icon_relative_path#$icon_relative_root/}"
-          fi
-          if [[ ! -d "$path_name" ]]; then
-            log w "Path for icon $icon could not be found, skipping..."
-            continue
-          fi
-        elif [[ -d "$rd_home_path/$icon_relative_path" ]]; then
-          path_name="$rd_home_path/$icon_relative_path"
-          icon_relative_path="${icon_relative_path#.}" # Remove leading dot from actual icon name reference
+        if [[ "$setting_name" == "logs_path" ]]; then
+          dir_prep "$rd_xdg_config_logs_path" "$new_value"
+          log d "Logs folder linked from $rd_xdg_config_logs_path to $new_value"
         else
-          log w "Path for icon $icon could not be found, skipping..."
-          continue
+          create_dir "$new_value"
         fi
+      done < <(jq -r '.paths | to_entries[] | [.key, (.value | tostring)] | @tsv' "$rd_conf")
 
-        log d "Creating file $path_name/.directory"
-        echo '[Desktop Entry]' > "$path_name/.directory"
-        echo "Icon=$folder_iconsets_dir/$iconset/$icon_relative_path.ico" >> "$path_name/.directory"
-      done < <(find "$folder_iconsets_dir/$iconset" -maxdepth 2 -type f -iname "*.ico")
-      set_setting_value "$rd_conf" "iconset" "$iconset" retrodeck "options"
-    else
-      configurator_generic_dialog "RetroDeck Configurator - Toggle Folder Iconsets" "The chosen iconset <span foreground='$purple'><b>$iconset</b></span> could not be found in the RetroDECK assets."
-      return 1
-    fi
-  else
-    while read -r path; do
-      find -L "$path" -maxdepth 2 -type f -iname '.directory' -exec rm {} \;
-    done < <(jq -r 'del(.paths.downloaded_media_path, .paths.themes_path, .paths.sdcard) | .paths[]' "$rd_conf")
-    set_setting_value "$rd_conf" "iconset" "false" retrodeck "options"
-  fi
+      create_dir -d "$XDG_CONFIG_HOME/retrodeck/graphics"
+      rsync -rlD --delete --mkpath "/app/retrodeck/graphics/folder-iconsets/" "$XDG_CONFIG_HOME/retrodeck/graphics/folder-iconsets/"
+    ;;
+
+    postmove)
+      log i "--------------------------------"
+      log i "Post-moving RetroDECK Framework"
+      log i "--------------------------------"
+
+      while IFS=$'\t' read -r setting_name setting_value; do
+        [[ -z "$setting_name" ]] && continue
+        [[ "$setting_name" =~ ^(rd_home_path|sdcard)$ ]] && continue
+
+        local new_path="$rd_home_path/${setting_value#*retrodeck/}"
+        if [[ -d "$new_path" ]]; then
+          set_setting_value "$rd_conf" "$setting_name" "$new_path" "retrodeck" "paths"
+        fi
+      done < <(jq -r '.paths | to_entries[] | [.key, (.value | tostring)] | @tsv' "$rd_conf")
+
+      dir_prep "$rd_xdg_config_logs_path" "$logs_path"
+    ;;
+
+    shutdown)
+      log i "Shutting down RetroDECK's framework"
+      pkill -f "retrodeck"
+    ;;
+
+  esac
 }
