446d1907da2824fac0e6804b38fa5018a5776aac
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-05 17:19:32 -0500

    Migrate GZDOOM component prepare actions into component_functions for new router

--- gzdoom/component_functions.sh
+++ gzdoom/component_functions.sh
@@ -1,3 +1,40 @@
 #!/bin/bash
 
 gzdoom_config="$XDG_CONFIG_HOME/gzdoom/gzdoom.ini"
+
+_prepare_component::gzdoom() {
+  local action="$1"
+
+  local component_config="$(get_own_component_path)/rd_config"
+
+  case "$action" in
+
+    reset)
+      log i "------------------------"
+      log i "Resetting GZDOOM"
+      log i "------------------------"
+      
+      # TODO: do a proper script
+      # This is just a placeholder script to test the emulator's flow
+
+      create_dir "$XDG_CONFIG_HOME/gzdoom"
+      create_dir "$XDG_DATA_HOME/gzdoom/audio/midi"
+      create_dir "$XDG_DATA_HOME/gzdoom/audio/fm_banks"
+      create_dir "$XDG_DATA_HOME/gzdoom/audio/soundfonts"
+      create_dir "$bios_path/gzdoom"
+      create_dir "$storage_path/gzdoom/audio/soundfonts"
+      create_dir "$storage_path/gzdoom/audio/fm_banks"
+      create_dir "$storage_path/gzdoom/audio/midi"
+
+      cp -fv "$component_config/gzdoom.ini" "$gzdoom_config"
+
+      # This is an unfortunate one-off because set_setting_value does not currently support multiple setting names with the same name in the same section
+      sed -i "s#RETRODECKHOMEDIR#${rd_home_path}#g" "$gzdoom_config"
+      sed -i "s#RETRODECKROMSDIR#${roms_path}#g" "$gzdoom_config"
+      sed -i "s#RETRODECKBIOSDIR#${bios_path}#g" "$gzdoom_config"
+      sed -i "s#RETRODECKSAVESDIR#${saves_path}#g" "$gzdoom_config"
+      sed -i "s#RETRODECKSTORAGESDIR#${storage_path}#g" "$gzdoom_config"
+    ;;
+
+  esac
+}
