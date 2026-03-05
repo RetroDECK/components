45f587d1a093b5807f87122b5accfcebb98b0660
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-05 17:08:32 -0500

    Migrate Dolphin component prepare actions into component_functions for new router.

--- dolphin/component_functions.sh
+++ dolphin/component_functions.sh
@@ -50,6 +50,64 @@ _get_setting_value::dolphin() {
   fi
 }
 
+_prepare_component::dolphin() {
+  local action="$1"
+
+  local component_config="$(get_own_component_path)/rd_config"
+
+  case "$action" in
+
+    reset)
+      log i "----------------------"
+      log i "Resetting Dolphin"
+      log i "----------------------"
+
+      create_dir -d "$XDG_CONFIG_HOME/dolphin-emu/"
+      cp -fvr "$component_config/"* "$XDG_CONFIG_HOME/dolphin-emu/"
+      set_setting_value "$dolphin_config" "BIOS" "$bios_path" "dolphin" "GBA"
+      set_setting_value "$dolphin_config" "SavesPath" "$saves_path/gba" "dolphin" "GBA"
+      set_setting_value "$dolphin_config" "ISOPath0" "$roms_path/wii" "dolphin" "General"
+      set_setting_value "$dolphin_config" "ISOPath1" "$roms_path/gc" "dolphin" "General"
+      set_setting_value "$dolphin_config" "WiiSDCardPath" "$saves_path/wii/dolphin/sd.raw" "dolphin" "General"
+      dir_prep "$saves_path/gc/dolphin/EU" "$XDG_DATA_HOME/dolphin-emu/GC/EUR"
+      dir_prep "$saves_path/gc/dolphin/US" "$XDG_DATA_HOME/dolphin-emu/GC/USA"
+      dir_prep "$saves_path/gc/dolphin/JP" "$XDG_DATA_HOME/dolphin-emu/GC/JAP"
+      dir_prep "$screenshots_path" "$XDG_DATA_HOME/dolphin-emu/ScreenShots"
+      dir_prep "$states_path/dolphin" "$XDG_DATA_HOME/dolphin-emu/StateSaves"
+      dir_prep "$saves_path/wii/dolphin" "$XDG_DATA_HOME/dolphin-emu/Wii"
+      dir_prep "$mods_path/Dolphin/GraphicMods" "$dolphin_mods_path"
+      dir_prep "$texture_packs_path/Dolphin/Textures" "$dolphin_textures_path"
+      dir_prep "$shaders_path/Dolphin" "$XDG_DATA_HOME/dolphin-emu/Shaders"
+      dir_prep "$logs_path/Dolphin" "$XDG_DATA_HOME/dolphin-emu/Logs"
+      dir_prep "$storage_path/Dolphin/Dump" "$XDG_DATA_HOME/dolphin-emu/Dump"
+    ;;
+
+    postmove)
+      log i "----------------------"
+      log i "Post-moving Dolphin"
+      log i "----------------------"
+
+      dir_prep "$saves_path/gc/dolphin/EU" "$XDG_DATA_HOME/dolphin-emu/GC/EUR"
+      dir_prep "$saves_path/gc/dolphin/US" "$XDG_DATA_HOME/dolphin-emu/GC/USA"
+      dir_prep "$saves_path/gc/dolphin/JP" "$XDG_DATA_HOME/dolphin-emu/GC/JAP"
+      dir_prep "$screenshots_path" "$XDG_DATA_HOME/dolphin-emu/ScreenShots"
+      dir_prep "$states_path/dolphin" "$XDG_DATA_HOME/dolphin-emu/StateSaves"
+      dir_prep "$saves_path/wii/dolphin" "$XDG_DATA_HOME/dolphin-emu/Wii"
+      dir_prep "$mods_path/Dolphin/GraphicMods" "$dolphin_mods_path"
+      dir_prep "$texture_packs_path/Dolphin/Textures" "$dolphin_textures_path"
+      dir_prep "$shaders_path/Dolphin" "$XDG_DATA_HOME/dolphin-emu/Shaders"
+      dir_prep "$logs_path/Dolphin" "$XDG_DATA_HOME/dolphin-emu/Logs"
+      dir_prep "$storage_path/Dolphin/Dump" "$XDG_DATA_HOME/dolphin-emu/Dump"
+      set_setting_value "$dolphin_config" "BIOS" "$bios_path" "dolphin" "GBA"
+      set_setting_value "$dolphin_config" "SavesPath" "$saves_path/gba" "dolphin" "GBA"
+      set_setting_value "$dolphin_config" "ISOPath0" "$roms_path/wii" "dolphin" "General"
+      set_setting_value "$dolphin_config" "ISOPath1" "$roms_path/gc" "dolphin" "General"
+      set_setting_value "$dolphin_config" "WiiSDCardPath" "$saves_path/wii/dolphin/sd.raw" "dolphin" "General"
+    ;;
+
+  esac
+}
+
 _validate_for_compression::rvz() {
   local file="$1"
   local normalized_filename=$(echo "$file" | tr '[:upper:]' '[:lower:]')
