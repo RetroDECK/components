b97fc030ed79c24c4e1a9bfb9b5d56100560dd21
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-05 17:19:17 -0500

    Migrate Flips component prepare actions into component_functions for new router

--- flips/component_functions.sh
+++ flips/component_functions.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+_prepare_component::flips() {
+  local action="$1"
+
+  local component_config="$(get_own_component_path)/rd_config"
+
+  case "$action" in
+
+    reset)
+        log i "----------------------"
+        log i "Resetting Flips"
+        log i "----------------------"
+
+        create_dir "$XDG_CONFIG_HOME/flips"
+    ;;
+
+  esac
+}
