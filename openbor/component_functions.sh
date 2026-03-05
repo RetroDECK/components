8dbbe12586ce878c204fb7cbcd02441145eaa8b6
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-05 17:20:16 -0500

    Migrate OpenBOR component prepare actions into component_functions for new router

--- openbor/component_functions.sh
+++ openbor/component_functions.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+_prepare_component::openbor() {
+  local action="$1"
+
+  local component_config="$(get_own_component_path)/rd_config"
+
+  case "$action" in
+
+    reset)
+      log i "------------------------"
+      log i "Resetting OpenBOR"
+      log i "------------------------"
+    ;;
+
+  esac
+}
