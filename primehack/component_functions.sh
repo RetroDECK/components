c5602352b383009ab7190c94cdfe3d353e80d4cd
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-05 17:21:34 -0500

    Migrate Primehack component prepare actions into component_functions for new router

--- primehack/component_functions.sh
+++ primehack/component_functions.sh
@@ -47,3 +47,53 @@ _get_setting_value::primehack() {
        }' "$file"
   fi
 }
+
+_prepare_component::primehack() {
+  local action="$1"
+
+  local component_config="$(get_own_component_path)/rd_config"
+
+  case "$action" in
+
+    reset)
+      log i "------------------------"
+      log i "Resetting Primehack"
+      log i "------------------------"
+      
+      create_dir -d "$XDG_CONFIG_HOME/primehack/"
+      cp -fvr "$component_config/config/"* "$XDG_CONFIG_HOME/primehack/"
+      set_setting_value "$primehack_config" "ISOPath0" "$roms_path/primehack" "primehack" "General"
+      dir_prep "$saves_path/gc/primehack/EU" "$XDG_DATA_HOME/primehack/GC/EUR"
+      dir_prep "$saves_path/gc/primehack/US" "$XDG_DATA_HOME/primehack/GC/USA"
+      dir_prep "$saves_path/gc/primehack/JP" "$XDG_DATA_HOME/primehack/GC/JAP"
+      dir_prep "$screenshots_path" "$XDG_DATA_HOME/primehack/ScreenShots"
+      dir_prep "$states_path/primehack" "$XDG_DATA_HOME/primehack/StateSaves"
+      dir_prep "$saves_path/wii/primehack" "$XDG_DATA_HOME/primehack/Wii"
+      dir_prep "$mods_path/Primehack" "$XDG_DATA_HOME/primehack/Load/GraphicMods"
+      dir_prep "$texture_packs_path/Primehack" "$XDG_DATA_HOME/primehack/Load/Textures"
+      dir_prep "$shaders_path/Primehack" "$XDG_DATA_HOME/primehack/Shaders"
+      dir_prep "$logs_path/Primehack" "$XDG_DATA_HOME/primehack/Logs"
+      dir_prep "$storage_path/Primehack/Dump" "$XDG_DATA_HOME/Primehack/Dump"
+    ;;
+
+    postmove)
+      log i "------------------------"
+      log i "Post-moving Primehack"
+      log i "------------------------"
+
+      dir_prep "$saves_path/gc/primehack/EU" "$XDG_DATA_HOME/primehack/GC/EUR"
+      dir_prep "$saves_path/gc/primehack/US" "$XDG_DATA_HOME/primehack/GC/USA"
+      dir_prep "$saves_path/gc/primehack/JP" "$XDG_DATA_HOME/primehack/GC/JAP"
+      dir_prep "$screenshots_path" "$XDG_DATA_HOME/primehack/ScreenShots"
+      dir_prep "$states_path/primehack" "$XDG_DATA_HOME/primehack/StateSaves"
+      dir_prep "$saves_path/wii/primehack" "$XDG_DATA_HOME/primehack/Wii/"
+      dir_prep "$mods_path/Primehack" "$XDG_DATA_HOME/primehack/Load/GraphicMods"
+      dir_prep "$texture_packs_path/Primehack" "$XDG_DATA_HOME/primehack/Load/Textures"
+      dir_prep "$shaders_path/Primehack" "$XDG_DATA_HOME/primehack/Shaders"
+      dir_prep "$logs_path/Primehack" "$XDG_DATA_HOME/primehack/Logs"
+      dir_prep "$storage_path/Primehack/Dump" "$XDG_DATA_HOME/Primehack/Dump"
+      set_setting_value "$primehack_config" "ISOPath0" "$roms_path/primehack" "primehack" "General"
+    ;;
+
+  esac
+}
