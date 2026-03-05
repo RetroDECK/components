48d062b4acfe9b03f49f6221004b59d0f810fb85
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-05 17:08:59 -0500

    Migrate Duckstation component prepare actions into component_functions for new router.

--- duckstation/component_functions.sh
+++ duckstation/component_functions.sh
@@ -42,3 +42,52 @@ _get_setting_value::duckstation() {
        }' "$file"
   fi
 }
+
+_prepare_component::duckstation() {
+  local action="$1"
+
+  local component_config="$(get_own_component_path)/rd_config"
+
+  case "$action" in
+
+    reset)
+      log i "------------------------"
+      log i "Resetting Duckstation"
+      log i "------------------------"
+      
+      create_dir -d "$XDG_CONFIG_HOME/duckstation/"
+      create_dir "$saves_path/psx/duckstation/memcards"
+      cp -fv "$component_config/"* "$XDG_CONFIG_HOME/duckstation"
+      set_setting_value "$duckstation_config" "SearchDirectory" "$bios_path" "duckstation" "BIOS"
+      set_setting_value "$duckstation_config" "Card1Path" "$saves_path/psx/duckstation/memcards/shared_card_1.mcd" "duckstation" "MemoryCards"
+      set_setting_value "$duckstation_config" "Card2Path" "$saves_path/psx/duckstation/memcards/shared_card_2.mcd" "duckstation" "MemoryCards"
+      set_setting_value "$duckstation_config" "Directory" "$saves_path/psx/duckstation/memcards" "duckstation" "MemoryCards"
+      set_setting_value "$duckstation_config" "RecursivePaths" "$roms_path/psx" "duckstation" "GameList"
+      dir_prep "$states_path/psx/duckstation" "$XDG_CONFIG_HOME/duckstation/savestates" # This is hard-coded in Duckstation, always needed
+      dir_prep "$texture_packs_path/Duckstation/textures" "$duckstation_textures_path"
+      dir_prep "$screenshots_path/Duckstation/" "$duckstation_screenshots_path"
+      dir_prep "$shaders_path/Duckstation/" "$duckstation_shaders_path"
+      dir_prep "$storage_path/Duckstation/dump/textures" "$duckstation_dump_textures_path"
+      dir_prep "$storage_path/Duckstation/dump/audio" "$duckstation_dump_audio_path"
+    ;;
+
+    postmove)
+      log i "------------------------"
+      log i "Post-moving Duckstation"
+      log i "------------------------"
+
+      set_setting_value "$duckstation_config" "SearchDirectory" "$bios_path" "duckstation" "BIOS"
+      set_setting_value "$duckstation_config" "Card1Path" "$saves_path/psx/duckstation/memcards/shared_card_1.mcd" "duckstation" "MemoryCards"
+      set_setting_value "$duckstation_config" "Card2Path" "$saves_path/psx/duckstation/memcards/shared_card_2.mcd" "duckstation" "MemoryCards"
+      set_setting_value "$duckstation_config" "Directory" "$saves_path/psx/duckstation/memcards" "duckstation" "MemoryCards"
+      set_setting_value "$duckstation_config" "RecursivePaths" "$roms_path/psx" "duckstation" "GameList"
+      dir_prep "$states_path/psx/duckstation" "$XDG_CONFIG_HOME/duckstation/savestates" # This is hard-coded in Duckstation, always needed
+      dir_prep "$texture_packs_path/Duckstation/textures" "$duckstation_textures_path"
+      dir_prep "$screenshots_path/Duckstation/" "$duckstation_screenshots_path"
+      dir_prep "$shaders_path/Duckstation/" "$duckstation_shaders_path"
+      dir_prep "$storage_path/Duckstation/dump/textures" "$duckstation_dump_textures_path"
+      dir_prep "$storage_path/Duckstation/dump/audio" "$duckstation_dump_audio_path"
+    ;;
+
+  esac
+}
