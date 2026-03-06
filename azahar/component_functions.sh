60d93c4c335a862ec2454ab80116a4dc0785bdc3
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-06 10:53:56 -0500

    Add explicit exports to Azahar component_functions variables

--- azahar/component_functions.sh
+++ azahar/component_functions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-azahar_config_path="$XDG_CONFIG_HOME/azahar-emu"
-azahar_qtconfig="$azahar_config_path/qt-config.ini"
-azahar_mods_path="$XDG_DATA_HOME/azahar-emu/load/mods"
-azahar_textures_path="$XDG_DATA_HOME/azahar-emu/load/textures"
-azahar_shaders_path="$XDG_DATA_HOME/azahar-emu/shaders"
-azahar_logs_path="$XDG_DATA_HOME/azahar-emu/log"
-azahar_cheats_path="$XDG_DATA_HOME/azahar-emu/cheats"
+export azahar_config_path="$XDG_CONFIG_HOME/azahar-emu"
+export azahar_qtconfig="$azahar_config_path/qt-config.ini"
+export azahar_mods_path="$XDG_DATA_HOME/azahar-emu/load/mods"
+export azahar_textures_path="$XDG_DATA_HOME/azahar-emu/load/textures"
+export azahar_shaders_path="$XDG_DATA_HOME/azahar-emu/shaders"
+export azahar_logs_path="$XDG_DATA_HOME/azahar-emu/log"
+export azahar_cheats_path="$XDG_DATA_HOME/azahar-emu/cheats"
 
 _set_setting_value::azahar() {
   local file="$1"
