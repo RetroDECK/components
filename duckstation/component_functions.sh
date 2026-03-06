23142576cefc90316397fbc9bddfb0fb9b6ec69d
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-06 10:54:18 -0500

    Add explicit exports to Duckstation component_functions variables

--- duckstation/component_functions.sh
+++ duckstation/component_functions.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-duckstation_config="$XDG_CONFIG_HOME/duckstation/settings.ini"
-duckstation_rd_config_dir="$rd_components/duckstation/rd_config"
-duckstation_textures_path="$XDG_CONFIG_HOME/duckstation/textures"
-duckstation_screenshots_path="$XDG_CONFIG_HOME/duckstation/screenshots"
-duckstation_videos_path="$XDG_CONFIG_HOME/duckstation/videos"
-duckstation_gamesettings_path="$XDG_CONFIG_HOME/duckstation/gamesettings"
-duckstation_shaders_path="$XDG_CONFIG_HOME/duckstation/shaders"
-duckstation_dump_textures_path="$XDG_CONFIG_HOME/duckstation/dump/textures"
-duckstation_dump_audio_path="$XDG_CONFIG_HOME/duckstation/dump/audio"
-duckstation_covers_path="$XDG_CONFIG_HOME/duckstation/covers"
+export duckstation_config="$XDG_CONFIG_HOME/duckstation/settings.ini"
+export duckstation_rd_config_dir="$rd_components/duckstation/rd_config"
+export duckstation_textures_path="$XDG_CONFIG_HOME/duckstation/textures"
+export duckstation_screenshots_path="$XDG_CONFIG_HOME/duckstation/screenshots"
+export duckstation_videos_path="$XDG_CONFIG_HOME/duckstation/videos"
+export duckstation_gamesettings_path="$XDG_CONFIG_HOME/duckstation/gamesettings"
+export duckstation_shaders_path="$XDG_CONFIG_HOME/duckstation/shaders"
+export duckstation_dump_textures_path="$XDG_CONFIG_HOME/duckstation/dump/textures"
+export duckstation_dump_audio_path="$XDG_CONFIG_HOME/duckstation/dump/audio"
+export duckstation_covers_path="$XDG_CONFIG_HOME/duckstation/covers"
 
 _set_setting_value::duckstation() {
   local file="$1"
