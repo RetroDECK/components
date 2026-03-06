7e9ab676e540a8976e1c78a06c054028232dbfc1
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-06 10:47:55 -0500

    Rebuild splash_screen function for new component manifest cache

--- es-de/component_functions.sh
+++ es-de/component_functions.sh
@@ -100,14 +100,16 @@ splash_screen::es-de() {
   current_time=$(date +"%H%M") # Read the current time in a format that can be calculated in ranges
 
   # Read the JSON file and extract splash screen data using jq
-  splash_screen=$(jq -r --arg current_day "$current_day" --arg current_time "$current_time" '
-    .splash_screens | to_entries[] |
+  splash_screen=$(get_component_manifest_cache | jq -r --arg current_day "$current_day" --arg current_time "$current_time" '
+    .[] | .manifest | select(has("es-de")) | .["es-de"].splash_screens |
+    to_entries[] |
     select(
       ($current_day | tonumber) >= (.value.start_date | tonumber) and
       ($current_day | tonumber) <= (.value.end_date | tonumber) and
       ($current_time | tonumber) >= (.value.start_time | tonumber) and
       ($current_time | tonumber) <= (.value.end_time | tonumber)
-    ) | .value.filename' "$features")
+    ) | .value.filename
+  ')
 
   # Determine the splash file to use
   if [[ -n "$splash_screen" ]]; then
