f21926dd076c235a4d31c48ce246063bd4c14621
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-06 10:54:32 -0500

    Add explicit exports to MAME component_functions variables

--- mame/component_functions.sh
+++ mame/component_functions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mame_config="$XDG_CONFIG_HOME/mame/ini/mame.ini"
-mame_config_ui="$XDG_CONFIG_HOME/mame/ini/ui.ini"
-mame_config_default="$XDG_CONFIG_HOME/mame/cfg/default.cfg"
+export mame_config="$XDG_CONFIG_HOME/mame/ini/mame.ini"
+export mame_config_ui="$XDG_CONFIG_HOME/mame/ini/ui.ini"
+export mame_config_default="$XDG_CONFIG_HOME/mame/cfg/default.cfg"
 
 _set_setting_value::mame() {
   local file="$1"
