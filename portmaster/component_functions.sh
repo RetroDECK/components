8c8f9b3a6813c9d33d42f0e14729d94d79fcbc6b
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-06 09:09:45 -0500

    Migrate Portmaster prepare actions into component_functions for new router

--- portmaster/component_functions.sh
+++ portmaster/component_functions.sh
@@ -1,6 +1,63 @@
 #!/bin/bash
 
-portmaster_show(){
+_set_setting_value::portmaster() {
+  local file="$1" name="$2" value="$3"
+
+  local tmp
+  tmp=$(jq --arg key "$name" --arg val "$value" \
+    '(.[$key]) |= (if type == "number" then ($val | tonumber) elif type == "boolean" then ($val | test("true")) else $val end)' \
+    "$file") && printf '%s\n' "$tmp" > "$file"
+}
+
+_get_setting_value::portmaster() {
+  local file="$1" name="$2"
+
+  jq -r --arg key "$name" '.[$key]' "$file"
+}
+
+_prepare_component::portmaster() {
+  local action="$1"
+
+  local component_config="$(get_own_component_path)/rd_config"
+
+  case "$action" in
+
+    reset)
+      log i "----------------------"
+      log i "Performing Portmaster $action actions"
+      log i "----------------------"
+
+      rm -rf "$XDG_DATA_HOME/PortMaster"
+      unzip "/app/retrodeck/components/portmaster/PortMaster.zip" -d "$XDG_DATA_HOME/"
+      cp -f "$XDG_DATA_HOME/PortMaster/retrodeck/PortMaster.txt" "$XDG_DATA_HOME/PortMaster/PortMaster.sh"
+      chmod +x "$XDG_DATA_HOME/PortMaster/PortMaster.sh"
+      rm -f "$roms_path/portmaster/PortMaster.sh"
+      install -Dm755 "$XDG_DATA_HOME/PortMaster/PortMaster.sh" "$roms_path/portmaster/PortMaster.sh"
+      create_dir "$XDG_DATA_HOME/PortMaster/config/"
+      cp "$component_config/config.json" "$XDG_DATA_HOME/PortMaster/config/config.json"
+      set_setting_value "$rd_conf" "portmaster_path" "$rd_home_path/PortMaster" "retrodeck" "paths"
+      create_dir "$portmaster_path"
+    ;;
+
+    startup)
+      log i "------------------------"
+      log i "Performing Portmaster $action actions"
+      log i "------------------------"
+
+      log d "Checking if PortMaster should be shown"
+      if [[ $(get_setting_value "$rd_conf" "portmaster_show" "retrodeck" "options") == "false" ]]; then
+        log d "Assuring that PortMaster is hidden on ES-DE"
+        portmaster_show "false"
+      else
+        log d "Assuring that PortMaster is shown on ES-DE"
+        portmaster_show "true"
+      fi
+    ;;
+
+  esac
+}
+
+portmaster_show() {
   log d "Setting PortMaster visibility in ES-DE"
   if [ "$1" = "true" ]; then
     log d "\"$roms_path/portmaster/PortMaster.sh\" is not found, installing it"
@@ -46,19 +103,4 @@ configurator_portmaster_toggle_dialog() {
   fi
 
   configurator_global_presets_and_settings_dialog
-}
-
-_set_setting_value::portmaster() {
-  local file="$1" name="$2" value="$3"
-
-  local tmp
-  tmp=$(jq --arg key "$name" --arg val "$value" \
-    '(.[$key]) |= (if type == "number" then ($val | tonumber) elif type == "boolean" then ($val | test("true")) else $val end)' \
-    "$file") && printf '%s\n' "$tmp" > "$file"
-}
-
-_get_setting_value::portmaster() {
-  local file="$1" name="$2"
-
-  jq -r --arg key "$name" '.[$key]' "$file"
 }
