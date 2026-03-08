b21a22b126fd66858765482ca884d17f2ed1669e
IceNine451 <benjamin.r.shelton@protonmail.com>
2026-03-08 12:47:31 -0400

    Migrate prepare actions to component_functions for Xroar

--- xroar/component_functions.sh
+++ xroar/component_functions.sh
@@ -43,3 +43,31 @@ _get_setting_value::xroar() {
        }' "$file"
   fi
 }
+
+_prepare_component::xroar() {
+  local action="$1"
+  shift
+
+  local component_config="$(get_own_component_path)/rd_config"
+
+  case "$action" in
+
+    reset)
+      log i "------------------------"
+      log i "Resetting XROAR"
+      log i "------------------------"
+
+      reate_dir -d "$XDG_CONFIG_HOME/xroar"
+      cp -f "$component_config/xroar.conf" "$xroar_config" && log i "Copied default xroar.conf to $xroar_config"
+      sed -i "s#RETRODECKROMSDIR#${roms_path}#g" "$xroar_config" && log i "Set ROMs directory in xroar.conf"
+      sed -i "s#RETRODECKBIOSDIR#${bios_path}#g" "$xroar_config" && log i "Set BIOS directory in xroar.conf"
+
+      # XRoar does not provide a default directory for state files in the config, so you must choose a folder each time you save or load.
+
+      create_dir "$states_path/xroar/coco/"
+      create_dir "$states_path/xroar/dragon32/"
+      create_dir "$states_path/xroar/tanodragon/"
+    ;;
+
+  esac
+}
