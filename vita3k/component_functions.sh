4a90d457c55ac716290156cf0400f54b336c05a1
IceNine451 <benjamin.r.shelton@protonmail.com>
2026-03-08 12:47:08 -0400

    Migrate prepare actions to component_functions for Vita3k

--- vita3k/component_functions.sh
+++ vita3k/component_functions.sh
@@ -48,3 +48,46 @@ _get_setting_value::vita3k() {
 
   yq -r ".[\"${name}\"]" "$file"
 }
+
+_prepare_component::vita3k() {
+  local action="$1"
+  shift
+
+  local component_config="$(get_own_component_path)/rd_config"
+
+  case "$action" in
+
+    reset)
+      log i "------------------------"
+      log i "Resetting Vita3k"
+      log i "------------------------"
+
+      # NOTE: the component is writing in "." so it must be placed in the rw filesystem. A symlink of the binary is already placed in /app/bin/Vita3K
+      rm -rf "$XDG_CONFIG_HOME/Vita3K"
+      create_dir "$XDG_CONFIG_HOME/Vita3K"
+      cp -fv "$component_config/config.yml" "$vita3k_config" # component config
+      create_dir "$storage_path/psvita/Vita3K/"
+      cp -frv "$component_config/ux0" "$storage_path/psvita/Vita3K/" # User config
+      set_setting_value "$vita3k_config" "pref-path" "$storage_path/psvita/Vita3K/" "vita3k"
+      dir_prep "$saves_path/psvita/vita3k" "$storage_path/psvita/Vita3K/ux0/user/00/savedata" # Multi-user safe?
+      dir_prep "$texture_packs_path/Vita3K/import" "$vita3k_textures_path" # Textures
+      dir_prep "$storage_path/psvita/Vita3K/lang"  "$vita3k_lang_path"
+      dir_prep "$storage_path/psvita/Vita3K/patch"  "$vita3k_patch_path"
+      dir_prep "$shaders_path/Vita3K/"  "$vita3k_shaders_path"
+    ;;
+
+    postmove)
+      log i "------------------------"
+      log i "Post-moving Vita3k"
+      log i "------------------------"
+
+      dir_prep "$saves_path/psvita/vita3k" "$storage_path/psvita/Vita3K/ux0/user/00/savedata" # Multi-user safe?
+      dir_prep "$texture_packs_path/Vita3K/import" "$vita3k_textures_path" # Textures
+      dir_prep "$storage_path/psvita/Vita3K/lang"  "$vita3k_lang_path"
+      dir_prep "$storage_path/psvita/Vita3K/patch"  "$vita3k_patch_path"
+      dir_prep "$shaders_path/Vita3K/"  "$vita3k_shaders_path"
+      set_setting_value "$vita3k_config" "pref-path" "$storage_path/psvita/Vita3K/" "vita3k"
+    ;;
+
+  esac
+}
