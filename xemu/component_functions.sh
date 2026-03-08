9b14d897fa04cb9a467cf01e437a23b7608c661e
IceNine451 <benjamin.r.shelton@protonmail.com>
2026-03-08 12:47:19 -0400

    Migrate prepare actions to component_functions for Xemu

--- xemu/component_functions.sh
+++ xemu/component_functions.sh
@@ -41,3 +41,49 @@ _get_setting_value::xemu() {
        }' "$file"
   fi
 }
+
+_prepare_component::xemu() {
+  local action="$1"
+  shift
+
+  local component_config="$(get_own_component_path)/rd_config"
+
+  case "$action" in
+
+    reset)
+      log i "------------------------"
+      log i "Resetting Xemu"
+      log i "------------------------"
+
+      rm -rf "$XDG_CONFIG_HOME/xemu"
+      rm -rf "$XDG_DATA_HOME/xemu"
+
+      # Creating config folder in $XDG_CONFIG_HOME for consistentcy and linking back to original location where component will look
+      dir_prep "$XDG_CONFIG_HOME/xemu" "$XDG_DATA_HOME/xemu/xemu"
+      dir_prep "$shaders_path/xemu" "$XDG_DATA_HOME/xemu/xemu/shaders"
+
+      cp -fv "$component_config/xemu.toml" "$xemu_conf"
+      create_dir "$screenshots_path/xemu"
+      set_setting_value "$xemu_conf" "screenshot_dir" "'$screenshots_path/xemu'" "xemu" "General"
+      set_setting_value "$xemu_conf" "bootrom_path" "'$bios_path/mcpx_1.0.bin'" "xemu" "sys.files"
+      set_setting_value "$xemu_conf" "flashrom_path" "'$bios_path/Complex.bin'" "xemu" "sys.files"
+      create_dir "$saves_path/xbox/xemu"
+      set_setting_value "$xemu_conf" "eeprom_path" "'$saves_path/xbox/xemu/xbox-eeprom.bin'" "xemu" "sys.files"
+      set_setting_value "$xemu_conf" "hdd_path" "'$bios_path/xbox_hdd.qcow2'" "xemu" "sys.files"
+    ;;
+
+    postmove)
+      log i "------------------------"
+      log i "Post-moving Xemu"
+      log i "------------------------"
+
+      dir_prep "$shaders_path/xemu" "$XDG_DATA_HOME/xemu/xemu/shaders"
+      set_setting_value "$xemu_conf" "screenshot_dir" "'$screenshots_path/xemu'" "xemu" "General"
+      set_setting_value "$xemu_conf" "bootrom_path" "'$bios_path/mcpx_1.0.bin'" "xemu" "sys.files"
+      set_setting_value "$xemu_conf" "flashrom_path" "'$bios_path/Complex.bin'" "xemu" "sys.files"
+      set_setting_value "$xemu_conf" "eeprom_path" "'$saves_path/xbox/xemu/xbox-eeprom.bin'" "xemu" "sys.files"
+      set_setting_value "$xemu_conf" "hdd_path" "'$bios_path/xbox_hdd.qcow2'" "xemu" "sys.files"
+    ;;
+
+  esac
+}
