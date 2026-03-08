4c886e812ed5c43658f0e05aa7d6ca029a754a0e
IceNine451 <benjamin.r.shelton@protonmail.com>
2026-03-08 12:46:57 -0400

    Migrate prepare actions to component_functions for Solarus

--- solarus/component_functions.sh
+++ solarus/component_functions.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+_prepare_component::solarus() {
+  local action="$1"
+  shift
+
+  local component_config="$(get_own_component_path)/rd_config"
+
+  case "$action" in
+
+    reset)
+      log i "------------------------"
+      log i "Resetting Solarus"
+      log i "------------------------"
+
+      create_dir "$XDG_CONFIG_HOME/solarus"
+    ;;
+
+  esac
+}
