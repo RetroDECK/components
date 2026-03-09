ebfa4b64692c97aad7ebf91fbc9caa164f3b94de
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-09 09:38:07 -0400

    Switch from wget to curl in Alchemist downloaders

--- automation-tools/alchemist/lib/downloaders/github_release.sh
+++ automation-tools/alchemist/lib/downloaders/github_release.sh
@@ -96,7 +96,7 @@ download() {
   log info "Destination: $final_dest"
 
   download_cmd() {
-    wget -q -O "$final_dest" "$final_url" 2>&1
+    curl --silent --location --output "$final_dest" "$final_url" 2>&1
   }
 
   if ! try "$max_retries" "$initial_delay" "$max_delay" download_cmd; then
