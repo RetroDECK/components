0ea54f372b4036fbf5cb167f53d2a5e85b3f5bc1
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-09 11:34:50 -0400

    Rix whitespace in Ruffle component_launcher file

--- ruffle/component_launcher.sh
+++ ruffle/component_launcher.sh
@@ -15,11 +15,11 @@ static_invoke="--config /var/data/ruffle --save-directory $saves_path/ruffle"
 
 #Check if Steam Deck in Desktop Mode
 if [[ $(check_desktop_mode) == "true" ]]; then
-    log d "Running Ruffle in Desktop Mode"
-    log d "ruffle --graphics vulkan $static_invoke $@"
-    exec "$component_path/bin/ruffle" --graphics vulkan $static_invoke "$@"
+  log d "Running Ruffle in Desktop Mode"
+  log d "ruffle --graphics vulkan $static_invoke $@"
+  exec "$component_path/bin/ruffle" --graphics vulkan $static_invoke "$@"
 else
-    log d "Running Ruffle in Gaming Mode"
-    log d "ruffle --graphics gl --no-gui $static_invoke $@"
-    exec "$component_path/bin/ruffle" --graphics gl --no-gui $static_invoke "$@"
+  log d "Running Ruffle in Gaming Mode"
+  log d "ruffle --graphics gl --no-gui $static_invoke $@"
+  exec "$component_path/bin/ruffle" --graphics gl --no-gui $static_invoke "$@"
 fi
