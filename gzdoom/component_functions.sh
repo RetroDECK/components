c4bd4c9e4a489b5a9582f2173bf2099d3cff6cae
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-11 12:13:34 -0400

    Migrate component_update actions into _post_update routed function in component_functions for GZDOOM

--- gzdoom/component_functions.sh
+++ gzdoom/component_functions.sh
@@ -39,3 +39,29 @@ _prepare_component::gzdoom() {
 
   esac
 }
+
+_post_update::gzdoom() {
+  local previous_version="$1"
+
+}
+
+_post_update_legacy::gzdoom() {
+  # This function is to cover users upgrading from prior to 0.11.0, when per-component versioning was introduced. It can be removed once we are confident all users are running 0.11.0 or higher
+  
+  local previous_version="$1"
+
+  if check_version_is_older_than "$previous_version" "0.8.0b"; then
+    log i "- The following components are been added and need to be initialized: es-de 3.0, MAME-SA, Vita3K, GZDoom"
+
+    prepare_component "reset" "gzdoom"
+  fi
+
+  if check_version_is_older_than "$previous_version" "0.10.0b"; then
+
+    log i "0.10.0b Upgrade - Reset: GZDoom"
+
+    prepare_component "reset" "gzdoom"
+
+    # GZDOOM needs to be reset as the changes are in the config that connects to the new folders.
+  fi
+}
