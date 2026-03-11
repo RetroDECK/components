538e7deb20772ee011cc397708076b121e69e91c
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-11 12:14:32 -0400

    Migrate component_update actions into _post_update routed function in component_functions for PICO-8

--- pico-8/component_functions.sh
+++ pico-8/component_functions.sh
@@ -55,3 +55,41 @@ _prepare_component::pico-8() {
 
   esac
 }
+
+_post_update::pico-8() {
+  local previous_version="$1"
+
+}
+
+_post_update_legacy::pico-8() {
+  # This function is to cover users upgrading from prior to 0.11.0, when per-component versioning was introduced. It can be removed once we are confident all users are running 0.11.0 or higher
+  
+  local previous_version="$1"
+
+  if check_version_is_older_than "$previous_version" "0.6.2b"; then
+    # In version 0.6.2b, the following changes were made that required config file updates/reset:
+    # - Fix PICO-8 folder structure. ROM and save folders are now sane and binary files will go into ~/retrodeck/bios/pico-8/
+
+    mv "$bios_path/pico8" "$bios_path/pico8_olddata" # Move legacy (and incorrect / non-functional ) PICO-8 location for future cleanup / less confusion
+    dir_prep "$bios_path/pico-8" "$HOME/.lexaloffle/pico-8" # Store binary and config files together. The .lexaloffle directory is a hard-coded location for the PICO-8 config file, cannot be changed
+    dir_prep "$roms_path/pico8" "$bios_path/pico-8/carts" # Symlink default game location to RD roms for cleanliness (this location is overridden anyway by the --root_path launch argument anyway)
+    dir_prep "$bios_path/pico-8/cdata" "$saves_path/pico-8" # PICO-8 saves folder
+  fi
+
+  if check_version_is_older_than "$previous_version" "0.6.3b"; then
+    # In version 0.6.3b, the following changes were made that required config file updates/reset:
+    # - Fix symlink to hard-coded PICO-8 config folder (dir_prep doesn't like ~)
+
+    rm -rf "$HOME/~/" # Remove old incorrect location from 0.6.2b
+    rm -f "$HOME/.lexaloffle/pico-8" # Remove old symlink to prevent recursion
+    dir_prep "$bios_path/pico-8" "$HOME/.lexaloffle/pico-8" # Store binary and config files together. The .lexaloffle directory is a hard-coded location for the PICO-8 config file, cannot be changed
+    dir_prep "$saves_path/pico-8" "$bios_path/pico-8/cdata" # PICO-8 saves folder structure was backwards, fixing for consistency.
+  fi
+
+  if check_version_is_older_than "$previous_version" "0.7.0b"; then
+    # In version 0.7.0b, the following changes were made that required config file updates/reset or other changes to the filesystem:
+    # - Init PICO-8 as it has newly-shipped config files
+
+    prepare_component "reset" "pico-8"
+  fi
+}
