75e39932331959b8778c280c4644778e421385f6
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-11 12:16:18 -0400

    Migrate component_update actions into _post_update routed function in component_functions for Solarus

--- solarus/component_functions.sh
+++ solarus/component_functions.sh
@@ -18,3 +18,22 @@ _prepare_component::solarus() {
 
   esac
 }
+
+_post_update::solarus() {
+  local previous_version="$1"
+
+}
+
+_post_update_legacy::solarus() {
+  # This function is to cover users upgrading from prior to 0.11.0, when per-component versioning was introduced. It can be removed once we are confident all users are running 0.11.0 or higher
+  
+  local previous_version="$1"
+
+  if check_version_is_older_than "$previous_version" "0.10.0b"; then
+    # In version 0.10.0b, the following changes were made that required config file updates/reset or other changes to the filesystem:
+    # - Init Solarus as it is a new emulator
+    
+    log i "0.10.0b Upgrade - Reset: Solarus"
+    prepare_component "reset" "solarus"
+  fi
+}
