f11ab995f5c7bd567b28c2974c851d194337b20a
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-13 13:18:41 -0400

    Remove Steam user detection in SRM reset, as the steam_sync option is defaulted to false during reset

--- steam-rom-manager/component_functions.sh
+++ steam-rom-manager/component_functions.sh
@@ -30,14 +30,6 @@ _prepare_component::steam-rom-manager() {
       cp -fv "$component_config/"*.json "$srm_userdata"
       cp -fvr "$component_config/manifests" "$srm_userdata"
 
-      get_steam_user
-
-      if [[ -n "$steam_userdata_current" ]]; then
-        log i "Updating steamDirectory and romDirectory lines in $srm_userdata/userSettings.json"
-        jq '.environmentVariables.steamDirectory = "'"$steam_userdata_current"'"' "$srm_userdata/userSettings.json" > "$srm_userdata/tmp.json" && mv -f "$srm_userdata/tmp.json" "$srm_userdata/userSettings.json"
-        jq '.environmentVariables.romsDirectory = "'"$rd_home_path"'/.sync"' "$srm_userdata/userSettings.json" > "$srm_userdata/tmp.json" && mv -f "$srm_userdata/tmp.json" "$srm_userdata/userSettings.json"
-      fi
-
       if [[ ! -z $(find "$HOME/.steam/steam/controller_base/templates/" -maxdepth 1 -type f -iname "RetroDECK*.vdf" 2>/dev/null) || ! -z $(find "$HOME/.var/app/com.valvesoftware.Steam/.steam/steam/controller_base/templates/" -maxdepth 1 -type f -iname "RetroDECK*.vdf" 2>/dev/null) ]]; then # If RetroDECK controller profile has been previously installed
         install_retrodeck_controller_profile
       fi
