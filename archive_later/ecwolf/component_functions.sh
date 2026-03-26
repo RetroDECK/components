0befec01f97d26f940cf6a011f1cf78a61e4a419
XargonWan <xargonwan@gmail.com>
2026-03-26 12:33:08 +0900

    fix(ecwolf): adapted component prepare for new 0.11.0b structure [skip ci]

--- archive_later/ecwolf/component_functions.sh
+++ archive_later/ecwolf/component_functions.sh
@@ -10,5 +10,30 @@ ecwolf_config_path="$XDG_CONFIG_HOME/ecwolf"
 ecwolf_saves_path="$XDG_DATA_HOME/ecwolf/saves"
 ecwolf_roms_path="$roms_path/wolf"
 
+_prepare_component::ecwolf() {
+    # Setting component name and path based on the directory name
+    component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
+    component_config="/app/retrodeck/components/$component_name/rd_config"
+    ecwolf_rd_cfg="$XDG_CONFIG_HOME/$component_name/ecwolf_rd.cfg"
 
+    if [[ "$action" == "reset" ]]; then # Run reset-only commands
+    log i "----------------------"
+    log i "Preparing $component_name"
+    log i "----------------------"
+
+    dir_prep "$saves_path/ecwolf" "$ecwolf_saves_path"
+    rm -vrf "$ecwolf_rd_cfg"
+    cp -v "$component_config/ecwolf_rd.cfg" "$ecwolf_rd_cfg"
+
+    fi
+
+    if [[ "$action" == "postmove" ]]; then # Run commands that apply to both resets and moves
+    log i "----------------------"
+    log i "Post-moving $component_name"
+    log i "----------------------"
+
+    dir_prep "$saves_path/ecwolf" "$ecwolf_saves_path"
+    
+    fi    
+}
 
