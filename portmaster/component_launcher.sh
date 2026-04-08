47db66de649e412ccde48ec8a1ab6cfbe38e5482
icenine451 <benjamin.r.shelton@protonmail.com>
2026-04-08 14:32:46 -0400

    Remove undefined path from PortMaster LD_LIBRARY_PATH modification

--- portmaster/component_launcher.sh
+++ portmaster/component_launcher.sh
@@ -2,7 +2,7 @@
 
 source /app/libexec/launcher_functions.sh
 
-export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs:$rd_shared_libs/org.gnome.Platform/49/:$ffmpeg_path/25.08:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs:$rd_shared_libs/org.gnome.Platform/49/:${LD_LIBRARY_PATH}"
 export QT_PLUGIN_PATH="${QT_PLUGIN_PATH}"
 export QT_QPA_PLATFORM_PLUGIN_PATH="${QT_QPA_PLATFORM_PLUGIN_PATH}"
 
