47b4c39911f20c687f402fb928d08a19ff98662a
icenine451 <benjamin.r.shelton@protonmail.com>
2026-04-08 14:31:29 -0400

    Remove undefined path from FLIPS LD_LIBRARY_PATH modification

--- flips/component_launcher.sh
+++ flips/component_launcher.sh
@@ -6,7 +6,7 @@ source /app/libexec/launcher_functions.sh
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-export LD_LIBRARY_PATH="$component_path/lib:$ffmpeg_path/25.08:$rd_shared_libs/org.gnome.Platform/49/:${DEFAULT_LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs/org.gnome.Platform/49/:${DEFAULT_LD_LIBRARY_PATH}"
 
 # Set plugin paths
 
