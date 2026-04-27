c3e0d2c8d554f83858216ea629273bb5f5d6357e
XargonWan <xargonwan@gmail.com>
2026-04-27 21:47:34 +0900

    feat(satellaview): fix ROM path check and streamline environment variable exports in launcher script

--- satellaview_plus/component_launcher.sh
+++ satellaview_plus/component_launcher.sh
@@ -2,21 +2,20 @@
 
 source /app/libexec/launcher_functions.sh
 
-if [[ ! -f "$satellaview_plus_bsx_path/bs-x.sfc" ]]; then
+if [[ ! -f "$roms_path/satellaview_plus/bs-x.sfc" ]]; then
   rd_zenity --error --no-wrap \
         --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" \
         --title "RetroDECK: Satellaview+ - Error: ROM not found" \
         --text="Satellaview+ ROM not found.\n\nPlease provide the 'bs-x.sfc' ROM file in the\n'$satellaview_plus_bsx_path'\n\ndirectory and try again."
   exit 1
+else
+  cp "$roms_path/satellaview_plus/bs-x.sfc" "$satellaview_plus_bsx_path/bs-x.sfc"
+  # TODO: if we need to patch the roms we can do it here
 fi
 
 # Setting component name and path based on the directory name
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
-
-export LD_LIBRARY_PATH="$component_path/usr/lib/:$component_path/lib:$rd_shared_libs:${DEFAULT_LD_LIBRARY_PATH}"
-export QT_PLUGIN_PATH="${QT_PLUGIN_PATH}"
-export QT_QPA_PLATFORM_PLUGIN_PATH="${QT_QPA_PLATFORM_PLUGIN_PATH}"
 
 log i "RetroDECK is now launching $component_name"
 log d "Library path is: $LD_LIBRARY_PATH"
@@ -25,10 +24,17 @@ log d "Library path is: $LD_LIBRARY_PATH"
 command=$(cat "$1")
 
 case $command in
+
   run)
     XDG_CONFIG_HOME="$XDG_CONFIG_HOME/satellaview_plus/snes9x" exec "$component_path/bin/snes9x-gtk" "$satellaview_plus_bsx_path/bs-x.sfc"
     ;;
+
   tuner)
+
+    export LD_LIBRARY_PATH="$component_path/usr/lib/:$component_path/lib:$rd_shared_libs:${DEFAULT_LD_LIBRARY_PATH}"
+    export QT_PLUGIN_PATH="${QT_PLUGIN_PATH}"
+    export QT_QPA_PLATFORM_PLUGIN_PATH="${QT_QPA_PLATFORM_PLUGIN_PATH}"
+
     # We move here because the config file is read from ./
     cd "$satellaview_plus_config_path"
     exec "$component_path/AppRun"
