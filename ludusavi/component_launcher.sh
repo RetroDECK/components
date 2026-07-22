8efba71ff60b9723d39b6663a45d520c72fbb96d
icenine451 <benjamin.r.shelton@protonmail.com>
2026-07-22 14:07:29 -0400

    Initial Ludusavi component launcher

---
+++ ludusavi/component_launcher.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source /app/libexec/launcher_functions.sh
+
+# Setting component name and path based on the directory name
+component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
+component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+export LD_LIBRARY_PATH="${DEFAULT_LD_LIBRARY_PATH}"
+
+# Set plugin paths
+
+log i "RetroDECK is now launching $component_name"
+log d "Library path is: $LD_LIBRARY_PATH"
+log d "AppDir is: $component_path"
+
+exec "$component_path/bin/ludusavi" "$@"
