47143f0458d5c86f6fbc0608a075803b5af31791
lazzite <thelazorne@protonmail.com>
2026-08-06 07:17:14 +0200

    raproxy files from dev

--- raofflineproxy/component_functions.sh
+++ raofflineproxy/component_functions.sh
@@ -13,9 +13,13 @@ _prepare_component::raofflineproxy() {
       log i "Resetting RAOfflineProxy"
       log i "----------------------"
 
-  # RAOfflineProxy resolves its configuration directory at startup using `XDG_CONFIG_HOME` (fallback: `~/.config/raofflineproxy`). 
-
+  # raofflineproxy resolves its config dir from XDG_CONFIG_HOME and its API
+  # response cache from XDG_CACHE_HOME at startup (falling back to
+  # ~/.config/raofflineproxy and ~/.cache/raofflineproxy), so just make sure
+  # both exist ahead of first launch.
+  
   create_dir "$XDG_CONFIG_HOME/raofflineproxy"
+  create_dir "$XDG_CACHE_HOME/raofflineproxy"
     ;;
 
   esac
