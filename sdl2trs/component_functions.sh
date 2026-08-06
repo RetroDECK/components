f59593227b1f39d097a8e5a212baee19905e1cf3
lazzite <thelazorne@protonmail.com>
2026-08-06 06:55:42 +0200

    sdl2trs

--- sdl2trs/component_functions.sh
+++ sdl2trs/component_functions.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export sdl2trs_config="$XDG_CONFIG_HOME/sdl2trs/.sdltrs.t8c"
+
 _prepare_component::sdl2trs() {
   local action="$1"
   shift
@@ -13,6 +15,24 @@ _prepare_component::sdl2trs() {
       log i "Resetting SDL2TRS"
       log i "----------------------"
 
+      cp -f "$component_config/sdltrs.t8c" "$XDG_CONFIG_HOME/sdl2trs/.sdltrs.t8c"
+      
+      create_dir -d "$states_path/trs-80"
+      create_dir -d "$storage_path/SDL2TRS"
+
+      sed -i "s|^statedir=.*|statedir=$states_path/trs-80|" $sdl2trs_config
+
+      sed -i "s|^romfile1=.*|romfile1=$bios_path/level2.rom|" $sdl2trs_config
+      sed -i "s|^romfile3=.*|romfile3=$bios_path/model3.rom|" $sdl2trs_config
+      sed -i "s|^romfile4p=.*|romfile4p=$bios_path/model4p.rom|" $sdl2trs_config
+
+      sed -i "s|^cassdir=.*|cassdir=$roms_path/trs-80|" $sdl2trs_config
+      sed -i "s|^diskdir=.*|diskdir=$roms_path/trs-80|" $sdl2trs_config
+      sed -i "s|^disksetdir=.*|disksetdir=$roms_path/trs-80|" $sdl2trs_config
+      sed -i "s|^harddir=.*|harddir=$roms_path/trs-80|" $sdl2trs_config
+
+      sed -i "s|^printerdir=.*|printerdir=$storage_path/SDL2TRS|" $sdl2trs_config
+
     ;;
 
     postmove)
@@ -20,6 +40,23 @@ _prepare_component::sdl2trs() {
       log i "Post-moving SDL2TRS"
       log i "----------------------"
       
+      cp -f "$component_config/sdltrs.t8c" "$XDG_CONFIG_HOME/sdl2trs/.sdltrs.t8c"
+      
+      create_dir -d "$states_path/trs-80"
+      create_dir -d "$storage_path/SDL2TRS"
+
+      sed -i "s|^statedir=.*|statedir=$states_path/trs-80|" $sdl2trs_config
+
+      sed -i "s|^romfile1=.*|romfile1=$bios_path/level2.rom|" $sdl2trs_config
+      sed -i "s|^romfile3=.*|romfile3=$bios_path/model3.rom|" $sdl2trs_config
+      sed -i "s|^romfile4p=.*|romfile4p=$bios_path/model4p.rom|" $sdl2trs_config
+
+      sed -i "s|^cassdir=.*|cassdir=$roms_path/trs-80|" $sdl2trs_config
+      sed -i "s|^diskdir=.*|diskdir=$roms_path/trs-80|" $sdl2trs_config
+      sed -i "s|^disksetdir=.*|disksetdir=$roms_path/trs-80|" $sdl2trs_config
+      sed -i "s|^harddir=.*|harddir=$roms_path/trs-80|" $sdl2trs_config
+
+      sed -i "s|^printerdir=.*|printerdir=$storage_path/SDL2TRS|" $sdl2trs_config
 
   esac
 }
