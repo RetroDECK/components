8e8a562c22d3949994f4d7a47a2bb2ca106e8719
lazzite <thelazorne@protonmail.com>
2026-08-08 15:50:31 +0200

    fs-uae function config

--- fs-uae/component_functions.sh
+++ fs-uae/component_functions.sh
@@ -90,3 +90,33 @@ _prepare_component::fs-uae() {
   esac
 }
 
+fs-uae_amigavision_toggle() {
+  local file="$roms_path/amiga/AmigaVision.fdi"
+
+  local response
+  response=$(rd_zenity --question --no-wrap \
+    --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" \
+    --title="AmigaVision" \
+    --ok-label="Enable" \
+    --cancel-label="Cancel" \
+    --extra-button="Disable" \
+    --text="<big><b>AmigaVision in ES-DE under the Commodore Amiga menu.</b></big>\n\nEnable or disable AmigaVision?")
+
+  local rc=$?
+
+  if [ $rc -eq 0 ]; then 
+    touch "$file"
+    rd_zenity --info --no-wrap \
+      --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" \
+      --title="Success" \
+      --text="AmigaVision has been enabled.\n\nA restart of RetroDECK is required for the entry to appear." \
+      --width=400
+  elif [ "$response" = "Disable" ]; then
+    [[ -f "$file" ]] && rm "$file"
+    rd_zenity --info --no-wrap \
+      --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" \
+      --title="Success" \
+      --text="AmigaVision has been disabled." \
+      --width=400
+  fi  # 
+}
