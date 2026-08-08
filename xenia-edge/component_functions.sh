750e2ac75f0c1a33fce15d089acc35203c7027cb
lazzite <thelazorne@protonmail.com>
2026-08-08 20:12:39 +0200

    xenia edge init

---
+++ xenia-edge/component_functions.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+export xenia_edge_config="$XDG_CONFIG_HOME/Xenia/xenia-edge.config.toml"
+
+## Xenia Edge uses Xenia as directory name and default writes into $XDG_DATA_HOME/Xenia
+
+_prepare_component::xenia-edge() {
+  local action="$1"
+  shift
+
+  local component_config="$(get_own_component_path)/rd_config"
+
+  case "$action" in
+
+    reset)
+      log i "----------------------"
+      log i "Resetting Xenia Edge"
+      log i "----------------------"
+
+      create_dir -d "$XDG_DATA_HOME/Xenia"
+      cp -fr "$component_config/"* "$XDG_DATA_HOME/Xenia"
+      dir_prep "$storage_path/Xenia/cache" "$XDG_DATA_HOME/Xenia/cache"
+      dir_prep "$storage_path/Xenia/cache0" "$XDG_DATA_HOME/Xenia/cache0"
+      dir_prep "$storage_path/Xenia/cache1" "$XDG_DATA_HOME/Xenia/cache1"
+      dir_prep "$storage_path/Xenia/content" "$XDG_DATA_HOME/Xenia/content"
+      dir_prep "$storage_path/Xenia/library" "$XDG_DATA_HOME/Xenia/library"
+
+    ;;
+
+
+    postmove)
+      log i "------------------------"
+      log i "Post-moving Xenia Edge"
+      log i "------------------------"
+
+      dir_prep "$storage_path/Xenia/cache" "$XDG_DATA_HOME/Xenia/cache"
+      dir_prep "$storage_path/Xenia/cache0" "$XDG_DATA_HOME/Xenia/cache0"
+      dir_prep "$storage_path/Xenia/cache1" "$XDG_DATA_HOME/Xenia/cache1"
+      dir_prep "$storage_path/Xenia/content" "$XDG_DATA_HOME/Xenia/content"
+      dir_prep "$storage_path/Xenia/library" "$XDG_DATA_HOME/Xenia/library"
+
+    ;;
+
+  esac
+}
