073747b04b6652fdb508fd7ffe096f29f6c9b19d
lazzite <thelazorne@protonmail.com>
2026-08-09 17:27:41 +0200

    vpx init

---
+++ vpinballx/component_functions.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export vpinballx_config="$XDG_CONFIG_HOME/vpinballx/VPinballX.ini"
+
+
+_prepare_component::vpinballx() {
+  local action="$1"
+  shift
+
+  local component_config="$(get_own_component_path)/rd_config"
+
+  case "$action" in
+
+    reset)
+      log i "----------------------"
+      log i "Resetting Visual Pinball X"
+      log i "----------------------"
+
+      create_dir "$XDG_CONFIG_HOME/vpinballx/.vpinball/user"
+      cp -fr "$component_config/"* "$XDG_CONFIG_HOME/vpinballx"
+      
+
+    ;;
+
+    postmove)
+      log i "----------------------"
+      log i "Post-moving Visual Pinball X"
+      log i "----------------------"
+
+
+    ;;
+    
+  esac
+}
