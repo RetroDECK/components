e291db81c0be5c021556036e496365f3ad1744d0
lazzite <thelazorne@protonmail.com>
2026-08-09 20:28:16 +0200

    supermodel

---
+++ supermodel/component_functions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export simcoupe_config="$XDG_CONFIG_HOME/supermodel/Config/Supermodel.ini"
+
+_prepare_component::supermodel() {
+  local action="$1"
+  shift
+
+  local component_config="$(get_own_component_path)/rd_config"
+
+  case "$action" in
+
+    reset)
+      log i "----------------------"
+      log i "Resetting SimCoupé"
+      log i "----------------------"
+
+      create_dir -d "$XDG_CONFIG_HOME/supermodel/"
+      cp -fr "$component_config/"* "$XDG_CONFIG_HOME/supermodel/Config"
+
+    ;;
+
+  esac
+}
