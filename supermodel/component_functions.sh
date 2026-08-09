9b217e60b7868b1eeb8a008234c2882239b56ef4
lazzite <thelazorne@protonmail.com>
2026-08-09 21:44:53 +0200

    roms to config

--- supermodel/component_functions.sh
+++ supermodel/component_functions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Supermodel all config folders and files needs to be next to the roms.
+# Supermodel needs to point at the config folders from roms
 
-export supermodel_config="$roms_path/model3/Config/Supermodel.ini"
+export supermodel_config="$XDG_CONFIG_HOME/supermodel/Config/Supermodel.ini"
 
 _prepare_component::supermodel() {
   local action="$1"
@@ -17,7 +17,28 @@ _prepare_component::supermodel() {
       log i "Resetting Supermodel"
       log i "----------------------"
 
-      cp -fr "$component_config/"* "$roms_path/model3/"
+      create_dir -d "$XDG_CONFIG_HOME/supermodel/"
+      cp -fr "$component_config/"* "$XDG_CONFIG_HOME/supermodel/"
+      dir_prep "$XDG_CONFIG_HOME/supermodel/NVRAM" "$roms_path/model3/NVRAM"
+      dir_prep "$XDG_CONFIG_HOME/supermodel/Config" "$roms_path/model3/Config"
+      dir_prep "$XDG_CONFIG_HOME/supermodel/Assets" "$roms_path/model3/Assets"
+      dir_prep "$logs_path/supermodel/Analysis" "$roms_path/model3/Analysis"
+      dir_prep "$logs_path/supermodel/Logs" "$roms_path/model3/Logs"
+      dir_prep "$saves_path/model3/supermodel/Saves" "$roms_path/model3/Saves"
+
+    ;;
+
+    postmove)
+          log i "----------------------"
+          log i "Post-moving Supermodel"
+          log i "----------------------"
+
+      dir_prep "$XDG_CONFIG_HOME/supermodel/NVRAM" "$roms_path/model3/NVRAM"
+      dir_prep "$XDG_CONFIG_HOME/supermodel/Config" "$roms_path/model3/Config"
+      dir_prep "$XDG_CONFIG_HOME/supermodel/Assets" "$roms_path/model3/Assets"
+      dir_prep "$logs_path/supermodel/Analysis" "$roms_path/model3/Analysis"
+      dir_prep "$logs_path/supermodel/Logs" "$roms_path/model3/Logs"
+      dir_prep "$saves_path/model3/supermodel/Saves" "$roms_path/model3/Saves"
 
     ;;
 
