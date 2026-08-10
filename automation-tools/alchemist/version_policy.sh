609e3f453d083bfcf45d648d5e72bc2de306a3f3
etjanster <thelazorne@protonmail.com>
2026-08-10 10:58:49 +0200

    xenia edge temp hardcode fix

--- automation-tools/alchemist/version_policy.sh
+++ automation-tools/alchemist/version_policy.sh
@@ -237,7 +237,7 @@ export FS_UAE_VERSION_POLICY="latest"
 # Source: AppImage
 # Link: https://github.com/has207/xenia-edge/releases
 # ------------------------------------------------------------------------------
-export XENIA_EDGE_VERSION_POLICY="latest"
+export XENIA_EDGE_VERSION_POLICY="newest"
 
 # ------------------------------------------------------------------------------
 # Visual Pinball X - Pinball Simulation Engine
