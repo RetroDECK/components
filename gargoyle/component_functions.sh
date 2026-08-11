55cbeece5b0424749f2789d2f42e84e781632da8
etjanster <thelazorne@protonmail.com>
2026-08-11 10:06:23 +0200

    garg confg

--- gargoyle/component_functions.sh
+++ gargoyle/component_functions.sh
@@ -17,10 +17,23 @@ _prepare_component::gargoyle() {
       log i "Resetting Gargoyle"
       log i "----------------------"
 
-  create_dir "$XDG_CONFIG_HOME/gargoyle"
-  dir_prep "$storage_path/gargoyle/themes" "$XDG_DATA_HOME/gargoyle/themes"
-  
+      create_dir "$XDG_CONFIG_HOME/gargoyle"
+      create_dir "$XDG_DATA_HOME/gargoyle"
+      dir_prep "$storage_path/gargoyle/themes" "$XDG_DATA_HOME/gargoyle/themes"
+      dir_prep "$saves_path/if/gargoyle/gamedata" "$XDG_DATA_HOME/gargoyle/gamedata"
+
     ;;
 
-      esac
+      postmove)
+      log i "------------------------"
+      log i "Post-moving Gargoyle"
+      log i "------------------------"
+
+      dir_prep "$storage_path/gargoyle/themes" "$XDG_DATA_HOME/gargoyle/themes"
+      dir_prep "$saves_path/if/gargoyle/gamedata" "$XDG_DATA_HOME/gargoyle/gamedata"
+    ;;
+
+  esac
+
 }
+
