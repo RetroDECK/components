486e1dba372470ae73c7d32f94fc472494905b57
lazzite <thelazorne@protonmail.com>
2026-08-11 17:18:53 +0200

    scummvm config

--- scummvm/component_functions.sh
+++ scummvm/component_functions.sh
@@ -17,12 +17,29 @@ _prepare_component::scummvm() {
 
         cp -fvr "$component_config/"* "$XDG_CONFIG_HOME/scummvm/"
 
+        create_dir -d "$screenshots_path/scummvm"
+        create_dir -d "$saves_path/scummvm/scummvm-sa"
+        create_dir -d "$storage_path/scummvm/extra"
+        create_dir -d "$storage_path/scummvm/themes"
+
+        sed -i "s|^screenshotpath= .*|screenshotpath=$screenshots_path/scummvm|" $scummvm_config
+        sed -i "s|^savepath= .*|savepath=$saves_path/scummvm/scummvm-sa|" $scummvm_config
+
+        sed -i "s|^extrapath= .*|extrapath=$storage_path/scummvm/extra|" $scummvm_config
+        sed -i "s|^themepath= .*|themepath=$storage_path/scummvm/themes|" $scummvm_config
+      
     ;;
 
     postmove)
       log i "----------------------"
       log i "Post-moving ScummVM"
       log i "----------------------"
+
+        sed -i "s|^screenshotpath= .*|screenshotpath=$screenshots_path/scummvm|" $scummvm_config
+        sed -i "s|^savepath= .*|savepath=$saves_path/scummvm/scummvm-sa|" $scummvm_config
+
+        sed -i "s|^extrapath= .*|extrapath=$storage_path/scummvm/extra|" $scummvm_config
+        sed -i "s|^themepath= .*|themepath=$storage_path/scummvm/themes|" $scummvm_config
 
     ;;
     
