e3879d34ce68e37a6cf156d6602a56715f29ce7b
icenine451 <benjamin.r.shelton@protonmail.com>
2026-08-12 15:47:42 -0400

    Change OpenSurge Surge the Rabbit visibility to gamelist manipulation system

--- opensurge/component_functions.sh
+++ opensurge/component_functions.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 
 opensurge_surgetherabbit_toggle() {
-  local file="$roms_path/opensurge/Surge the Rabbit.surge"
+  if [[ ! $(get_component_option "opensurge" "surge_the_rabbit_visible") == "false" ]]; then
+    rd_zenity --question \
+    --no-wrap --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" \
+    --title "RetroDECK Configurator - OpenSurge Surge the Rabbit Visibility" \
+    --text="OpenSurge Surge the Rabbit visibility is currently <span foreground='$purple'><b>Enabled</b></span>. Do you want to disable it?"
+    
+    if [ $? == 0 ] # User clicked "Yes"
+    then
+      remove_gamelist_entry "opensurge" "opensurge_surge_the_rabbit"
+      set_component_option "opensurge" "surge_the_rabbit_visible" "false"
+      
+      rd_zenity --info \
+      --no-wrap --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" \
+      --title "RetroDECK Configurator - OpenSurge Surge the Rabbit Visibility" \
+      --text="OpenSurge Surge the Rabbit visibility is now <span foreground='$purple'><b>Disabled</b></span>."
+    fi
+  else
+    rd_zenity --question \
+    --no-wrap --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" \
+    --title "RetroDECK Configurator - OpenSurge Surge the Rabbit Visibility" \
+    --text="OpenSurge Surge the Rabbit visibility is currently <span foreground='$purple'><b>Disabled</b></span>. Do you want to enable it?"
 
-  local response
-  response=$(rd_zenity --question --no-wrap \
-    --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" \
-    --title="Surge the Rabbit" \
-    --ok-label="Enable" \
-    --cancel-label="Cancel" \
-    --extra-button="Disable" \
-    --text="<big><b>Surge the Rabbit in Open Surge</b></big>\n\nEnable or disable the game?")
+    if [ $? == 0 ] # User clicked "Yes"
+    then
+      create_gamelist_entry "opensurge" "opensurge_surge_the_rabbit"
+      set_component_option "opensurge" "surge_the_rabbit_visible" "true"
 
-  local rc=$?
-
-  if [ $rc -eq 0 ]; then 
-    touch "$file"
-    rd_zenity --info --no-wrap \
-      --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" \
-      --title="Success" \
-      --text="Surge the Rabbit has been enabled.\n\nA restart of RetroDECK is required for the game to appear." \
-      --width=400
-  elif [ "$response" = "Disable" ]; then
-    [[ -f "$file" ]] && rm "$file"
-    rd_zenity --info --no-wrap \
-      --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" \
-      --title="Success" \
-      --text="Surge the Rabbit has been disabled." \
-      --width=400
-  fi  # 
+      rd_zenity --info \
+      --no-wrap --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" \
+      --title "RetroDECK Configurator - OpenSurge Surge the Rabbit Visibility" \
+      --text="OpenSurge Surge the Rabbit visibility is now <span foreground='$purple'><b>Enabled</b></span>."
+    fi
+  fi
 }
