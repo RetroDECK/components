d25716ad35c3fbe42c9d6528590544f6170179d4
lazzite <thelazorne@protonmail.com>
2026-08-12 20:39:43 +0200

    fix missing export in fs-uae

--- fs-uae/component_functions.sh
+++ fs-uae/component_functions.sh
@@ -4,13 +4,13 @@
 # Make a function that copies AmigaVision.fdi to $roms_path/amiga/ If enabled. 
 # If disabled it deletes the file $roms_path/amiga/AmigaVision.fdi if it's there during boot.
 
-fs-uae_config_amiga1200="$XDG_CONFIG_HOME/fs-uae/RetroDECK-amiga1200.fs-uae"
-fs-uae_config_amiga600="$XDG_CONFIG_HOME/fs-uae/RetroDECK-amiga600.fs-uae"
-fs-uae_config_amigacd32="$XDG_CONFIG_HOME/fs-uae/RetroDECK-amigacd32.fs-uae"
-fs-uae_config_cdtv="$XDG_CONFIG_HOME/fs-uae/RetroDECK-cdtv.fs-uae"
-amigavision_config="$XDG_CONFIG_HOME/fs-uae/AmigaVision.fs-uae"
-custom_config_amiga600="$XDG_CONFIG_HOME/fs-uae/Custom/custom-amiga600.fs-uae"
-custom_config_amiga1200="$XDG_CONFIG_HOME/fs-uae/Custom/custom-amiga1200.fs-uae"
+export fs-uae_config_amiga1200="$XDG_CONFIG_HOME/fs-uae/RetroDECK-amiga1200.fs-uae"
+export fs-uae_config_amiga600="$XDG_CONFIG_HOME/fs-uae/RetroDECK-amiga600.fs-uae"
+export fs-uae_config_amigacd32="$XDG_CONFIG_HOME/fs-uae/RetroDECK-amigacd32.fs-uae"
+export fs-uae_config_cdtv="$XDG_CONFIG_HOME/fs-uae/RetroDECK-cdtv.fs-uae"
+export amigavision_config="$XDG_CONFIG_HOME/fs-uae/AmigaVision.fs-uae"
+export custom_config_amiga600="$XDG_CONFIG_HOME/fs-uae/Custom/custom-amiga600.fs-uae"
+export custom_config_amiga1200="$XDG_CONFIG_HOME/fs-uae/Custom/custom-amiga1200.fs-uae"
 
 
 _prepare_component::fs-uae() {
