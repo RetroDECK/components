4df7cd201e097c94c122653f703a6de509c51edc
etjanster <thelazorne@protonmail.com>
2026-08-17 11:28:15 +0200

    raze init

---
+++ raze/component_functions.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+export raze_config="$XDG_CONFIG_HOME/raze/.config/raze.ini"
+
+_prepare_component::raze() {
+  local action="$1"
+  shift
+
+  local component_config="$(get_own_component_path)/rd_config"
+
+  case "$action" in
+
+    reset)
+      log i "------------------------"
+      log i "Resetting Raze"
+      log i "------------------------"
+
+      create_dir -d "$XDG_CONFIG_HOME/raze/.config/"
+      cp -fv "$component_config/"* "$XDG_CONFIG_HOME/raze/.config/"
+
+      create_dir -d "$saves_path/raze"      
+      create_dir -d "$logs_path/raze"   
+      create_dir -d "$storage_path/raze/soundfonts"
+      create_dir -d "$screenhots_path/raze"           
+
+      sed -i "s|^Path=RETRODECKROMSDIR*|Path=$roms_path/raze|" $raze_config
+      sed -i "s|^Path=RETRODECKSTORAGEDIR/raze/soundfonts*|Path=$storage_path/raze/soundfonts|" $raze_config
+      sed -i "s|^save_dir=RETRODECKSAVEDIR*|save_dir=$saves_path/raze|" $raze_config
+      sed -i "s|^screenshot_dir=RETRODECKSAVEDIR*|screenshot_dir=$screenshots_path/raze|" $raze_config
+
+    ;;
+
+    postmove)
+      log i "------------------------"
+      log i "Post-moving Raze"
+      log i "------------------------"
+
+      sed -i "s|^Path=RETRODECKROMSDIR*|Path=$roms_path/raze|" $raze_config
+      sed -i "s|^Path=RETRODECKSTORAGEDIR/raze/soundfonts*|Path=$storage_path/raze/soundfonts|" $raze_config
+      sed -i "s|^save_dir=RETRODECKSAVEDIR*|save_dir=$saves_path/raze|" $raze_config
+      sed -i "s|^screenshot_dir=RETRODECKSAVEDIR*|screenshot_dir=$screenshots_path/raze|" $raze_config
+      
+    ;;
+
+  esac
+}
+
+
---
+++ raze/component_launcher.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source /app/libexec/launcher_functions.sh
+
+# Setting component name and path based on the directory name
+component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
+component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs:${DEFAULT_LD_LIBRARY_PATH}"
+
+HOME=/var/config/raze
+
+log i "RetroDECK is now launching $component_name"
+log d "Library path is: $LD_LIBRARY_PATH"
+
+# Launch
+exec "$component_path/bin/raze" "$@"
