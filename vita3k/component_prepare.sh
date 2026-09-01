7b4a5edcc328500b3e11da9c564d9b6a4b1e1fa3
lazzite <thelazorne@protonmail.com>
2026-09-01 17:53:48 +0200

    vita3k [skip ci]

--- vita3k/component_prepare.sh
+++ vita3k/component_prepare.sh
@@ -12,7 +12,7 @@ if [[ "$action" == "reset" ]]; then # Run reset-only commands
   # NOTE: the component is writing in "." so it must be placed in the rw filesystem. A symlink of the binary is already placed in /app/bin/Vita3K
   rm -rf "$XDG_CONFIG_HOME/Vita3K"
   create_dir "$XDG_CONFIG_HOME/Vita3K"
-  cp -fv "$component_config/config.yml" "$vita3k_config" # component config
+  cp -fr "$component_config/"* "$XDG_CONFIG_HOME/Vita3K" 
   create_dir "$storage_path/psvita/Vita3K/"
   cp -frv "$component_config/ux0" "$storage_path/psvita/Vita3K/" # User config
   set_setting_value "$vita3k_config" "pref-path" "$storage_path/psvita/Vita3K/" "vita3k"
