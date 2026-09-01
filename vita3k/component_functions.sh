c40393bf00a09d78655e52db63ee463b53b33623
lazzite <thelazorne@protonmail.com>
2026-09-01 17:55:46 +0200

    wiki

--- vita3k/component_functions.sh
+++ vita3k/component_functions.sh
@@ -2,6 +2,7 @@
 
 export vita3k_config="$XDG_CONFIG_HOME/Vita3K/config.yml"
 export vita3k_config_user="$XDG_CONFIG_HOME/Vita3K/ux0/user/00/user.xml"
+export vita3k_config_gui="$XDG_CONFIG_HOME/Vita3K/gui-configs/CurrentSettings.ini"
 export vita3k_vu_firmware_url="http://dus01.psv.update.playstation.net/update/psv/image/2022_0209/rel_f2c7b12fe85496ec88a0391b514d6e3b/PSVUPDAT.PUP"
 export vita3k_2u_firmware_url="http://dus01.psp2.update.playstation.net/update/psp2/image/2019_0924/sd_8b5f60b56c3da8365b973dba570c53a5/PSP2UPDAT.PUP?dest=us"
 export vita3k_component_dir="$rd_components/vita3k"
