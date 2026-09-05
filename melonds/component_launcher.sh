33b3d25e12bde9cc649e5d7359cc004e9e3cfda0
lazzite <thelazorne@protonmail.com>
2026-09-05 12:32:47 +0200

    melonds fixes

--- melonds/component_launcher.sh
+++ melonds/component_launcher.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-source /app/libexec/launcher_functions.sh
-
 # Setting component name and path based on the directory name
 component_name="$(basename "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 component_path="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-export LD_LIBRARY_PATH="$rd_shared_libs:$rd_shared_libs/org.gnome.Platform/49/:${DEFAULT_LD_LIBRARY_PATH}"
-export QT_PLUGIN_PATH="${QT_PLUGIN_PATH}"
-export QT_QPA_PLATFORM_PLUGIN_PATH="${QT_QPA_PLATFORM_PLUGIN_PATH}"
+export LD_LIBRARY_PATH="$component_path/lib:$rd_shared_libs/org.kde.Platform/6.10/:${DEFAULT_LD_LIBRARY_PATH}"
+export QT_PLUGIN_PATH="$rd_shared_libs/org.kde.Platform/6.10/plugins/:${QT_PLUGIN_PATH}"
+export QT_QPA_PLATFORM_PLUGIN_PATH="$rd_shared_libs/org.kde.Platform/6.10/plugins/platforms/:${QT_QPA_PLATFORM_PLUGIN_PATH}"
 
 log i "RetroDECK is now launching $component_name"
 log d "Library path is: $LD_LIBRARY_PATH"
