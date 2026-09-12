da669e7a9aa59e60917b218820dbfff90fd40fde
lazzite <thelazorne@protonmail.com>
2026-09-12 18:30:00 +0200

    remove exec

--- ironwail/component_launcher.sh
+++ ironwail/component_launcher.sh
@@ -17,4 +17,4 @@ log d "QT plugin path is: $QT_PLUGIN_PATH"
 log d "QT QPA PLATFORM plugin path is: $QT_QPA_PLATFORM_PLUGIN_PATH"
 
 # Launch
-exec "$component_path/ironwail" -basedir /var/data/ironwail/ +exec /var/config/ironwail/ironwail.cfg "$@"
+exec "$component_path/ironwail" -basedir /var/data/ironwail/ "$@"
