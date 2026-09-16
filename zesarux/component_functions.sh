09be64c2dfe232fbbdf5b6e3ee8f51ef3d4a4b9b
lazzite <thelazorne@protonmail.com>
2026-09-16 06:13:41 +0200

    config fixes zesarux

--- zesarux/component_functions.sh
+++ zesarux/component_functions.sh
@@ -14,7 +14,21 @@ _prepare_component::zesarux() {
       log i "----------------------"
 
       create_dir -d "$XDG_CONFIG_HOME/zesarux/"
+      create_dir -d "$XDG_DATA_HOME/zesarux/esxdos"
+      cp -fr "$component_config/"* "$XDG_CONFIG_HOME/zesarux"
+
+      dir_prep "$storage_path/Zesarux/esxdos"  "$XDG_DATA_HOME/zesarux/esxdos"
       
+    ;;
+
+
+    postmove)
+      log i "------------------------"
+      log i "Post-moving Zesarux"
+      log i "------------------------"
+
+      dir_prep "$storage_path/Zesarux/esxdos"  "$XDG_DATA_HOME/zesarux/esxdos"
+
     ;;
 
   esac
