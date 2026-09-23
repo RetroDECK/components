5f3b90e31e189b4593d51e0232cea38b5f55bcd0
XargonWan <xargonwan@gmail.com>
2026-09-23 10:09:10 +0200

    test(ES-DE): poiting to 3.5.0-beta test build

--- automation-tools/alchemist/desired_versions.sh
+++ automation-tools/alchemist/desired_versions.sh
@@ -280,10 +280,10 @@ export DESIRED_QT5_RUNTIME_VERSION="5.15-25.08"
 # ------------------------------------------------------------------------------
 # MAIN (Stable)
 # Version: 3.4.0
-  export ES_DE_DESIRED_VERSION="latest"
+#  export ES_DE_DESIRED_VERSION="latest"
 
 # COOKER (Override)
-#  export ES_DE_DESIRED_VERSION="latest"
+export ES_DE_DESIRED_VERSION="release-update-3.5.0-beta-20260923-072547"
 
 
 # ------------------------------------------------------------------------------
